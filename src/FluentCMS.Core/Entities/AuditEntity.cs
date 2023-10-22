308ab01e8341b7abbc5aeebb8d2b14b165bdafde
Mostafa Rowghanian <mostafatech@gmail.com>
2023-10-22 11:45:27 +0330

    add very basic structure for core and repository

---
+++ src/FluentCMS.Core/Entities/AuditEntity.cs
@@ -0,0 +1,30 @@
+﻿namespace FluentCMS.Core.Entities
+{
+    public interface IAuditEntity<TKey> : IEntity<TKey> where TKey : IEquatable<TKey>
+    {
+        string CreatedBy { get; set; } // UserName
+        DateTime CreatedAt { get; set; }
+
+        string LastUpdatedBy { get; set; } // UserName
+        DateTime LastUpdatedAt { get; set; }
+    }
+
+    public interface IAuditEntity : IAuditEntity<Guid>, IEntity
+    {
+    }
+
+    public class AuditEntity<TKey> : IAuditEntity<TKey> where TKey : IEquatable<TKey>
+    {
+        public TKey Id { get; set; }
+
+        public string CreatedBy { get; set; } // UserName
+        public DateTime CreatedAt { get; set; }
+
+        public string LastUpdatedBy { get; set; } // UserName
+        public DateTime LastUpdatedAt { get; set; }
+    }
+
+    public class AuditEntity : AuditEntity<Guid>, IAuditEntity
+    {
+    }
+}
---
+++ src/FluentCMS.Core/Entities/Entity.cs
@@ -0,0 +1,20 @@
+﻿namespace FluentCMS.Core.Entities
+{
+    public interface IEntity<TKey> where TKey : IEquatable<TKey>
+    {
+        TKey Id { get; set; }
+    }
+
+    public interface IEntity : IEntity<Guid>
+    {
+    }
+
+    public class Entity<TKey> : IEntity<TKey> where TKey : IEquatable<TKey>
+    {
+        public TKey Id { get; set; } = default!;
+    }
+
+    public class Entity : Entity<Guid>, IEntity
+    {
+    }
+}
