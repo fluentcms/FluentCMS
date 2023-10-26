288d3bffa49adee09b8b80705fecf2f610ecbb6b
Parsa Gachkar <parsagachkar@gmail.com>
2023-10-26 18:32:44 +0330

    fix warnings

--- src/FluentCMS.Domain/Entities/ContentTypes/ContentTypeField.cs
+++ src/FluentCMS.Domain/Entities/ContentTypes/ContentTypeField.cs
@@ -1,14 +1,14 @@
 ﻿namespace FluentCMS.Entities.ContentTypes;
 
-public record ContentTypeField(string typeId, Type Type)
+public record ContentTypeField(string TypeId, Type Type)
 {
-    public static ContentTypeField Text = new("text", typeof(string));
-    public static ContentTypeField Number = new("number", typeof(decimal));
-    public static ContentTypeField Date = new("date", typeof(DateOnly));
-    public static ContentTypeField DateTime = new("date-time", typeof(DateTime));
-    public static ContentTypeField Bool = new("bool", typeof(bool));
+    public readonly static ContentTypeField Text = new("text", typeof(string));
+    public readonly static ContentTypeField Number = new("number", typeof(decimal));
+    public readonly static ContentTypeField Date = new("date", typeof(DateOnly));
+    public readonly static ContentTypeField DateTime = new("date-time", typeof(DateTime));
+    public readonly static ContentTypeField Bool = new("bool", typeof(bool));
 
-    public ContentTypeField FindById(string id)
+    public static ContentTypeField FindById(string id)
     {
         return id switch
         {
