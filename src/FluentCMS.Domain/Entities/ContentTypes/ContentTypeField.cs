e5113b2bf7010282664e6e1f19f0bde97d138cac
Parsa Gachkar <parsagachkar@gmail.com>
2023-10-26 18:41:44 +0330

    Add Field Settings

--- src/FluentCMS.Domain/Entities/ContentTypes/ContentTypeField.cs
+++ src/FluentCMS.Domain/Entities/ContentTypes/ContentTypeField.cs
@@ -1,23 +1,7 @@
-﻿namespace FluentCMS.Entities.ContentTypes;
+﻿
 
-public record ContentTypeField(string TypeId, Type Type)
+namespace FluentCMS.Entities.ContentTypes;
+
+public record ContentTypeField(FieldType FieldType,bool Required, string? Regex)
 {
-    public readonly static ContentTypeField Text = new("text", typeof(string));
-    public readonly static ContentTypeField Number = new("number", typeof(decimal));
-    public readonly static ContentTypeField Date = new("date", typeof(DateOnly));
-    public readonly static ContentTypeField DateTime = new("date-time", typeof(DateTime));
-    public readonly static ContentTypeField Bool = new("bool", typeof(bool));
-
-    public static ContentTypeField FindById(string id)
-    {
-        return id switch
-        {
-            "text" => Text,
-            "number" => Number,
-            "date" => Date,
-            "date-time" => DateTime,
-            "bool" => Bool,
-            _ => throw new IndexOutOfRangeException()
-        };
-    }
 }
---
+++ src/FluentCMS.Domain/Entities/ContentTypes/FieldType.cs
@@ -0,0 +1,23 @@
+﻿namespace FluentCMS.Entities.ContentTypes;
+
+public record FieldType(string TypeId, Type Type)
+{
+    public readonly static FieldType Text = new("text", typeof(string));
+    public readonly static FieldType Number = new("number", typeof(decimal));
+    public readonly static FieldType Date = new("date", typeof(DateOnly));
+    public readonly static FieldType DateTime = new("date-time", typeof(DateTime));
+    public readonly static FieldType Bool = new("bool", typeof(bool));
+
+    public static FieldType FindById(string id)
+    {
+        return id switch
+        {
+            "text" => Text,
+            "number" => Number,
+            "date" => Date,
+            "date-time" => DateTime,
+            "bool" => Bool,
+            _ => throw new IndexOutOfRangeException()
+        };
+    }
+}
