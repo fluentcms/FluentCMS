86a590ca258c0519e4f6a7d29ec8b4a5aa763c16
Parsa Gachkar <parsagachkar@gmail.com>
2023-10-29 16:54:38 +0330

    Implement #18

--- src/FluentCMS.Domain/Entities/ContentTypes/ContentTypeField.cs
+++ src/FluentCMS.Domain/Entities/ContentTypes/ContentTypeField.cs
@@ -2,18 +2,33 @@
 
 public class ContentTypeField
 {
-    public ContentTypeField(string title, FieldType fieldType, IDictionary<string, string> options)
+    public ContentTypeField(
+        string title,
+        FieldType fieldType,
+        IDictionary<string, string> options = null,
+        string description = "",
+        string label = "",
+        bool hidden = false,
+        object? defaultValue = null)
     {
         Title = title;
+        Description = description;
+        Label = label;
+        Hidden = hidden;
+        DefaultValue = defaultValue;
         FieldType = fieldType;
-        Options = options;
+        Options = options??new Dictionary<string,string>();
     }
     protected ContentTypeField()
     {
 
     }
 
-    public string Title { get; private set; } = "";
+    public string Title { get; set; } = "";
+    public string Description { get; set; } = "";
+    public string Label { get; set; } = "";
+    public bool Hidden { get; set; }
+    public object? DefaultValue { get; }
     public FieldType FieldType { get; private set; } = FieldType.Text;
     public IDictionary<string, string> Options { get; private set; } = new Dictionary<string, string>();
 }
