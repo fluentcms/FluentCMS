e96d04bd952563f4722a24a86535285ebc31d160
Parsa Gachkar <parsagachkar@gmail.com>
2023-10-30 21:02:58 +0330

    Implement Site Repository

---
+++ src/FluentCMS.Domain/Repository/ISiteRepository.cs
@@ -0,0 +1,12 @@
+﻿using FluentCMS.Entities.Sites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCMS.Repository;
+public interface ISiteRepository:IGenericRepository<Site>
+{
+    Task<Site> GetByUrl(string url);
+}
---
+++ src/FluentCMS.Repository.LiteDb/LiteDbSiteRepository.cs
@@ -0,0 +1,20 @@
+﻿using FluentCMS.Entities;
+using FluentCMS.Entities.Sites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCMS.Repository.LiteDb;
+public class LiteDbSiteRepository : LiteDbGenericRepository<Site>, ISiteRepository
+{
+    public LiteDbSiteRepository(LiteDbContext dbContext) : base(dbContext)
+    {
+    }
+
+    public Task<Site> GetByUrl(string url)
+    {
+        return Collection.FindOneAsync(x => x.Urls.Contains(url));
+    }
+}
