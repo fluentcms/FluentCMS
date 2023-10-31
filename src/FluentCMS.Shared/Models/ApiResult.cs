cedc1b991773a772f81e1569fac36e7fcd6789d0
Mostafa Rowghanian <mostafatech@gmail.com>
2023-10-31 12:28:21 +0330

    add users list to blazor ui app

---
+++ src/FluentCMS.Shared/Models/ApiResult.cs
@@ -0,0 +1,9 @@
+﻿namespace FluentCMS.Models;
+
+public class ApiResult<TData>
+{
+    public TData? Data { get; set; }
+    public string? ErrorType { get; set; }
+    public ICollection<string> Errors { get; set; } = new List<string>();
+    public bool IsSuccess { get => !Errors.Any(); }
+}
