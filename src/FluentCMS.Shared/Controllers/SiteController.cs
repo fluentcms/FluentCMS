cce6f1992862f022bd9558ecdf847870bc602ec1
Parsa Gachkar <parsagachkar@gmail.com>
2023-10-31 14:56:25 +0330

    implement Site List

--- src/FluentCMS.Shared/Controllers/SiteController.cs
+++ src/FluentCMS.Shared/Controllers/SiteController.cs
@@ -3,53 +3,54 @@ using FluentCMS.Entities.Sites;
 using FluentCMS.Web.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using MediatR;
+using FluentCMS.Models;
 
 namespace FluentCMS.Shared.Controllers;
 public class SiteController(IMediator mediator) : BaseController
 {
     //GetAll
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Site>>> GetAll()
+    public async Task<ActionResult<ApiResult<IEnumerable<Site>>>> GetAll()
     {
-        return Ok(await mediator.Send(new GetSitesQuery()));
+        return SucessResult(await mediator.Send(new GetSitesQuery()));
     }
 
     //GetById
     [HttpGet("{id}")]
-    public async Task<ActionResult<Site?>> GetById(Guid id)
+    public async Task<ActionResult<ApiResult<Site>>> GetById(Guid id)
     {
-        return Ok(await mediator.Send(new GetSiteByIdQuery { Id = id }));
+        return SucessResult(await mediator.Send(new GetSiteByIdQuery { Id = id }));
     }
 
-    [HttpGet]
+    [HttpGet("[action]")]
     //GetByUrl
-    public async Task<ActionResult<Site?>> GetByUrl([FromQuery] string url)
+    public async Task<ActionResult<ApiResult<Site>>> GetByUrl([FromQuery] string url)
     {
-        return Ok(await mediator.Send(new GetSiteByUrlQuery { Url = url }));
+        return SucessResult(await mediator.Send(new GetSiteByUrlQuery { Url = url }));
     }
 
     //Create
     [HttpPost()]
-    public async Task<ActionResult<Guid>> Create(CreateSiteCommand request)
+    public async Task<ActionResult<ApiResult<Guid>>> Create(CreateSiteCommand request)
     {
-        return Ok(await mediator.Send(request));
+        return SucessResult(await mediator.Send(request));
     }
 
     //Update
     [HttpPatch]
-    public async Task<ActionResult<Guid>> Update(EditSiteCommand request)
+    public async Task<ActionResult<ApiResult<Guid>>> Update(EditSiteCommand request)
     {
-        return Ok(await mediator.Send(request));
+        return SucessResult(await mediator.Send(request));
     }
 
     [HttpDelete]
     //Delete
-    public async Task<ActionResult<Guid>> Delete(Guid id)
+    public async Task<ActionResult<ApiResult<Guid>>> Delete(Guid id)
     {
-        return Ok(await mediator.Send(new DeleteSiteCommand { Id = id }));
+        return SucessResult(await mediator.Send(new DeleteSiteCommand { Id = id }));
     }
 
-    [HttpPut]
+    [HttpPut("[action]")]
     //AddUrl
     public async Task<ActionResult> AddUrl(AddSiteUrlCommand request)
     {
@@ -57,7 +58,7 @@ public class SiteController(IMediator mediator) : BaseController
         return Ok();
     }
 
-    [HttpDelete]
+    [HttpDelete("[action]")]
     //RemoveUrl
     public async Task<ActionResult> RemoveUrl(RemoveSiteUrlCommand request)
     {
