6d9386659dd095a6a2b0d392a72d67bf0b00a525
Mostafa Rowghanian <mostafatech@gmail.com>
2023-10-31 19:00:04 +0330

    add create and edit user pages and delete

---
+++ src/FluentCMS.Application/Users/EditUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace FluentCMS.Application.Users;
+public class EditUserCommand : IRequest
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
