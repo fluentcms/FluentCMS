f76bc8f60bc61c1afcc8be755c61bf6ea7fba917
Parsa Gachkar <parsagachkar@gmail.com>
2023-11-01 15:13:48 +0330

    Implement Repository

---
+++ src/FluentCMS.Domain/Repository/IPageRepository.cs
@@ -0,0 +1,12 @@
+﻿using FluentCMS.Entities.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCMS.Repository;
+public interface IPageRepository:IGenericRepository<Page>
+{
+    public Task<Page> FindByPath(string path);
+}
---
+++ src/FluentCMS.Repository.LiteDb/LiteDbPageRepository.cs
@@ -0,0 +1,19 @@
+﻿using FluentCMS.Entities.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCMS.Repository.LiteDb;
+internal class LiteDbPageRepository : LiteDbGenericRepository<Page>, IPageRepository
+{
+    public LiteDbPageRepository(LiteDbContext dbContext) : base(dbContext)
+    {
+    }
+
+    public Task<Page> FindByPath(string path)
+    {
+        return Collection.FindOneAsync(x => x.Path == path);
+    }
+}
