4e199f488df065c4601a651f802e416ac577fe98
Parsa Gachkar <parsagachkar@gmail.com>
2023-11-06 17:41:37 +0330

    Implement Edit and Delete for Page

--- src/FluentCMS.Services/PageService.cs
+++ src/FluentCMS.Services/PageService.cs
@@ -1,4 +1,5 @@
 ﻿using FluentCMS.Entities.Pages;
+using FluentCMS.Entities.Sites;
 using FluentCMS.Repositories.Abstractions;
 
 namespace FluentCMS.Services;
@@ -8,6 +9,8 @@ public interface IPageService
     Task<IEnumerable<Page>> GetBySiteId(Guid siteId, CancellationToken cancellationToken = default);
     Task<Page> GetById(Guid id, CancellationToken cancellationToken = default);
     Task<Page> Create(Page page, CancellationToken cancellationToken = default);
+    Task<Page> Edit(Page page, CancellationToken cancellationToken = default);
+    Task Delete(Page page, CancellationToken cancellationToken = default);
 }
 
 public class PageService : IPageService
@@ -37,16 +40,45 @@ public class PageService : IPageService
         // Except for the first site, which is created by the system
 
         // normalizing the page path to lowercase
-        page.Path = page.Path.ToLower();
-
-        // check if the page path is unique
-        var samePathPage = await _pageRepository.GetByPath(page.Path);
-        if (samePathPage != null)
-        {
-            throw new ApplicationException("Page path must be unique");
-        }
+        NormalizePath(page);
+        await CheckForDuplicatePath(page);
 
         var newPage = await _pageRepository.Create(page, cancellationToken);
         return newPage ?? throw new Exception("Page not created");
+    }
+
+    private async Task CheckForDuplicatePath(Page page)
+    {
+        // check if the page path is unique
+        var samePathPage = await _pageRepository.GetByPath(page.Path);
+        if (samePathPage != null && samePathPage?.Id != page.Id)
+        {
+            throw new ApplicationException("Page path must be unique");
+        }
+    }
+
+    public async Task<Page> Edit(Page page, CancellationToken cancellationToken = default)
+    {
+        // TODO: check permissions, only admins can create a page
+        // Except for the first site, which is created by the system
+
+        // normalizing the page path to lowercase
+        NormalizePath(page);
+
+        // check if the page path is unique
+        await CheckForDuplicatePath(page);
+
+        var newPage = await _pageRepository.Update(page, cancellationToken);
+        return newPage ?? throw new Exception("Page not created");
+    }
+
+    private static void NormalizePath(Page page)
+    {
+        page.Path = page.Path.ToLower();
+    }
+
+    public Task Delete(Page page, CancellationToken cancellationToken = default)
+    {
+        return _pageRepository.Delete(page.Id);
     }
 }
