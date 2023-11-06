92aea78c72abd8f2a2ed81ec18a7739406b7c89c
Parsa Gachkar <parsagachkar@gmail.com>
2023-11-06 14:43:19 +0330

    clean up unused files

--- src/FluentCMS.Domain/Repository/IPageRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿using FluentCMS.Entities.Pages;
-
-namespace FluentCMS.Repository;
-public interface IPageRepository : IGenericRepository<Page>
-{
-    public Task<Page> FindByPath(string path);
-}
