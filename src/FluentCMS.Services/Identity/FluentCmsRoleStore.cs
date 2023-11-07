d2cd99ae030959fe5d68a99d500c3190d125ff29
Parsa Gachkar <parsagachkar@gmail.com>
2023-11-07 18:52:04 +0330

    Add Role Store

---
+++ src/FluentCMS.Services/Identity/FluentCmsRoleStore.cs
@@ -0,0 +1,66 @@
+﻿using FluentCMS.Entities.Users;
+using Microsoft.AspNetCore.Identity;
+
+namespace FluentCMS.Services.Identity;
+
+public class FluentCmsRoleStore : IRoleStore<Role>
+{
+    public FluentCmsRoleStore()
+    {
+
+    }
+    public Task<IdentityResult> CreateAsync(Role role, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IdentityResult> DeleteAsync(Role role, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dispose()
+    {
+        
+    }
+
+    public Task<Role?> FindByIdAsync(string roleId, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Role?> FindByNameAsync(string normalizedRoleName, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string?> GetNormalizedRoleNameAsync(Role role, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> GetRoleIdAsync(Role role, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string?> GetRoleNameAsync(Role role, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SetNormalizedRoleNameAsync(Role role, string? normalizedName, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SetRoleNameAsync(Role role, string? roleName, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IdentityResult> UpdateAsync(Role role, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
