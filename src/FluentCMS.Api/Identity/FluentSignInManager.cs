745bd85eaef0bf3cf0e317baed473443296148f7
Parsa Gachkar <parsagachkar@gmail.com>
2023-11-07 18:54:39 +0330

    Implement Signin Manager

---
+++ src/FluentCMS.Api/Identity/FluentSignInManager.cs
@@ -0,0 +1,14 @@
+﻿using FluentCMS.Entities.Users;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace FluentCMS.Api.Identity;
+public class FluentSignInManager : SignInManager<User>
+{
+    public FluentSignInManager(FluentUserManager userManager, IHttpContextAccessor contextAccessor, IUserClaimsPrincipalFactory<User> claimsFactory, IOptions<IdentityOptions> optionsAccessor, ILogger<FluentSignInManager> logger, IAuthenticationSchemeProvider schemes, IUserConfirmation<User> confirmation) : base(userManager, contextAccessor, claimsFactory, optionsAccessor, logger, schemes, confirmation)
+    {
+    }
+}
