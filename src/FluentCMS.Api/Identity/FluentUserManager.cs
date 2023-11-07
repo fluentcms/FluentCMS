0dbac28ed4a08181131b9a162dabfb48aceee4f4
Parsa Gachkar <parsagachkar@gmail.com>
2023-11-07 18:54:20 +0330

    Implement User Manager

---
+++ src/FluentCMS.Api/Identity/FluentUserManager.cs
@@ -0,0 +1,14 @@
+﻿using FluentCMS.Entities.Users;
+using FluentCMS.Services.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace FluentCMS.Api.Identity;
+
+public class FluentUserManager : UserManager<User>
+{
+    public FluentUserManager(FluentCmsUserStore store, IOptions<IdentityOptions> optionsAccessor, IPasswordHasher<User> passwordHasher, IEnumerable<IUserValidator<User>> userValidators, IEnumerable<IPasswordValidator<User>> passwordValidators, ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors, IServiceProvider services, ILogger<FluentUserManager> logger) : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger)
+    {
+    }
+}
