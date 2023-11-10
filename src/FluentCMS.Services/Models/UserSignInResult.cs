14dc92110ff08c77575e84dcbe71b74a4efc8f1f
Amir Pournasserian <ap@momentaj.com>
2023-11-10 11:34:57 -0500

    #130 delete unused file/class

--- src/FluentCMS.Services/Models/UserSignInResult.cs
+++
@@ -1,8 +0,0 @@
-﻿//namespace FluentCMS.Services.Models;
-
-//public class UserSignInResult
-//{
-//    public Guid UserId { get; set; }
-//    public List<Guid> RoleIds { get; set; } = [];
-//    public string? Token { get; set; }
-//}
