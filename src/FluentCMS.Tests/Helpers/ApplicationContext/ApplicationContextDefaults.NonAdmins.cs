2717e716597deb55f964459141f77a4947b50fcd
Parsa Gachkar <parsagachkar@gmail.com>
2023-11-14 12:57:01 +0330

    Refactor AppContext Tests

---
+++ src/FluentCMS.Tests/Helpers/ApplicationContext/ApplicationContextDefaults.NonAdmins.cs
@@ -0,0 +1,27 @@
+﻿using FluentCMS.Entities;
+
+namespace FluentCMS.Tests.Helpers.ApplicationContext;
+
+
+    public static partial class ApplicationContextDefaults
+    {
+        public static class NonAdmins
+        {
+
+            public static readonly Role TestRole = new Role
+            {
+                Id = Guid.NewGuid(),
+                Name = "TestRole",
+            };
+            public static readonly User TestUser = new User
+            {
+                //fill user data
+                Id = Guid.NewGuid(),
+                UserName = "Test",
+                RoleIds = new() { TestRole.Id },
+
+            };
+        }
+    }
+
+
---
+++ src/FluentCMS.Tests/Helpers/ApplicationContext/ApplicationContextDefaults.cs
@@ -0,0 +1,24 @@
+﻿using FluentCMS.Entities;
+
+namespace FluentCMS.Tests.Helpers.ApplicationContext;
+
+
+    public static partial class ApplicationContextDefaults
+    {
+        public static readonly Host DefaultHost = new Host
+        {
+            Id = Guid.NewGuid(),
+            CreatedAt = DateTime.UtcNow,
+            CreatedBy = "TestSuperAdmin",
+            LastUpdatedAt = DateTime.UtcNow,
+            LastUpdatedBy = "TestSuperAdmin",
+            SuperUsers = { "TestSuperAdmin" }
+        };
+        public static readonly Site DefaultSite = new()
+        {
+            Name = "DefaultSite",
+            AdminRoleIds = new[] {Admins.TestAdminRole.Id, SuperAdmins.TestSuperAdminUser.Id }.ToList(),
+        };
+    }
+
+
