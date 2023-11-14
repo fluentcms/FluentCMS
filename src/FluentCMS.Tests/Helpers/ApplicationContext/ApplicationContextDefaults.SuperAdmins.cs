a004f54f1760322b819762e629bf85ff663931f1
Parsa Gachkar <parsagachkar@gmail.com>
2023-11-14 14:39:00 +0330

    cleanup styling

--- src/FluentCMS.Tests/Helpers/ApplicationContext/ApplicationContextDefaults.SuperAdmins.cs
+++ src/FluentCMS.Tests/Helpers/ApplicationContext/ApplicationContextDefaults.SuperAdmins.cs
@@ -2,26 +2,21 @@
 
 namespace FluentCMS.Tests.Helpers.ApplicationContext;
 
-
-    public static partial class ApplicationContextDefaults
+public static partial class ApplicationContextDefaults
+{
+    public static class SuperAdmins
     {
-        public static class SuperAdmins
+        public static readonly Role TestSuperAdminRole = new()
         {
-
-            public static readonly Role TestSuperAdminRole = new Role
-            {
-                Id = Guid.NewGuid(),
-                Name = "TestSuperAdminRole",
-            };
-            public static readonly User TestSuperAdminUser = new User
-            {
-                //fill user data
-                Id = Guid.NewGuid(),
-                UserName = "TestSuperAdmin",
-                RoleIds = new() { TestSuperAdminRole.Id },
-
-            };
-        }
+            Id = Guid.NewGuid(),
+            Name = "TestSuperAdminRole",
+        };
+        public static readonly User TestSuperAdminUser = new()
+        {
+            //fill user data
+            Id = Guid.NewGuid(),
+            UserName = "TestSuperAdmin",
+            RoleIds = [TestSuperAdminRole.Id],
+        };
     }
-
-
+}
