0ce914d25848648172493b6e0e8bd3ffa940f53c
Parsa Gachkar <parsagachkar@gmail.com>
2023-11-14 22:31:11 +0330

    update mapping to include path

--- src/FluentCMS.Api/MappingProfiles.cs
+++ src/FluentCMS.Api/MappingProfiles.cs
@@ -34,13 +34,13 @@ public class MappingProfiles : Profile
         //});
         CreateMap<List<Page>, List<PageResponse>>().ConstructUsing((x, ctx) =>
         {
-            return MapPagesWithParentId(x, ctx, null,"/");
+            return MapPagesWithParentId(x, ctx, null,"");
             static List<PageResponse> MapPagesWithParentId(List<Page> x, ResolutionContext ctx, Guid? parentId, string pathPrefix)
             {
                 var items = ctx.Mapper.Map<List<PageResponse>>(x.Where(x => x.ParentId == parentId));
                 foreach (var item in items)
                 {
-                    item.Path = pathPrefix + item.Path;
+                    item.Path = string.Join("/", pathPrefix , item.Path);
                     item.Children = MapPagesWithParentId(x, ctx, item.Id, item.Path);
                 }
                 return items.ToList();
