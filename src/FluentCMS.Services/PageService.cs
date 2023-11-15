2581a97d579115c816553deb3a13963c7280387c
Parsa Gachkar <parsagachkar@gmail.com>
2023-11-15 13:56:44 +0330

    match styling with host service

--- src/FluentCMS.Services/PageService.cs
+++ src/FluentCMS.Services/PageService.cs
@@ -1,5 +1,6 @@
 ﻿using FluentCMS.Entities;
 using FluentCMS.Repositories.Abstractions;
+using Microsoft.IdentityModel.Tokens;
 
 namespace FluentCMS.Services;
 
@@ -23,11 +24,13 @@ public class PageService : BaseService<Page>, IPageService
 
     public async Task<Page> Create(Page page, CancellationToken cancellationToken = default)
     {
+        // Block users with roles lower than "SiteAdmin" from creating a page
         if (!Current.IsSiteAdmin)
             throw new AppPermissionException();
 
         PrepareForCreate(page);
 
+        // this could be an async validation if we're using FluentValidator
         await BlockDuplicatePath(page);
 
         return await _pageRepository.Create(page, cancellationToken) ?? throw new AppException(ExceptionCodes.PageUnableToCreate);
@@ -35,7 +38,11 @@ public class PageService : BaseService<Page>, IPageService
 
     public async Task Delete(Page page, CancellationToken cancellationToken = default)
     {
-        if (IsPageAdmin(page))
+        // make sure that we are checking permissions from database, not the data that user has sent
+        var previousPage = await _pageRepository.GetById(page.Id, cancellationToken) ?? throw new AppException(ExceptionCodes.PageNotFound);
+
+        // block users with roles lower than "PageAdmin" from deleting a page
+        if (IsPageAdmin(previousPage))
             throw new AppPermissionException();
 
         _ = await _pageRepository.Delete(page.Id, cancellationToken) ?? throw new AppException(ExceptionCodes.PageUnableToDelete);
@@ -46,8 +53,13 @@ public class PageService : BaseService<Page>, IPageService
     {
         var page = await _pageRepository.GetById(id, cancellationToken) ?? throw new AppException(ExceptionCodes.PageNotFound);
 
-        if (!Current.IsInRole(page.ViewRoleIds) && !Current.IsSiteAdmin)
+        // check if the page has restricted view roles, and if the user is not in those roles, and user is not a site admin
+        if (!page.ViewRoleIds.IsNullOrEmpty() &&
+            !Current.IsInRole(page.ViewRoleIds) &&
+            !Current.IsSiteAdmin)
+        {
             throw new AppPermissionException();
+        }
 
         return page;
     }
@@ -56,16 +68,20 @@ public class PageService : BaseService<Page>, IPageService
     {
         var pages = await _pageRepository.GetBySiteId(siteId, cancellationToken) ?? throw new AppException(ExceptionCodes.PageNotFound);
 
-        return pages.Where(x => Current.IsInRole(x.ViewRoleIds) || Current.IsSiteAdmin);
+        // filter pages based on the user roles
+        // the page does not have a view role, or if the user is in those roles, or user is a site admin
+        return pages.Where(x => !x.ViewRoleIds.IsNullOrEmpty() || Current.IsInRole(x.ViewRoleIds) || Current.IsSiteAdmin);
     }
 
     public async Task<Page> Update(Page page, CancellationToken cancellationToken = default)
     {
+        // make sure that we are checking permissions from database, not the data that user has sent
         var previousPage = await _pageRepository.GetById(page.Id, cancellationToken) ?? throw new AppException(ExceptionCodes.PageNotFound);
 
         if (!IsPageAdmin(previousPage))
-            throw new AppPermissionException(); // we should check previous roles
+            throw new AppPermissionException();
 
+        // this could be an async validation if we're using FluentValidator
         await BlockDuplicatePath(page);
 
         PrepareForUpdate(page);
@@ -75,8 +91,10 @@ public class PageService : BaseService<Page>, IPageService
 
     private async Task BlockDuplicatePath(Page page)
     {
+        // find a page with the same path
         var matchingPath = await _pageRepository.GetByPath(page.Path);
 
+        // if there's a page with the same path and it's not the page we're updating
         if (matchingPath != null && matchingPath.Id != page.Id)
         {
             throw new AppException(ExceptionCodes.PagePathMustBeUnique);
