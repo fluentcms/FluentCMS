337a5d1b681647dce42743963995e7151e2c1451
Parsa Gachkar <parsagachkar@gmail.com>
2023-11-27 17:50:58 +0330

    Refactor Login to use Cookie based Authorization

---
+++ src/FluentCMS.Web.UI/Pages/Auth/LoginPage.razor.cs
@@ -0,0 +1,61 @@
+using Amazon.Runtime.Internal.Endpoints.StandardLibrary;
+using FluentCMS.Api.Models;
+using FluentCMS.Services.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace FluentCMS.Web.UI.Pages.Auth;
+public partial class LoginPage:ComponentBase
+{
+    [Inject] HttpClient HttpClient { get; set; }
+    [Inject] NavigationManager NavigationManager { get; set; }
+    [Inject] ProtectedLocalStorage ProtectedLocalStorage { get; set; }
+    [Inject] IHttpContextAccessor HttpContextAccessor { get; set; }
+    public bool Remember { get; set; }
+    [SupplyParameterFromForm]
+    public UserAuthenticateRequest UserAuthenticateRequest { get; set; } = new()
+    {
+        Username = "",
+        Password = ""
+    };
+    public async Task Submit()
+    {
+        //curl - X 'POST' \
+        //  'https://localhost:7164/api/Account/Authenticate' \
+        //  -H 'accept: application/json' \
+        //  -H 'Content-Type: application/json' \
+        //  -d '{
+        //  "username": "string",
+        //  "password": "string"
+        //}'
+        var response = await HttpClient.PostAsJsonAsync<UserAuthenticateRequest>("/api/Account/Authenticate", UserAuthenticateRequest);
+        if (response.IsSuccessStatusCode)
+        {
+            var userAuthenticationResult = await response.Content.ReadFromJsonAsync<ApiResult<UserAuthenticateDto>>();
+            // persist auth data
+            if (userAuthenticationResult?.Data != null)
+            {
+                //await ProtectedLocalStorage.SetAsync("access-token", userAuthenticationResult.Data.Token);
+                //await ProtectedLocalStorage.SetAsync("user-id", userAuthenticationResult.Data.UserId);
+                //await ProtectedLocalStorage.SetAsync("role-ids", userAuthenticationResult.Data.RoleIds);
+                HttpContextAccessor.HttpContext!.Response.Cookies.Append("access-token", userAuthenticationResult.Data.Token);
+                HttpContextAccessor.HttpContext!.Response.Cookies.Append("user-id", JsonSerializer.Serialize(userAuthenticationResult.Data.UserId));
+                HttpContextAccessor.HttpContext!.Response.Cookies.Append("role-ids", JsonSerializer.Serialize(userAuthenticationResult.Data.RoleIds));
+            }
+            NavigationManager.NavigateTo("/");
+        }
+    }
+
+}
+
+public class NonPrerenderedServerMode : InteractiveServerRenderMode
+{
+    public NonPrerenderedServerMode():base(false)
+    {
+        
+    }
+}
