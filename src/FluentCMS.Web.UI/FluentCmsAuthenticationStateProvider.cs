fb131c508975bba4c938a0d6962a76b643c71cf7
Parsa Gachkar <parsagachkar@gmail.com>
2023-11-30 12:04:07 +0330

    implment AuthenticationStateProvider

---
+++ src/FluentCMS.Web.UI/FluentCmsAuthenticationStateProvider.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.IdentityModel.JsonWebTokens;
+using System.Security.Claims;
+
+namespace FluentCMS.Web.UI;
+
+public class FluentCmsAuthenticationStateProvider : AuthenticationStateProvider
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public FluentCmsAuthenticationStateProvider(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        // fetch access-token from cookies
+        var accessToken = _httpContextAccessor.HttpContext?.Request.Cookies["access-token"];
+
+        // if no access-token in cookies
+        if (string.IsNullOrEmpty(accessToken))
+        {
+            //fail auth
+            return Task.FromResult(new AuthenticationState(new ClaimsPrincipal()));
+        }
+
+        // get claims from access-token
+        var claims = new JsonWebToken(accessToken).Claims;
+
+        // authenticated with access-token
+        return Task.FromResult(new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(claims, "access-token"))));
+    }
+}
