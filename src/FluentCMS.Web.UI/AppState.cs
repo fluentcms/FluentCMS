62484a0fdd5c1bf4e4f4f5e5c2e0b18fb81a31c0
Amir Pournasserian <ap@momentaj.com>
2023-11-30 08:51:05 -0500

    #243 trying some other approaches for the dynamic rendering engine

--- src/FluentCMS.Web.UI/AppState.cs
+++ src/FluentCMS.Web.UI/AppState.cs
@@ -1,12 +1,50 @@
 ﻿using FluentCMS.Api.Models;
 using FluentCMS.Entities;
+using System.Web;
+using Microsoft.AspNetCore.Components;
 
 namespace FluentCMS.Web.UI;
 
 public class AppState
 {
+    private readonly HttpClient _http;
+    
     public SiteResponse? Site { get; set; }
     public PageResponse? Page { get; set; }
     public Layout? Layout { get; set; }
     public string? Route { get; set; }
+    public Uri? Uri { get; set; }
+    public string Host { get; set; }
+
+    public AppState(HttpClient http, NavigationManager navigation)
+    {
+        Host = navigation.BaseUri;
+        if (Host.EndsWith("/"))
+            Host = Host.Substring(0, Host.Length - 1);
+        _http = http;
+    }
+
+    public async Task OnNavigateAsync(string? path)
+    {
+        if (path == null)
+            return;
+
+        Route = path;
+
+        var siteResult = await _http.GetFromJsonAsync<ApiResult<SiteResponse>>($"Site/GetByUrl?url={Host}");
+        Site = siteResult?.Data;
+        Layout = Site?.Layout;
+
+        if (Site != null)
+        {
+            var query = HttpUtility.ParseQueryString(string.Empty);
+            query["siteId"] = Site.Id.ToString();
+            query["path"] = Route;
+
+            var pageResult = await _http.GetFromJsonAsync<ApiResult<PageResponse>>($"Page/GetByPath?{query}");
+            Page = pageResult?.Data;
+            if (Page != null && Page.Layout != null)
+                Layout = Page.Layout;
+        }
+    }
 }
--- src/FluentCMS.Web.UI/UIServiceExtensions.cs
+++ src/FluentCMS.Web.UI/UIServiceExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Components;
 using System.Web;
 using FluentCMS.Web.UI;
+using Microsoft.AspNetCore.Components.Routing;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -16,33 +17,69 @@ public static class UIServiceExtensions
 
         services.AddCascadingValue(sp =>
         {
-            var appState = new AppState();
-
             var navigation = sp.GetRequiredService<NavigationManager>();
             var http = sp.GetRequiredService<HttpClient>();
-
-            var host = navigation.BaseUri;
-            if (host.EndsWith("/"))
-                host = host.Substring(0, host.Length - 1);
-
-            var siteResult = http.GetFromJsonAsync<ApiResult<SiteResponse>>($"Site/GetByUrl?url={host}").GetAwaiter().GetResult();
-            appState.Site = siteResult?.Data;
-            appState.Layout = appState.Site?.Layout;
-
-            if (appState.Site != null)
-            {
-                var query = HttpUtility.ParseQueryString(string.Empty);
-                query["siteId"] = appState.Site.Id.ToString();
-                query["path"] = "";
-
-                var pageResult = http.GetFromJsonAsync<ApiResult<PageResponse>>($"Page/GetByPath?{query}").GetAwaiter().GetResult();
-                appState.Page = pageResult?.Data;
-                if (appState.Page != null && appState.Page.Layout != null)
-                    appState.Layout = appState.Page.Layout;
-            }
+            var appState = new AppState(http, navigation);
             return appState;
         });
+
+        //services.AddScoped<AppState>();
+
+        //services.AddCascadingValue(sp =>
+        //{
+        //    var navigation = sp.GetRequiredService<NavigationManager>();
+
+        //    var appState = new AppState
+        //    {
+        //        Uri = navigation.ToAbsoluteUri(navigation.Uri)
+        //    };
+
+        //    var host = navigation.BaseUri;
+        //    if (host.EndsWith("/"))
+        //        host = host.Substring(0, host.Length - 1);
+
+        //    var http = sp.GetRequiredService<HttpClient>();
+
+        //    var siteResult = http.GetFromJsonAsync<ApiResult<SiteResponse>>($"Site/GetByUrl?url={host}").GetAwaiter().GetResult();
+        //    appState.Site = siteResult?.Data;
+        //    appState.Layout = appState.Site?.Layout;
+
+        //    if (appState.Site != null)
+        //    {
+        //        var query = HttpUtility.ParseQueryString(string.Empty);
+        //        query["siteId"] = appState.Site.Id.ToString();
+        //        query["path"] = appState.Uri.LocalPath;
+
+        //        var pageResult = http.GetFromJsonAsync<ApiResult<PageResponse>>($"Page/GetByPath?{query}").GetAwaiter().GetResult();
+        //        appState.Page = pageResult?.Data;
+        //        if (appState.Page != null && appState.Page.Layout != null)
+        //            appState.Layout = appState.Page.Layout;
+        //    }
+        //    return appState;
+        //});
+
+        //services.AddTransient<AppNavigation>();
 
         return services;
     }
 }
+//public class AppNavigation:IDisposable
+//{
+//    private readonly NavigationManager _navigationManager;
+
+//    public AppNavigation(NavigationManager navigationManager)
+//    {        
+//        _navigationManager = navigationManager;
+//        _navigationManager.LocationChanged += OnLocationChanged;
+//    }
+//    public void OnLocationChanged(object sender, LocationChangedEventArgs e)
+//    {
+//        string navigationMethod = e.IsNavigationIntercepted ? "HTML" : "code";
+//        System.Diagnostics.Debug.WriteLine($"Notified of navigation via {navigationMethod} to {e.Location}");
+//    }
+
+//    public void Dispose()
+//    {
+//        _navigationManager.LocationChanged -= OnLocationChanged;
+//    }
+//}
