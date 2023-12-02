02340463d9ef2f2e7ac06057234786957a229d5c
Amir Pournasserian <ap@momentaj.com>
2023-12-02 09:56:05 -0500

    #271 initial test for dynamic content

---
+++ src/FluentCMS.Api/Controllers/ContentController.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FluentCMS.Api.Models;
+using FluentCMS.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FluentCMS.Api.Controllers;
+
+public class ContentController(IMapper mapper) : BaseController
+{
+
+    [HttpPost]
+    public async Task<IApiResult<Content>> Create(Content content, CancellationToken cancellationToken = default)
+    {
+        await Task.CompletedTask;
+        return new ApiResult<Content>(content);
+    }
+}
---
+++ src/FluentCMS.Entities/Content.cs
@@ -0,0 +1,35 @@
+﻿namespace FluentCMS.Entities;
+
+public class Content : Dictionary<string, object>, IAuditEntity
+{
+    public Guid Id { get; set; } = default!;
+    public string CreatedBy { get; set; } = string.Empty; // UserName
+    public DateTime CreatedAt { get; set; }
+    public string LastUpdatedBy { get; set; } = string.Empty; // UserName
+    public DateTime LastUpdatedAt { get; set; }
+    public Guid TypeId { get; set; }
+
+}
+
+public class ContentType : AuditEntity
+{
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public IEnumerable<ContentTypeField> Fields { get; set; } = [];
+}
+
+public class ContentTypeField
+{
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public string Label { get; set; } = default!;
+    public string? Placeholder { get; set; }
+    public string? Hint { get; set; }
+    public string? DefaultValue { get; set; }
+    public bool IsRequired { get; set; }
+}
+public class ContentField
+{
+    public string Name { get; set; } = default!;
+    public string Value { get; set; } = default!;
+}
