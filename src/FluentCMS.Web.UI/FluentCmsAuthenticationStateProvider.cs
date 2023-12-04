61f2b16c9bc5fb366755758983bf713674648d73
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-04 15:56:13 +0330

    refactor Authentication State provider

--- src/FluentCMS.Web.UI/FluentCmsAuthenticationStateProvider.cs
+++ src/FluentCMS.Web.UI/FluentCmsAuthenticationStateProvider.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿using FluentCMS.Api.Models;
+using FluentCMS.Services.Models;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.VisualBasic;
 using System.Security.Claims;
 
 namespace FluentCMS.Web.UI;
@@ -7,10 +10,12 @@ namespace FluentCMS.Web.UI;
 public class FluentCmsAuthenticationStateProvider : AuthenticationStateProvider
 {
     private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly HttpClient _httpClient;
 
-    public FluentCmsAuthenticationStateProvider(IHttpContextAccessor httpContextAccessor)
+    public FluentCmsAuthenticationStateProvider(IHttpContextAccessor httpContextAccessor, HttpClient httpClient)
     {
         _httpContextAccessor = httpContextAccessor;
+        _httpClient = httpClient;
     }
     public override Task<AuthenticationState> GetAuthenticationStateAsync()
     {
@@ -29,5 +34,40 @@ public class FluentCmsAuthenticationStateProvider : AuthenticationStateProvider
 
         // authenticated with access-token
         return Task.FromResult(new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(claims, "access-token"))));
+    }
+
+    // Authenticate
+    public async Task<(bool result,List<Error> errors)> Authenticate(UserAuthenticateRequest request)
+    {
+        // TODO: NSwag generated Clients are Causing AntiForgeryToken related Errors
+        var result = await _httpClient.PostAsJsonAsync("/api/Account/Authenticate", request);
+
+        // check http status
+        if (result.IsSuccessStatusCode)
+        {
+            var authResult = await result.Content.ReadFromJsonAsync<ApiResult<UserAuthenticateDto>>(); // parse
+
+            // check authResult is not null
+            if (authResult is null)
+            {
+                return (false, []);
+            }
+
+            // check authResult Errors is empty
+            if (authResult.Errors is not null && authResult.Errors.Count > 0)
+            {
+                return (false,authResult.Errors);
+            }
+
+            // set access-token
+            _httpContextAccessor.HttpContext!.Response.Cookies.Append("access-token", authResult.Data!.Token, new CookieOptions
+            {
+                // TODO: Add Expiration to result
+            });
+            
+            return (true, []);
+        }
+        
+        return (false, []);
     }
 }
