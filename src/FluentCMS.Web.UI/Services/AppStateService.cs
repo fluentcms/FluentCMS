0b5e6c50fcf61b417e5260d2dca8f2c44abc01ce
Amir Pournasserian <ap@momentaj.com>
2023-12-10 10:34:19 -0500

    #302 enable InteractiveRender in some Counter and TextHtml plugins

--- src/FluentCMS.Web.UI/Services/AppStateService.cs
+++ src/FluentCMS.Web.UI/Services/AppStateService.cs
@@ -9,37 +9,44 @@ public class AppStateService(
     SiteClient siteClient,
     PageClient pageClient)
 {
-    public async Task<AppState> GetAppState(CancellationToken cancellationToken = default)
+    public async Task<AppState?> GetAppState(CancellationToken cancellationToken = default)
     {
-        var appState = new AppState
+        try
         {
-            Host = navigator.BaseUri.Remove(navigator.BaseUri.Length - 1),
-            Uri = new Uri(navigator.Uri)
-        };
+            var appState = new AppState
+            {
+                Host = navigator.BaseUri.Remove(navigator.BaseUri.Length - 1),
+                Uri = new Uri(navigator.Uri)
+            };
 
-        appState.Site = await siteClient.GetByUrl(appState.Host, cancellationToken);
-        appState.Layout = appState.Site?.Layout;
+            appState.Site = await siteClient.GetByUrl(appState.Host, cancellationToken);
+            appState.Layout = appState.Site?.Layout;
 
-        if (appState.Site != null)
-        {
-            var query = HttpUtility.ParseQueryString(string.Empty);
-            query["siteId"] = appState.Site.Id.ToString();
-            query["path"] = appState.Uri.LocalPath;
-            appState.Page = await pageClient.GetByPath(appState.Site.Id, appState.Uri.LocalPath, cancellationToken);
+            if (appState.Site != null)
+            {
+                var query = HttpUtility.ParseQueryString(string.Empty);
+                query["siteId"] = appState.Site.Id.ToString();
+                query["path"] = appState.Uri.LocalPath;
+                appState.Page = await pageClient.GetByPath(appState.Site.Id, appState.Uri.LocalPath, cancellationToken);
+            }
+
+            if (appState.Page != null && appState.Page.Layout != null)
+                appState.Layout = appState.Page.Layout;
+
+            var queryStrs = HttpUtility.ParseQueryString(appState.Uri.Query);
+
+            if (queryStrs["PluginId"] != null && Guid.TryParse(queryStrs["PluginId"], out var pluginId))
+                if (pluginId != Guid.Empty)
+                    appState.PluginId = pluginId;
+
+            if (!string.IsNullOrEmpty(queryStrs["ViewMode"]))
+                appState.ViewMode = queryStrs["ViewMode"];
+
+            return appState;
         }
-
-        if (appState.Page != null && appState.Page.Layout != null)
-            appState.Layout = appState.Page.Layout;
-
-        var queryStrs = HttpUtility.ParseQueryString(appState.Uri.Query);
-
-        if (queryStrs["PluginId"] != null && Guid.TryParse(queryStrs["PluginId"], out var pluginId))
-            if (pluginId != Guid.Empty)
-                appState.PluginId = pluginId;
-
-        if (!string.IsNullOrEmpty(queryStrs["ViewMode"]))
-            appState.ViewMode = queryStrs["ViewMode"];
-
-        return appState;
+        catch
+        {
+            return null;
+        }
     }
 }
