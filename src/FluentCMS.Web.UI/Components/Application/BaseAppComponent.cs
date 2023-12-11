0b6a4ef7f060fb588b54ab3541c4d18f538e7737
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-11 13:26:30 +0330

    Implement `OnPreRendering`

--- src/FluentCMS.Web.UI/Components/Application/BaseAppComponent.cs
+++ src/FluentCMS.Web.UI/Components/Application/BaseAppComponent.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Caching.Memory;
+using System.Reflection;
 
 namespace FluentCMS.Web.UI.Components.Application;
 
@@ -6,4 +8,50 @@ public class BaseAppComponent : ComponentBase
 {
     [CascadingParameter]
     public AppState AppState { get; set; } = default!;
+    [Inject]
+    public IHttpContextAccessor HttpContextAccessor { get; set; }
+    [Inject]
+    public IMemoryCache MemoryCache { get; set; }
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        // if is server-side prerendering
+        if (!HttpContextAccessor.HttpContext!.Response.HasStarted)
+        {
+            // call OnPreRendering to run any server-side prerendering logic
+            await OnPreRendering();
+
+            // cache final state of CachedValues
+            CacheValues();
+        }
+        else
+        {
+            // restore final state of CachedValues
+            RestoreValues();
+        }
+    }
+    public virtual async Task OnPreRendering()
+    {
+        await Task.CompletedTask;
+    }
+    public virtual void CacheValues()
+    {
+        var properties = GetType().GetProperties().Where(p => p.GetCustomAttribute<MemoryCachedValueAttribute>() != null);
+        foreach (var property in properties)
+        {
+            MemoryCache.Set($"{GetType().FullName}.{property.Name}", property.GetValue(this), new MemoryCacheEntryOptions()
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(30)
+            });
+        }
+    }
+    public virtual void RestoreValues()
+    {
+        var properties = GetType().GetProperties().Where(p => p.GetCustomAttribute<MemoryCachedValueAttribute>() != null);
+        foreach (var property in properties)
+        {
+            property.SetValue(this, MemoryCache.Get($"{GetType().FullName}.{property.Name}"));
+        }
+    }
 }
