12589d00861beb2eec1aec675ab1c6e8997872d0
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-13 12:38:29 +0330

    Define `IconsDefininion` Enum

---
+++ src/FluentCMS.Web.UI/Resources/IconDefinition.cs
@@ -0,0 +1,14 @@
+﻿namespace FluentCMS.Web.UI.Resources;
+
+// Enum Class for Icons
+public class IconDefinition
+{
+    // home icon
+    public static readonly IconDefinition Home = new IconDefinition("home");
+    private IconDefinition(string iconName)
+    {
+        IconName = iconName;
+    }
+
+    public string IconName { get; }
+}
