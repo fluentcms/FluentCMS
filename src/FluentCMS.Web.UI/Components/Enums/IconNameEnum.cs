75926670e893800b484329145b87a55537433698
Hadi Ahmadi <TheHadiAhmadi@gmail.com>
2023-12-13 14:26:51 +0430

    #321 Add Icon component

---
+++ src/FluentCMS.Web.UI/Components/Enums/IconNameEnum.cs
@@ -0,0 +1,13 @@
+namespace FluentCMS.Web.UI.Components.Core;
+
+public enum IconNameEnum
+{
+    Default,
+    Plus,
+    Delete,
+    InformationFilled,
+    Settings,
+    Filter,
+    Search,
+    Edit,
+}
