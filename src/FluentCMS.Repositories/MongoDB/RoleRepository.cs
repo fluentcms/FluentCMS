e7c7ac2ee188b0522521c34332f30a9b3602bce2
Amir Pournasserian <ap@momentaj.com>
2023-12-16 18:35:29 -0500

    #386 coding enhancement for RoleRepository

--- src/FluentCMS.Repositories/MongoDB/RoleRepository.cs
+++ src/FluentCMS.Repositories/MongoDB/RoleRepository.cs
@@ -2,10 +2,20 @@
 
 namespace FluentCMS.Repositories.MongoDB;
 
-public class RoleRepository(
-    IMongoDBContext mongoDbContext,
-    IApplicationContext applicationContext) :
-    SiteAssociatedRepository<Role>(mongoDbContext, applicationContext),
-    IRoleRepository
+/// <summary>
+/// Repository for managing Role entities in MongoDB.
+/// Extends SiteAssociatedRepository to handle roles associated with specific sites in a multi-tenant environment.
+/// </summary>
+public class RoleRepository : SiteAssociatedRepository<Role>, IRoleRepository
 {
+    /// <summary>
+    /// Initializes a new instance of the RoleRepository class.
+    /// </summary>
+    /// <param name="mongoDbContext">The MongoDB context used to access the database.</param>
+    /// <param name="applicationContext">The application context for the repository.</param>
+    public RoleRepository(IMongoDBContext mongoDbContext, IApplicationContext applicationContext)
+        : base(mongoDbContext, applicationContext)
+    {
+    }
+
 }
