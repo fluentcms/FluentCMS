764bd8bf9f20ff27f43306015f03f8f53ed0ceb9
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-18 00:59:35 +0330

    Implement Test Http Client Factory

---
+++ src/FluentCMS.E2eTests/E2eHttpClientFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace FluentCMS.E2eTests;
+
+internal class E2eHttpClientFactory<TStartup> : IHttpClientFactory
+    where TStartup : class
+{
+    private readonly WebApplicationFactory<TStartup> _appFactory;
+
+    public E2eHttpClientFactory(WebApplicationFactory<TStartup> appFactory) => _appFactory = appFactory;
+
+    public HttpClient CreateClient(string name) => _appFactory.CreateClient();
+}
