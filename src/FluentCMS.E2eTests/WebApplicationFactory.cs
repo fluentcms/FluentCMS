0c81cf3346ddf140b4fa9774b8406fce65d784c0
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-18 00:59:06 +0330

    Customise WebApplicationFactory Services

---
+++ src/FluentCMS.E2eTests/WebApplicationFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Net.Http.Headers;
+
+namespace FluentCMS.E2eTests;
+internal class WebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        base.ConfigureWebHost(builder);
+        builder.ConfigureServices(services =>
+        {
+            // replace HttpClientFactory with E2eHttpClientFactory
+            var httpClientFactoryDescriptor = services.First(x => x.ServiceType == typeof(IHttpClientFactory));
+            services.Remove(httpClientFactoryDescriptor);
+            services.AddSingleton<IHttpClientFactory>(new E2eHttpClientFactory<Program>(this));
+        });
+    }
+}
