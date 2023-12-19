50d643c10cd7b5b285a0f3554cf091c76651c25a
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-19 18:30:27 +0330

    Implement Authorized E2E base

---
+++ src/FluentCMS.E2eTests/AuthorizedBaseE2eTest.cs
@@ -0,0 +1,20 @@
+﻿using FluentCMS.Web.UI.ApiClients;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FluentCMS.E2eTests;
+
+public class AuthorizedBaseE2eTest<TInterface, TImplementation> : BaseE2eTest<TInterface, TImplementation>
+        where TInterface : IApiClient
+        where TImplementation : class, TInterface
+{
+    protected async Task Login(string userName = "superadmin", string password = "Passw0rd!")
+    {
+        var scope = WebUi.Services.CreateScope();
+        var accountClient = scope.ServiceProvider.GetRequiredService<AccountClient>();
+
+        var resp = await accountClient.LoginAsync(new UserLoginRequest() { Username = userName, Password = password });
+
+        E2eHttpClientFactory<Program>.OverrideToken = resp.Data.Token;
+    }
+
+}
