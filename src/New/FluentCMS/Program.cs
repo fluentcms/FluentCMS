0faf8bd77bef993ce29552c597c83b0e3fe5bdc9
Amir Pournasserian <ap@momentaj.com>
2023-12-20 16:13:20 -0500

    #434 extract Web.Api project

--- src/New/FluentCMS/Program.cs
+++ src/New/FluentCMS/Program.cs
@@ -1,25 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+#region Services
+
+var services = builder.Services;
+
+services.AddControllers();
+
+services.AddApiDocumentation();
+
+#endregion
+
+
+#region App
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
+
+app.UseApiDocumentation();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+#endregion
 
 app.Run();
