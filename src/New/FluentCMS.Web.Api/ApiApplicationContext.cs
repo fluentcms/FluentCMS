c1725fb134299446919203ea3771cdb1d3dea649
Amir Pournasserian <ap@momentaj.com>
2023-12-20 16:42:04 -0500

    #437 extract config folder

---
+++ src/New/FluentCMS.Web.Api/ApiApplicationContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FluentCMS.Web.Api;
+
+public class ApiApplicationContext : IApplicationContext
+{
+    private string _username;
+    private readonly IEnumerable<Guid> _roleIds;
+    private bool _isSuperAdmin;
+    private bool _isAuthenticated;
+
+    public ApiApplicationContext(IHttpContextAccessor httpContextAccessor)
+    {
+        _username = httpContextAccessor.HttpContext?.User?.Identity?.Name ?? string.Empty;
+        _roleIds = httpContextAccessor.HttpContext?.User?.Claims?.Where(x => x.Type == "role")?.Select(x => Guid.Parse(x.Value)) ?? Enumerable.Empty<Guid>();
+        _isSuperAdmin = httpContextAccessor.HttpContext?.User?.Claims?.Where(x => x.Type == "IsSuperAdmin")?.Select(x => bool.Parse(x.Value)).SingleOrDefault() ?? false;
+        _isAuthenticated = httpContextAccessor.HttpContext?.User?.Identity?.IsAuthenticated ?? false;
+    }
+
+    // TODO: This is a temporary workaround to set the super admin user into the context.
+    // It should be replaced with a proper authentication system in the future.
+    public void SetSuperAdmin(string username)
+    {
+        _username = username;
+        _isSuperAdmin = true;
+        _isAuthenticated = true;
+    }
+
+    public IEnumerable<Guid> RoleIds => _roleIds;
+    public string Username => _username;
+    public bool IsAuthenticated => _isAuthenticated;
+    public bool IsSuperAdmin => _isSuperAdmin;
+}
---
+++ src/New/FluentCMS.Web.Api/Extentions/ApiServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using FluentCMS;
+using FluentCMS.Web.Api;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ApiServiceExtensions
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+
+        services.AddAuthentication();
+
+        services.AddAuthorization();
+
+        services.AddHttpContextAccessor();
+
+        services.AddScoped<IApplicationContext, ApiApplicationContext>();
+
+        return services;
+    }
+}
