f853d443519627c272a8b1e8f69158bb18b6c45c
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-23 23:23:29 +0330

    fix conflicting tests by disabling paralel tests

--- src/New/Tests/FluentCMS.E2eTests/Extensions.cs
+++ src/New/Tests/FluentCMS.E2eTests/Extensions.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
 namespace FluentCMS.E2eTests;
 public static class Extensions
 {
