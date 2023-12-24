afee6d50d088ca985494aed1e4d3e1ba5676027b
Amir Pournasserian <amir.pournasserian@live.com>
2023-12-24 11:59:04 -0500

    #485 add Site related Entities

---
+++ src/New/FluentCMS.Entities/Site/Site.cs
@@ -0,0 +1,8 @@
+﻿namespace FluentCMS.Entities;
+public class Site : AuditableEntity
+{
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public List<string> Urls { get; set; } = [];
+    public Guid DefaultLayoutId { get; set; }
+}
