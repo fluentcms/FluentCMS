8a15030172ba7fd6e7f559d01f402afcd4c5eed8
Amir Pournasserian <ap@momentaj.com>
2023-12-24 18:08:33 -0500

    #485 add SiteContext in UI

--- src/New/FluentCMS.Web.UI/SiteContext.cs
+++ src/New/FluentCMS.Web.UI/SiteContext.cs
@@ -1,7 +1,31 @@
-﻿namespace FluentCMS.Web.UI;
+﻿using Microsoft.AspNetCore.Components;
+using System.Web;
+
+namespace FluentCMS.Web.UI;
 
 public class SiteContext
 {
     public SiteDetailResponse Site { get; set; } = default!;
-    public IEnumerable<PageDetailResponse> Pages { get; set; } = default!;
+    public PageDetailResponse Page { get; set; } = default!;
+
+    public SiteContext(NavigationManager navigator, SiteClient siteClient, PageClient pageClient)
+    {
+        var url = navigator.BaseUri.Remove(navigator.BaseUri.Length - 1);
+        var uri = new Uri(navigator.Uri);
+
+        var taskSite = Task.Run(() => siteClient.GetByUrlAsync(url));
+        taskSite.Wait();
+        Site = taskSite.Result.Data;
+
+        if (Site != null)
+        {
+            var query = HttpUtility.ParseQueryString(string.Empty);
+            query["siteId"] = Site.Id.ToString();
+            query["path"] = uri.LocalPath;
+
+            var taskPage = Task.Run(() => pageClient.GetByPathAsync(Site.Id, uri.LocalPath));
+            taskPage.Wait();
+            Page = taskPage.Result.Data;
+        }
+    }
 }
