bf4fb32855578c730ca91e33b5f664cd60a3e8b0
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-28 17:13:54 +0330

    feat: implement Given I Have Credentials

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/Given/GivenIHaveCredentials.cs
@@ -0,0 +1,18 @@
+﻿using FluentCMS.E2eTests.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow.Assist;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [Given("I have Credentials")]
+    public void GivenIHaveCredentials(Table table)
+    {
+        var credentials = table.CreateInstance<Credential>();
+        context.Set(credentials);
+    }
+}
