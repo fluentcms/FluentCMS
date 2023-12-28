33abe9794385703b95a0c46891d80a66c548d2c4
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-28 17:19:45 +0330

    feat: Implement "Then wait for n seconds"

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/Then/ThenWaitSeconds.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [Then("Wait {int} second(s)")]
+    public async Task ThenWaitSeconds(int seconds)
+    {
+        await Task.Delay(TimeSpan.FromSeconds(seconds));
+    }
+}
