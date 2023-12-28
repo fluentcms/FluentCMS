8870433f416d59938271d64d88794154402981be
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-28 17:16:59 +0330

    feat: Implement "Then Register Response Should be success"

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/Then/ThenRegisterResponseShouldBeSuccess.cs
@@ -0,0 +1,18 @@
+﻿using FluentCMS.E2eTests.ApiClients;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [Then("Register Response Should be success")]
+    public void ThenRegisterResponseShouldBeSuccess()
+    {
+        var registerResponse = context.Get<UserDetailResponseIApiResult>();
+        registerResponse.Errors.ShouldBeEmpty();
+    }
+}
