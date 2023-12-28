95b04c21ab654f2e0909a540782e25143162e8d3
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-28 17:14:35 +0330

    feat: implement @RequiresResetSetup tag

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/Tags/RequiresResetSetup.cs
@@ -0,0 +1,26 @@
+﻿using FluentCMS.E2eTests.ApiClients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [Before("RequiresResetSetup", Order = 10)]
+    public async Task RequiresResetSetup()
+    {
+        GivenIHaveAService("SetupClient");
+        await WhenIFetchSetupIsInitialized();
+        if (context.Get<BooleanIApiResult>(_isInitializedKey).Data)
+        {
+            await WhenIResetSetup();
+            await ThenWaitSeconds(2);
+            await WhenIFetchSetupIsInitialized();
+        }
+        ThenSetupInitializationStatusShouldBe("False");
+
+    }
+
+}
