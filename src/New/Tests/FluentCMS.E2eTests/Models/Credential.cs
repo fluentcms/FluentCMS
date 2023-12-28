1c4f91c319773ad3f7e2e70145729b5042daacb0
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-28 17:59:01 +0330

    fix: remove unused Model from test and reuse already available models from Client Generation

--- src/New/Tests/FluentCMS.E2eTests/Models/Credential.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FluentCMS.E2eTests.Models;
-
-public class Credential
-{
-    public string Email { get; set; } = string.Empty;
-    public string Username { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-}
