4087695f2c4b9c4bb3ef3e500cad029b588630c0
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-28 17:13:13 +0330

    feat: Implement a model to keep track of UserInfo

---
+++ src/New/Tests/FluentCMS.E2eTests/Models/Credential.cs
@@ -0,0 +1,8 @@
+﻿namespace FluentCMS.E2eTests.Models;
+
+public class Credential
+{
+    public string Email { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
