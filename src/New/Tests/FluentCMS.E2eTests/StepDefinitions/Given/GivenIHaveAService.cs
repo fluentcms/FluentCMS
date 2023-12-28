7c557a16193d7c1434a9f90370d47084aeb5f587
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-28 17:13:37 +0330

    feat: implement Given I Have Credentials

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/Given/GivenIHaveAService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [Given("I have a(n) {string}")]
+    public void GivenIHaveAService(string serviceName)
+    {
+        // find service type byName
+        var definedTypes = Assembly.GetAssembly(typeof(ClientServiceExtensions))!.DefinedTypes;
+        var serviceType = definedTypes.Single(x => x.Name == serviceName);
+        context[serviceType.FullName]=(context.GetServiceProvider().GetRequiredService(serviceType));
+    }
+}
