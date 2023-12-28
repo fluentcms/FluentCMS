25f603add809c0245641dfd9b59aca0b8fb6ac1c
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-28 17:22:52 +0330

    feat: Implement "When I Reset Setup"

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/When/WhenIResetSetup.cs
@@ -0,0 +1,16 @@
+﻿using FluentCMS.E2eTests.ApiClients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [When("I Reset Setup")]
+    public async Task WhenIResetSetup()
+    {
+        await context.Get<SetupClient>().ResetAsync();
+    }
+}
