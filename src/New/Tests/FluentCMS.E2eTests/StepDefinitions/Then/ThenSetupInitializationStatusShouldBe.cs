5636d7aa9b275e166d24a03fedfb6331f4118316
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-28 17:18:38 +0330

    feat: Implement "Then Setup initialization status should be {word}"

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/Then/ThenSetupInitializationStatusShouldBe.cs
@@ -0,0 +1,21 @@
+﻿using FluentCMS.E2eTests.ApiClients;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    private const string _isInitializedKey = "IsInitialized";
+
+    [Then("Setup initialization status should be {word}")]
+    public void ThenSetupInitializationStatusShouldBe(string statusStr)
+    {
+        var status = bool.Parse(statusStr);
+        var isInitialized = context.Get<BooleanIApiResult>(_isInitializedKey);
+        isInitialized.Data.ShouldBe(status);
+    }
+}
