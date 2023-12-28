03b2424fbe3329341239dd3330493d19353916ad
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-28 17:23:40 +0330

    feat: Implement "When I Start Setup again should throw Error"

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/When/WhenIStartSetupAgainShouldThrowError.cs
@@ -0,0 +1,19 @@
+﻿using FluentCMS.E2eTests.ApiClients;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [When("I Start Setup again should throw Error")]
+    public async Task WhenIStartSetupAgainShouldThrowError()
+    {
+        var setupClient = context.Get<SetupClient>();
+        var e = await (setupClient.StartAsync().ShouldThrowAsync<ApiClientException>());
+        // TODO: fetch and check error message
+    }
+}
