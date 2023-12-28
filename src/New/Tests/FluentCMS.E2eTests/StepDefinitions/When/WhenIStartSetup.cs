47916eff511bf5610cf96834a9133f1222a8e20d
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-28 17:23:15 +0330

    feat: Implement "When I Start Setup"

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/When/WhenIStartSetup.cs
@@ -0,0 +1,16 @@
+﻿using FluentCMS.E2eTests.ApiClients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [When("I Start Setup")]
+    public async Task WhenIStartSetup()
+    {
+        await context.Get<SetupClient>().StartAsync();
+    }
+}
