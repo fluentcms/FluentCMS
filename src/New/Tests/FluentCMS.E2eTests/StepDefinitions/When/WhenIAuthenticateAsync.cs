9fdb725029b69881232bce84873c774b299cc47f
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-28 17:20:35 +0330

    feat: Implement "When I Authenticate"

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/When/WhenIAuthenticateAsync.cs
@@ -0,0 +1,34 @@
+﻿using FluentCMS.E2eTests.ApiClients;
+using FluentCMS.E2eTests.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [When("I Authenticate")]
+    public async Task WhenIAuthenticateAsync()
+    {
+        var accountClient = context.Get<AccountClient>();
+        var credentials = context.Get<Credential>();
+        var authBody = new UserLoginRequest()
+        {
+            Username = credentials.Username,
+            Password = credentials.Password
+        };
+        try
+        {
+            var response = await accountClient.AuthenticateAsync(authBody);
+            context.Set(response);
+            context.Set(response.Errors);
+        }
+        catch (ApiClientException e)
+        {
+            // todo: fetch error messages
+            
+        }
+    }
+}
