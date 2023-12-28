aeffa83ab2af0389d0e76c259cdd469869c08414
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-28 17:19:20 +0330

    feat: Implement "Then user is returned"

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/Then/ThenUserIsReturned.cs
@@ -0,0 +1,15 @@
+using FluentCMS.E2eTests.ApiClients;
+using Shouldly;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [Then("user is returned")]
+    public void ThenUserIsReturned()
+    {
+        var user = context.Get<UserResponseIApiResult>();
+        user.Errors.ShouldBeEmpty();
+        user.Data.ShouldNotBeNull();
+        user.Data.Id.ShouldNotBe(default);
+    }
+}
