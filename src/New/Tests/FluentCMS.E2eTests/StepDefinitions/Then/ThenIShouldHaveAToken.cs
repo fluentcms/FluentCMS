5865d0c296bea41cfe4f1e75dd800a02affae8b3
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-28 17:15:57 +0330

    feat: Implement "Then I Should Have a Token"

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/Then/ThenIShouldHaveAToken.cs
@@ -0,0 +1,18 @@
+﻿using FluentCMS.E2eTests.ApiClients;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [Then("I Should Have a Token")]
+    public void ThenIShouldHaveAToken()
+    {
+        var authResponse = context.Get<UserLoginResponseIApiResult>();
+        authResponse.Data.Token.ShouldNotBeEmpty();
+    }
+}
