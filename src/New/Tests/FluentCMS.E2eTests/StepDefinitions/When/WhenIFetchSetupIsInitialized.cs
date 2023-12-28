0af4e999e792b7319d53f4b4e6e34d77672e814f
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-28 17:21:17 +0330

    feat: Implement "When I Fetch Setup IsInitialized"

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/When/WhenIFetchSetupIsInitialized.cs
@@ -0,0 +1,19 @@
+﻿using FluentCMS.E2eTests.ApiClients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [When("I Fetch Setup IsInitialized")]
+    public async Task WhenIFetchSetupIsInitialized()
+    {
+        var setupClient = context.Get<SetupClient>();
+        var status = await setupClient.IsInitializedAsync();
+        context[_isInitializedKey] = status;
+        context.Set(status.Errors);
+    }
+}
