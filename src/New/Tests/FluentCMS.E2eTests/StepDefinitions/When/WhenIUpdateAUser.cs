383118008057c1fd5ff46a42b267a5b3d14460ac
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-28 17:23:56 +0330

    feat: Implement "When I update a user"

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/When/WhenIUpdateAUser.cs
@@ -0,0 +1,18 @@
+using FluentCMS.E2eTests.ApiClients;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [When("I update a user")]
+    public async Task WhenIUpdateAUserAsync()
+    {
+        var userClient = context.Get<UserClient>();
+        var userId = context.Get<UserResponseIApiResult>().Data.Id;
+        var updateBody = new UserUpdateRequest() {
+            Id = userId,
+            Email = "UpdatedDummyEmail@localhost",
+        };
+        var result = await userClient.UpdateAsync(updateBody);
+        context.Set(result);
+    }
+}
