4e7b26d3f265536b206d8d3806be9249416679f7
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-28 17:15:03 +0330

    fix: Cleanup StepDefinitions

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/StepDefinitions.cs
@@ -0,0 +1,23 @@
+﻿using FluentCMS.E2eTests.ApiClients;
+using FluentCMS.E2eTests.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+[Binding]
+public partial class StepDefinitions(ScenarioContext context)
+{
+    [Before(Order = 0)]
+    public void RegisterServices()
+    {
+        var services = new ServiceCollection().ConfigureServices();
+        context[ScenarioContextExtensions.ServiceProviderKey] = services.BuildServiceProvider();
+    }
+
+}
