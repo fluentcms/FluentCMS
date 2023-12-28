e323d0795586abf153c224033c5e128b7b51daa0
Amir Pournasserian <amir.pournasserian@live.com>
2023-12-28 13:35:56 -0500

    #545 add plugin template to setup

--- src/New/FluentCMS.Web.Api/Setup/AdminTemplate.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FluentCMS.Web.Api.Setup;
-internal class AdminTemplate
-{
-    public Site Site { get; set; } = default!;
-    public List<Layout> Layouts { get; set; } = [];
-    public List<PluginDefinition> PluginDefinitions { get; set; } = [];
-    public List<PageTemplate> Pages { get; set; } = [];
-}
