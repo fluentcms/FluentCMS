45e1a99f89149f61ec6d587343315ffd1e6c4a2d
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-28 17:17:16 +0330

    feat: Implement "Then Response Errors Should be Empty"

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/Then/ThenResponseErrorsShouldBeEmpty.cs
@@ -0,0 +1,15 @@
+﻿using Shouldly;
+using System.Collections;
+using Error = FluentCMS.E2eTests.ApiClients.Error;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [Then("Response Errors Should be Empty")]
+    public void ThenResponseErrorsShouldBeEmpty()
+    {
+        var errors = context.Get<ICollection<Error>>();
+        errors.ShouldBeEmpty();
+    }
+
+}
