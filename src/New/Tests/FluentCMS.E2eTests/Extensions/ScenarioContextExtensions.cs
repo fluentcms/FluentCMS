d3d97af975c37c016bf1730c3e9bf488b5de13a6
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-28 17:07:42 +0330

    feat: Implement service provider extensions for ScenarioContext

---
+++ src/New/Tests/FluentCMS.E2eTests/Extensions/ScenarioContextExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+
+public static partial class ScenarioContextExtensions
+{
+    public const string ServiceProviderKey = "ServiceProvider";
+    public static ServiceProvider GetServiceProvider(this ScenarioContext context)
+    {
+        return context.Get<ServiceProvider>(ServiceProviderKey);
+    }
+
+}
