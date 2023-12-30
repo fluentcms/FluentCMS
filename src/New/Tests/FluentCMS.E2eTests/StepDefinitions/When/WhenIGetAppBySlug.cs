9ff4a13c2c4f22f35a148c8d963032db91d01958
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-30 17:20:49 +0330

    feat: implement When I Get App By Slug step

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/When/WhenIGetAppBySlug.cs
@@ -0,0 +1,16 @@
+using FluentCMS.E2eTests.ApiClients;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [When("I Get App By Slug {string}")]
+    public async Task WhenIGetAppBySlugAsync(string slug)
+    {
+        var appClient = context.Get<AppClient>();
+        context.Set(slug, "appSlug");
+        var app = await appClient.GetBySlugAsync(slug);
+        context.Set(app);
+
+
+    }
+}
