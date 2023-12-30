14375b2d6b6a46325d7d3c800e7892d6c9a47af9
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-30 17:17:00 +0330

    feat: implement Then App Create result should be Success step

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/Then/ThenAppCreateResultShouldBeSuccess.cs
@@ -0,0 +1,19 @@
+using FluentCMS.E2eTests.ApiClients;
+using Shouldly;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [Then("App Create result should be Success")]
+    public void ThenAppCreateResultShouldBeSuccess()
+    {
+        var appApiResponse = context.Get<AppResponseIApiResult>();
+
+        appApiResponse.Errors.ShouldBeEmpty();
+        appApiResponse.Data.ShouldNotBeNull();
+        appApiResponse.Data.Id.ShouldNotBe(default);
+
+        context.Set(appApiResponse);
+        context.Set(appApiResponse.Data.Id, "appId");
+    }
+}
