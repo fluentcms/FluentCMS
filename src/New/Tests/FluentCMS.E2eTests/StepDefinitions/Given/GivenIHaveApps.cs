10771abe8150bcfd5b81c93c40b70f141e995f33
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-30 17:15:27 +0330

    feat: implement GivenI have N Apps step

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/Given/GivenIHaveApps.cs
@@ -0,0 +1,23 @@
+using FluentCMS.E2eTests.ApiClients;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [Given("I have {int} Apps")]
+    public async Task GivenIHaveAppsAsync(int count)
+    {
+        var appsToCreate = Enumerable.Range(1, count).Select(x => new AppCreateRequest()
+        {
+            Title = $"DummyApp{x}",
+            Description = $"DummyApp{x} description",
+            Slug = $"dummy-app-{x}"
+        });
+        var appClient = context.Get<AppClient>();
+        foreach (var app in appsToCreate)
+        {
+            await appClient.CreateAsync(app);
+        }
+    }
+}
