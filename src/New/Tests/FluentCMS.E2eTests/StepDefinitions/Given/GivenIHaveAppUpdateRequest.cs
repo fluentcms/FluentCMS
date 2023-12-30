a7c268f3069191df12d77b2dfe53f662b49bc56a
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-30 17:16:38 +0330

    feat: implement Given I have App Update Request step

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/Given/GivenIHaveAppUpdateRequest.cs
@@ -0,0 +1,14 @@
+﻿using FluentCMS.E2eTests.ApiClients;
+using TechTalk.SpecFlow.Assist;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [Given("I have App Update Request")]
+    public void GivenIHaveAppUpdateRequest(Table table)
+    {
+        var appUpdateRequest = table.CreateInstance<AppUpdateRequest>();
+        context.Set(appUpdateRequest);
+    }
+
+}
