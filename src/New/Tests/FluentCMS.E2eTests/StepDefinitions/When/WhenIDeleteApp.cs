aa4a230d6df1e32ed44c9e1969e997b896704ab0
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-30 17:19:52 +0330

    feat: implement When I Delete App step

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/When/WhenIDeleteApp.cs
@@ -0,0 +1,14 @@
+using FluentCMS.E2eTests.ApiClients;
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [When("I Delete App")]
+    public async Task WhenIDeleteAppAsync()
+    {
+        var appId = context.Get<Guid>("appId");
+        var appClient = context.Get<AppClient>();
+        var result = await appClient.DeleteAsync(appId);
+        context.Set(result);
+    }
+}
