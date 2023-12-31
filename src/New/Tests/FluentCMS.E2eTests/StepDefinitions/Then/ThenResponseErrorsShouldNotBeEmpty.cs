674f8e4af45cfc9fe9df7d8c31be6d69931e8a17
Parsa Gachkar <parsagachkar@gmail.com>
2023-12-31 22:05:54 +0330

    fix: update naming for application error model

--- src/New/Tests/FluentCMS.E2eTests/StepDefinitions/Then/ThenResponseErrorsShouldNotBeEmpty.cs
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/Then/ThenResponseErrorsShouldNotBeEmpty.cs
@@ -4,7 +4,7 @@ public partial class StepDefinitions
     [Then("Response Errors Should not be Empty")]
     public void ThenResponseErrorsShouldNotBeEmpty()
     {
-        var errors = context.Get<ICollection<Error>>();
+        var errors = context.Get<ICollection<AppError>>();
         errors.ShouldNotBeEmpty();
     }
 }
