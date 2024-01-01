df01e97e70c4bf6cf885ed473cdb19658968fbfc
Parsa Gachkar <parsagachkar@gmail.com>
2024-01-02 00:19:56 +0330

    fix: add id to AppUpdateRequest

--- src/New/FluentCMS.Web.Api/Models/App/AppUpdateRequest.cs
+++ src/New/FluentCMS.Web.Api/Models/App/AppUpdateRequest.cs
@@ -2,6 +2,7 @@
 
 public class AppUpdateRequest
 {
+    public Guid Id { get; set; } = default!;
     public string Title { get; set; } = default!;
     public string? Description { get; set; }
     public string Slug { get; set; } = default!;
