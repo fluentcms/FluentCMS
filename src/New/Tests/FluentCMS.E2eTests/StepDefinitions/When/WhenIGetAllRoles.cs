e85db59555fd7ed5a1575384ae5b76c746c3e49b
Parsa Gachkar <parsagachkar@gmail.com>
2024-01-03 15:14:11 +0330

    feat: Implement  When I Get All Roles

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/When/WhenIGetAllRoles.cs
@@ -0,0 +1,13 @@
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+public partial class StepDefinitions
+{
+    [When("I Get All Roles")]
+    public async Task WhenIGetAllRolesAsync()
+    {
+        var app = context.Get<AppResponseIApiResult>();
+        var rolesClient = context.Get<RoleClient>();
+        var response = await rolesClient.GetAllAsync(app.Data.Slug!);
+        context.Set(response);
+    }
+}
