56a80de3b93f16144352afbb53b8f5ee88c4cb88
Parsa Gachkar <parsagachkar@gmail.com>
2024-01-04 15:48:59 +0330

    fix: implemented basic confirmservice functionality

--- src/FluentCMS.Web.UI/Components/Core/Confirm/ConfirmProvider.razor.cs
+++ src/FluentCMS.Web.UI/Components/Core/Confirm/ConfirmProvider.razor.cs
@@ -1,10 +1,23 @@
-﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components;
 
+namespace FluentCMS.Web.UI.Components.Core.Confirm;
 public partial class ConfirmProvider
 {
     [Parameter]
-    public string Message { get; set; } = "aaaaaa";
+    public string Message { get; set; } = "Are you sure?";
+    [Inject]
+    public ConfirmService ConfirmService { get; set; } = default!;
 
-    public void Info(string Message) {
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        ConfirmService.ConfirmProvider = this;
+    }
+    public bool IsOpen { get; set; } = false;
+    public void Show(string message)
+    {
+        Message = message;
+        IsOpen = true;
+        StateHasChanged();
     }
 }
---
+++ src/FluentCMS.Web.UI/Components/Core/Confirm/ConfirmService.cs
@@ -0,0 +1,10 @@
+﻿namespace FluentCMS.Web.UI.Components.Core.Confirm;
+
+public class ConfirmService
+{
+    public ConfirmProvider? ConfirmProvider { get; set; } = null;
+    public void Show(string message)
+    {
+        ConfirmProvider?.Show(message);
+    }
+}
