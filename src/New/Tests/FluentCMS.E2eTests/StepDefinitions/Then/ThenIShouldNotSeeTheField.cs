c02666243f7f5d383361dafd1b2a68bd6bc93adc
Parsa Gachkar <parsagachkar@gmail.com>
2024-01-08 18:31:29 +0330

    feat:Implement ThenIShouldNotSeeTheField

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/Then/ThenIShouldNotSeeTheField.cs
@@ -0,0 +1,26 @@
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+
+public partial class StepDefinitions
+{
+    [Then("I should not see the field")]
+    public async Task ThenIShouldNotSeeTheFieldAsync()
+    {
+        var client = context.Get<ContentTypeClient>();
+        var app = context.Get<AppResponseIApiResult>();
+
+        // fetch all content types
+        var result = await client.GetAllAsync(app.Data.Slug!);
+
+        // find the content type
+        var contentType = context.Get<ContentTypeResponseIApiResult>();
+
+        var updatedContentType = result.Data!.Single(x => x.Id == contentType.Data.Id);
+
+        // find the field
+        var field = updatedContentType.Fields!.SingleOrDefault(x => x.Slug == contentType.Data.Fields!.First().Slug);
+
+        // assert
+        field.ShouldBeNull();
+    }
+}
