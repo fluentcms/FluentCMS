b4d144b19d79ab28bbe93562c3b1e8ce898e6045
Parsa Gachkar <parsagachkar@gmail.com>
2024-01-08 18:31:22 +0330

    feat:Implement GivenIHaveContentTypes

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/Given/GivenIHaveContentTypes.cs
@@ -0,0 +1,27 @@
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+
+public partial class StepDefinitions
+{
+    [Given("I have {int} ContentTypes")]
+    public async Task GivenIHaveContentTypesAsync(int count)
+    {
+        var contentTypeClient = context.Get<ContentTypeClient>();
+
+        var app = context.Get<AppResponseIApiResult>();
+
+        var contentTypesToBeCreated = Enumerable.Range(1, count)
+            .Select(x => new ContentTypeCreateRequest()
+            {
+                Title = $"DummyContentType{x}",
+                Description = $"DummyContentType{x} description",
+                Slug = $"dummy-content-type-{x}",
+            });
+
+        // iterate and create contentTypesToBeCreated
+        foreach (var contentType in contentTypesToBeCreated)
+        {
+            await contentTypeClient.CreateAsync(app.Data.Slug, contentType);
+        }
+    }
+}
