4aca34306d5ac3ccd4187f1653428893d7fc51ff
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-01-08 10:47:21 +0330

    fix: update GivenIHaveRoles to accually create the roles (#616)

--- src/New/Tests/FluentCMS.E2eTests/StepDefinitions/Given/GivenIHaveRoles.cs
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/Given/GivenIHaveRoles.cs
@@ -1,14 +1,21 @@
+
 namespace FluentCMS.E2eTests.StepDefinitions;
 public partial class StepDefinitions
 {
     [Given("I have {int} Roles")]
-    public void GivenIHaveRoles(int count)
+    public async Task GivenIHaveRolesAsync(int count)
     {
         var rolesToCreate = Enumerable.Range(1, count).Select(x => new RoleCreateRequest()
         {
             Name = $"DummyRole{x}",
             Description = $"DummyRole{x} description"
         }).ToList();
+        var rolesClient = context.Get<RoleClient>();
+        var app = context.Get<AppResponseIApiResult>();
+        foreach (var role in rolesToCreate)
+        {
+            await rolesClient.CreateAsync(app.Data.Slug!, role);
+        }
         context.Set(rolesToCreate);
     }
 }
