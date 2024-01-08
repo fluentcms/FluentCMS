32adda467fc17e2b7cef2ad53c9d4bb6f9c8ff52
Parsa Gachkar <parsagachkar@gmail.com>
2024-01-08 18:31:40 +0330

    feat:Implement WhenIDeleteAContentType

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/When/WhenIDeleteAContentType.cs
@@ -0,0 +1,24 @@
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+
+public partial class StepDefinitions
+{
+    [When("I delete a content type")]
+    public async Task WhenIDeleteAContentTypeAsync()
+    {
+        // fetch content type from previous steps
+        var contentType = context.Get<ContentTypeResponseIApiResult>();
+
+        // delete content type
+        var app = context.Get<AppResponseIApiResult>();
+        var contentTypeClient = context.Get<ContentTypeClient>();
+
+        var deleteResult = await contentTypeClient.DeleteAsync(
+            app.Data.Slug!,
+            contentType.Data.Id);
+
+        deleteResult.Data.ShouldBeTrue();
+
+        context.Set(deleteResult);
+    }
+}
