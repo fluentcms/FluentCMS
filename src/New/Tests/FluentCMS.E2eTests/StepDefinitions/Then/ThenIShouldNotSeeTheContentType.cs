6dad4089314db5a4c092b34f7371b9129f67d0b6
Parsa Gachkar <parsagachkar@gmail.com>
2024-01-08 18:31:27 +0330

    feat:Implement ThenIShouldNotSeeTheContentType

---
+++ src/New/Tests/FluentCMS.E2eTests/StepDefinitions/Then/ThenIShouldNotSeeTheContentType.cs
@@ -0,0 +1,19 @@
+
+namespace FluentCMS.E2eTests.StepDefinitions;
+
+public partial class StepDefinitions
+{
+    [Then("I should not see the content type")]
+    public async Task ThenIShouldNotSeeTheContentTypeAsync()
+    {
+        var createResponse = context.Get<ContentTypeResponseIApiResult>();
+
+        var client = context.Get<ContentTypeClient>();
+
+        var app = context.Get<AppResponseIApiResult>();
+
+        var result = await client.GetAllAsync(app.Data.Slug!);
+
+        result.Data.ShouldNotContain(x => x.Slug == createResponse.Data.Slug);
+    }
+}
