4ba14af11b27ae0f21b8b2fbd4c7f25e9c9c6673
Parsa Gachkar <parsagachkar@gmail.com>
2024-01-08 20:55:10 +0330

    chore: rename `ContentTypeResponse` name to `ContentTypeDetailResponse`

--- src/New/FluentCMS.Web.Api/Models/ContentType/ContentTypeDetailResponse.cs
+++ src/New/FluentCMS.Web.Api/Models/ContentType/ContentTypeDetailResponse.cs
@@ -1,6 +1,6 @@
 ﻿namespace FluentCMS.Web.Api.Models;
 
-public class ContentTypeResponse : BaseAppAssociatedResponse
+public class ContentTypeDetailResponse : BaseAppAssociatedResponse
 {
     public string Slug { get; set; } = default!;
     public string Title { get; set; } = default!;
