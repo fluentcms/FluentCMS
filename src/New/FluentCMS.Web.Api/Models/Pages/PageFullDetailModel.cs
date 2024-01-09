042d743b24e9527a34b707b4c51ad25b34575b26
Parsa Gachkar <parsagachkar@gmail.com>
2024-01-09 22:09:08 +0330

    refactor: encapsulate necessary models into one object, AutoMapper only accepts one object as input!

---
+++ src/New/FluentCMS.Web.Api/Models/Pages/PageFullDetailModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FluentCMS.Web.Api.Models.Pages;
+
+public record PageFullDetailModel(
+    Page page,
+    string path,
+    Site site,
+    IEnumerable<Layout> layouts,
+    IEnumerable<Plugin> plugins,
+    Dictionary<Guid, PluginDefinition> pluginDefinitions,
+    bool forceMainSection)
+{
+
+}
