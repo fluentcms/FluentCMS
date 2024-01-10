c2cb583f2e51e9b98f2ecdcee45c2acc33cd89a9
Parsa Gachkar <parsagachkar@gmail.com>
2024-01-10 12:19:39 +0330

    feat: setup doc project

---
+++ src/New/FluentCms.Web.Ui.Components.Docs/Program.cs
@@ -0,0 +1,23 @@
+using FluentCms.Web.Ui.Components.Docs.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
