eee98b9b1ddace33b3a39b55f2bfe34cfddbdc2e
abdolian <m.abdolian@gmail.com>
2024-01-11 18:04:45 +0330

    chore: cleaned

--- src/New/FluentCMS.Web.UI.Components/Extensions/UIServiceExtensions.cs
+++ src/New/FluentCMS.Web.UI.Components/Extensions/UIServiceExtensions.cs
@@ -1,12 +1,8 @@
 ﻿using FluentCMS.Web.UI.Components.Resources;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Resources;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Microsoft.Extensions.DependencyInjection;
+
 public static class UIServiceExtensions
 {
     public static IServiceCollection AddFluentCmsUiComponents(this IServiceCollection services)
