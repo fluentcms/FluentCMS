ac23c0253eafe884eacd95346aa2a75cb0e89a4f
Masood Abdolian <m.abdolian@gmail.com>
2024-01-11 13:24:24 +0330

    638 setup doc project (#640)
    * feat: setup doc project
    
    * init
    
    * fix: string extension
    
    * fix: add style file
    
    * feat: Added primary tools
    
    * feat: Added icon
    style: Removed f-theme-dark dependency
    
    * fix: issue `dotnet run`
    
    ---------
    
    Co-authored-by: Parsa Gachkar <parsagachkar@gmail.com>

---
+++ src/New/FluentCMS.Web.UI.Components/Base/UISettings.cs
@@ -0,0 +1,8 @@
+namespace FluentCMS.Web.UI.Components;
+
+public static class UISettings
+{
+    public const string PREFIX = "f";
+
+    public const string SEPARATOR = "-";
+}
---
+++ src/New/FluentCMS.Web.UI.ComponentsDocs/Program.cs
@@ -0,0 +1,23 @@
+using FluentCMS.Web.UI.ComponentsDocs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
