f5d8e18e98c5b300d4d9e2756445baa2a600fd69
abdolian <m.abdolian@gmail.com>
2024-01-14 18:38:09 +0330

    chore: Clean up

---
+++ src/New/FluentCMS.Web.UI.Components/Components/Dropdown/Dropdown.razor.cs
@@ -0,0 +1,18 @@
+﻿namespace FluentCMS.Web.UI.Components;
+
+public enum DropdownPlacement
+{
+    Default,
+    Bottom,
+    BottomStart,
+    BottomEnd,
+    Top,
+    TopStart,
+    TopEnd,
+    Left,
+    LeftStart,
+    LeftEnd,
+    Right,
+    RightStart,
+    RightEnd,
+}
