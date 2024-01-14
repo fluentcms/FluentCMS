9b26e95d67efcbc41f6ebbde1ac17c4fcf36cd9d
abdolian <m.abdolian@gmail.com>
2024-01-14 12:45:13 +0330

    chore: Added confirm component

--- src/New/FluentCMS.Web.UI.Components/Extensions/UIServiceExtensions.cs
+++ src/New/FluentCMS.Web.UI.Components/Extensions/UIServiceExtensions.cs
@@ -1,4 +1,5 @@
-﻿using FluentCMS.Web.UI.Components.Resources;
+﻿using FluentCMS.Web.UI.Components;
+using FluentCMS.Web.UI.Components.Resources;
 using System.Resources;
 
 namespace Microsoft.Extensions.DependencyInjection;
@@ -7,11 +8,17 @@ public static class UIServiceExtensions
 {
     public static IServiceCollection AddFluentCmsUiComponents(this IServiceCollection services)
     {
-        services.AddKeyedScoped(typeof(Icons).FullName, (_, _) =>
-        {
-            var iconsType = typeof(Icons);
-            return new ResourceManager(iconsType.FullName!, iconsType.Assembly);
-        });
+        services.AddScoped<ConfirmService>();
+
+        services.AddKeyedScoped(
+            typeof(Icons).FullName,
+            (_, _) =>
+            {
+                var iconsType = typeof(Icons);
+                return new ResourceManager(iconsType.FullName!, iconsType.Assembly);
+            }
+        );
+
         return services;
     }
 }
