2a22771d8781d8b9487eae93baaee082475f3cf4
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-01-16 18:34:16 +0330

    chore: move `FluentCMS.Web.UI.ErrorHandling` to `New`

---
+++ src/New/FluentCMS.Web.UI.ErrorHandling/ErrorManager.cs
@@ -0,0 +1,37 @@
+﻿using FluentCMS.Web.UI.ErrorHandling;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCMS.Web.UI.Services;
+public class ErrorManager(IServiceProvider serviceProvider)
+{
+    public async Task HandleException(Exception ex)
+    {
+        var exceptionType = ex.GetType();
+        IErrorHandler? handler = null;
+        var explicitHandlerMethod = typeof(ErrorManager)
+            .GetMethod(nameof(FindExplicitHandler),System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)!
+            .MakeGenericMethod(ex.GetType());
+        handler = (IErrorHandler?) explicitHandlerMethod.Invoke(this, null);
+        if (handler == null)
+        {
+            handler = FindFallBackHandler();
+        }
+        await handler.HandleException(ex);
+    }
+
+    private IErrorHandler FindFallBackHandler()
+    {
+        return serviceProvider.GetRequiredService<IErrorHandler>();
+    }
+
+    private IErrorHandler<T>? FindExplicitHandler<T>()
+        where T : Exception
+    {
+        return serviceProvider.GetService<IErrorHandler<T>>();
+    }
+}
---
+++ src/New/FluentCMS.Web.UI.ErrorHandling/IErrorHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace FluentCMS.Web.UI.ErrorHandling;
+
+public interface IErrorHandler
+{
+    public Task HandleException(Exception ex);
+}
+
+public interface IErrorHandler<T> : IErrorHandler
+    where T : Exception
+{
+    public Task HandleException(T ex) => HandleException((Exception)ex);
+}
