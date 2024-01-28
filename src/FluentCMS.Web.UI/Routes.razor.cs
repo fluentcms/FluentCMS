0cac271c63e77a5cf897fb47984dea8e12f27c18
abdolian <m.abdolian@gmail.com>
2024-01-28 18:30:57 +0330

    feat: Added a naming route system

---
+++ src/FluentCMS.Web.UI/Routes.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace FluentCMS.Web.UI;
+
+public partial class Routes
+{
+    public static string AuthForgotPassword() => "/auth/forgot-password";
+
+    public static string AuthLogin() => "/auth/login";
+
+    public static string AuthLogout() => "/auth/logout";
+
+    public static string AuthRegister() => "/auth/register";
+
+    public static string Home() => "/";
+
+    public static string TermsOfUse() => "/terms-of-use";
+
+    public static string UserDetails(string Id) => string.Format("/user/$0", Id);
+
+    public static string PrivacyPolicy() => "/privacy-policy";
+}
