03c4a89b15d8a8bcea40d9eba7f465905f5795cc
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-02-26 02:37:32 +0330

    refactor: move login logic to backing file and modify it to use `AuthStateProvider` to avoid code duplication with `Setup`

---
+++ src/FluentCMS.Web.UI/Plugins/Auth/LoginViewPlugin.razor.cs
@@ -0,0 +1,24 @@
+﻿using FluentCMS.Web.UI.Services;
+using FluentCMS.Web.UI.Services.LocalStorage;
+using Microsoft.AspNetCore.Components;
+
+namespace FluentCMS.Web.UI.Plugins.Auth;
+
+public partial class LoginViewPlugin
+{
+    [Inject] private AuthStateProvider AuthStateProvider { get; set; } = default!;
+
+    [Inject] private NavigationManager NavigationManager { get; set; } = default!;
+
+    [Inject] private ILocalStorageService LocalStorageService { get; set; } = default!;
+    private UserLoginRequest Model { get; set; } = new();
+
+    private async Task OnSubmit()
+    {
+        var result = await AuthStateProvider.LoginAsync(Model);
+        if (result.Errors!.Count == 0)
+        {
+            NavigationManager.NavigateTo("/");
+        }
+    }
+}
