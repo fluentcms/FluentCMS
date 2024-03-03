fc3fa496ffd18cc71beca590d5070d49bc6d7469
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-03-03 12:11:05 +0330

    feat: add UserSendPasswordResetTokenRequest dto

---
+++ src/FluentCMS.Web.Api/Models/Users/UserSendPasswordResetTokenRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FluentCMS.Web.Api.Models.Users;
+
+public class UserSendPasswordResetTokenRequest
+{
+
+    [Required] public string Email { get; set; } = string.Empty;
+}
