f3c22987d4f80cbf58b6519df2d69e1609c1163d
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-03-04 18:24:28 +0430

    Add frontend of User profile section

---
+++ src/FluentCMS.Web.UI/Plugins/Auth/ProfileViewPlugin.razor.cs
@@ -0,0 +1,29 @@
+using FluentCMS.Web.UI.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace FluentCMS.Web.UI.Plugins.Auth;
+
+public partial class ProfileViewPlugin 
+{
+    public AuthenticationState? Model { get; set; } = default;
+    
+    [Inject] public AuthStateProvider AuthStateProvider { get; set; }
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        AuthStateProvider.AuthenticationStateChanged += AuthStateProviderOnAuthenticationStateChanged;
+
+        void AuthStateProviderOnAuthenticationStateChanged(Task<AuthenticationState> task)
+        {
+            Console.WriteLine(task.Result.User.Identity.IsAuthenticated);
+        }
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+        Model = await AuthStateProvider.GetAuthenticationStateAsync();
+        //StateHasChanged();
+    }
+    }
