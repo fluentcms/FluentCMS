6ca5ee7309f510275b5cd118cb6eb7c2b20a088c
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-03-05 14:57:44 +0330

    feat: add UpdateUserProfileDto

---
+++ src/FluentCMS.Web.Api/Models/Users/UserUpdateProfileRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FluentCMS.Web.Api.Models.Users;
+
+public class UserUpdateProfileRequest
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+}
