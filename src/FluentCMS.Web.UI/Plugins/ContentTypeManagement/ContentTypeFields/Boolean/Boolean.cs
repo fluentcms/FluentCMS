bfca63750fd774bc39f33b0e8346c3508a844d1f
abdolian <m.abdolian@gmail.com>
2024-03-17 15:42:19 +0330

    feat: Add boolean filed content type

---
+++ src/FluentCMS.Web.UI/Plugins/ContentTypeManagement/ContentTypeFields/Boolean/Boolean.cs
@@ -0,0 +1,18 @@
+﻿namespace FluentCMS.Web.UI.Plugins.ContentTypeManagement.ContentTypeFields;
+
+public class BooleanInfo : Base
+{
+    public static string Key = "boolean";
+
+    public static string Title = "Boolean";
+
+    public static string Description = "Yes or no, 1 or 0, true or false";
+
+    public static IconName Icon = IconName.Boolean;
+
+    public static int Order = 2;
+
+    public static Type BasicSettings = typeof(BooleanBasicSettings);
+
+    public static Type AdvancedSettings = typeof(BooleanAdvancedSettings);
+}
