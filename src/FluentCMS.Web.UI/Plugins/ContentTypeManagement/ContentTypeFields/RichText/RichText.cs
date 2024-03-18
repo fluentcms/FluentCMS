642974e4afadbf9d552bdb13c9a23f7a862d1217
abdolian <m.abdolian@gmail.com>
2024-03-18 14:00:53 +0330

    feat: Add rich text content type

---
+++ src/FluentCMS.Web.UI/Plugins/ContentTypeManagement/ContentTypeFields/RichText/RichText.cs
@@ -0,0 +1,18 @@
+﻿namespace FluentCMS.Web.UI.Plugins.ContentTypeManagement.ContentTypeFields;
+
+public class RichTextInfo : Base
+{
+    public static string Key = "rich-text";
+
+    public static string Title = "Rich Text";
+
+    public static string Description = "A rich text editor with formatting options";
+
+    public static IconName Icon = IconName.Paragraph;
+
+    public static int Order = 3;
+
+    public static Type BasicSettings = typeof(RichTextBasicSettings);
+
+    public static Type AdvancedSettings = typeof(RichTextAdvancedSettings);
+}
