b92b2f1a5073d8928c04541024c673d2136bfefb
Hadi Ahmadi <TheHadiAhmadi@gmail.com>
2024-03-24 12:27:07 +0430

    894 add email field type (#897)
    * feat: Add Email field type
    
    * add Email type for Input

---
+++ src/FluentCMS.Web.UI/Plugins/ContentTypeManagement/ContentTypeFields/Email/Email.cs
@@ -0,0 +1,18 @@
+namespace FluentCMS.Web.UI.Plugins.ContentTypeManagement.ContentTypeFields;
+
+public class EmailInfo : Base
+{
+    public static string Key = "email";
+
+    public static string Title = "Email";
+
+    public static string Description = "Email field with validations format";
+
+    public static IconName Icon = IconName.AtSign;
+
+    public static int Order = 6;
+
+    public static Type BasicSettings = typeof(EmailBasicSettings);
+
+    public static Type AdvancedSettings = typeof(EmailAdvancedSettings);
+}
