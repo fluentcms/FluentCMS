5900347cca2d451d86c17b846be98fd1f83be5ab
Hadi Ahmadi <TheHadiAhmadi@gmail.com>
2024-03-24 12:26:07 +0430

    feat: add Enumeration field type (#896)

---
+++ src/FluentCMS.Web.UI/Plugins/ContentTypeManagement/ContentTypeFields/Enumeration/Enumeration.cs
@@ -0,0 +1,18 @@
+namespace FluentCMS.Web.UI.Plugins.ContentTypeManagement.ContentTypeFields;
+
+public class EnumerationInfo : Base
+{
+    public static string Key = "enumeration";
+
+    public static string Title = "Enumeration";
+
+    public static string Description = "List of values, then pick one";
+
+    public static IconName Icon = IconName.List;
+
+    public static int Order = 7;
+
+    public static Type BasicSettings = typeof(EnumerationBasicSettings);
+
+    public static Type AdvancedSettings = typeof(EnumerationAdvancedSettings);
+}
