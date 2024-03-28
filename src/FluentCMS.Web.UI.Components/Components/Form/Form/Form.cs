72b5153b894b26c42e72ed80ae1fbf157d8d4937
Masood Abdolian <m.abdolian@gmail.com>
2024-03-28 11:12:47 +0330

    903 resolve technical debts in the design system and its documentation (#909)
    * chore: Move icon resource to the component directory.
    
    * chore: Clean the alert component.
    
    * chore: Move component scss files to their directories.
    
    * chore: Rename `CssProperty` attribute to `CSSProperty`.
    
    * chore: Update close button icon.
    
    * chore: Update styles.
    
    * chore: Update form inputs
    
    * fix: Fix confirm backdrop
    
    * chore: Move autocomplete component to form directory
    
    * chore: Fix null checking in FormField component
    
    * chore: rebuild styles
    
    * fix: usage of form field label
    
    * chore: update formfiled dependency

---
+++ src/FluentCMS.Web.UI.Components/Components/Form/Form/Form.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Components;
+
+namespace FluentCMS.Web.UI.Components;
+
+public abstract class FormElements : BaseComponent
+{
+    [Parameter]
+    public bool Dense { get; set; }
+
+    [Parameter]
+    [CSSProperty]
+    public bool Disabled { get; set; }
+
+    [Parameter]
+    public string? Hint { get; set; }
+
+    [Parameter]
+    public string? Id { get; set; }
+
+    [Parameter]
+    public string? Label { get; set; }
+
+    [Parameter]
+    public RenderFragment? LabelFragment { get; set; }
+
+    [Parameter]
+    public string? Name { get; set; }
+
+    [Parameter]
+    public bool Required { get; set; }
+}
+
+public abstract class FormElementsValuable<T> : FormElements
+{
+    protected T? CurrentValue { get; set; }
+
+    [Parameter]
+    public EventCallback<T> OnChange { get; set; }
+
+    [Parameter]
+    public T? Value
+    {
+        get => CurrentValue;
+        set
+        {
+            if (value != null && !value.Equals(CurrentValue))
+            {
+                CurrentValue = value;
+                ValueChanged.InvokeAsync(value);
+                OnChange.InvokeAsync(value);
+            }
+        }
+    }
+
+    [Parameter]
+    public EventCallback<T> ValueChanged { get; set; }
+}
+
+public abstract class FormCheckboxesType<T> : FormElementsValuable<T>
+{
+}
+
+public abstract class FormInputsType<T> : FormElementsValuable<T>
+{
+    [Parameter]
+    public IconName? IconEnd { get; set; }
+
+    [Parameter]
+    public IconName? IconStart { get; set; }
+
+    [Parameter]
+    public string? Placeholder { get; set; }
+}
+
+public enum FormFieldState
+{
+    Default,
+    Invalid,
+    Valid,
+}
--- src/FluentCMS.Web.UI.Components/Resources/Icons.cs
+++
@@ -1,5 +0,0 @@
-namespace FluentCMS.Web.UI.Components.Resources;
-
-public class Icons
-{
-}
