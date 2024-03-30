478bc1db5242cd07213f0931bed076707acc92f5
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-03-31 01:26:48 +0330

    feat: implment Dto models

---
+++ src/FluentCMS.Web.Api/Models/File/FileCreateRequest.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FluentCMS.Web.Api.Models.File;
+
+public class FileCreateRequest
+{
+    public string? Slug { get; set; } = null;
+    public IFormFile File { get; set; } = null;
+}
---
+++ src/FluentCMS.Web.Api/Models/File/FileDetailResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace FluentCMS.Web.Api.Models.File;
+
+public class FileDetailResponse
+{
+    public Guid Id { get; set; }
+    public string Slug { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Extension { get; set; } = string.Empty;
+    public string MimeType { get; set; } = string.Empty;
+    public long Size { get; set; } = 0;
+    public string CreatedBy { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+}
