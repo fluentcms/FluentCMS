74ad3d373ba46fd60cbdd238de756aeba10b9ae6
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-03-31 01:25:05 +0330

    feat: add default cancellationTokens to FileRepository interface

--- src/FluentCMS.Repositories.Abstractions/IFileRepository.cs
+++ src/FluentCMS.Repositories.Abstractions/IFileRepository.cs
@@ -1,6 +1,6 @@
 ﻿namespace FluentCMS.Repositories.Abstractions;
 public interface IFileRepository: IAuditableEntityRepository<Entities.File>
 {
-    public Task<Entities.File?> GetBySlug(string slug, CancellationToken cancellationToken);
-    public Task<Entities.File?> Delete(string slug, CancellationToken cancellationToken);
+    public Task<Entities.File?> GetBySlug(string slug, CancellationToken cancellationToken = default);
+    public Task<Entities.File?> Delete(string slug, CancellationToken cancellationToken = default);
 }
--- src/FluentCMS.Repositories.MongoDB/FileRepository.cs
+++ src/FluentCMS.Repositories.MongoDB/FileRepository.cs
@@ -10,12 +10,12 @@ namespace FluentCMS.Repositories.MongoDB;
 public class FileRepository(IMongoDBContext mongoDbContext, IAuthContext authContext)
     : AuditableEntityRepository<File>(mongoDbContext, authContext), IFileRepository
 {
-    public async Task<File?> GetBySlug(string slug, CancellationToken cancellationToken)
+    public async Task<File?> GetBySlug(string slug, CancellationToken cancellationToken = default)
     {
         return await (await Collection.FindAsync(x => x.Slug == slug, cancellationToken: cancellationToken)).SingleOrDefaultAsync(cancellationToken);
     }
 
-    public async Task<File?> Delete(string slug, CancellationToken cancellationToken)
+    public async Task<File?> Delete(string slug, CancellationToken cancellationToken = default)
     {
         return await Collection.FindOneAndDeleteAsync(x => x.Slug == slug, cancellationToken: cancellationToken);
     }
