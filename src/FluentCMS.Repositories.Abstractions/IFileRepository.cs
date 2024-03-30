e90425b8b6ef8ac3f91f0dca5dc933967e3f8424
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-03-30 20:03:52 +0330

    feat: repository abstraction

---
+++ src/FluentCMS.Repositories.Abstractions/IFileRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace FluentCMS.Repositories.Abstractions;
+public interface IFileRepository
+{
+    public Task<IEnumerable<Entities.File>> GetFiles(CancellationToken cancellationToken);
+    public Task<Entities.File> GetFile(Guid id, CancellationToken cancellationToken);
+    public Task<Entities.File> GetFile(string slug, CancellationToken cancellationToken);
+    public Task DeleteFile(Guid id, CancellationToken cancellationToken);
+    public Task DeleteFile(string slug, CancellationToken cancellationToken);
+    public Task Create(Entities.File file, CancellationToken cancellationToken);
+}
