1cafc0f0687994d758f1a73e29efa19cbe7ec228
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-03-31 01:26:31 +0330

    feat: implement FileController

---
+++ src/FluentCMS.Web.Api/Controllers/FileController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentCMS.Web.Api.Models.File;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+using File = FluentCMS.Entities.File;
+
+namespace FluentCMS.Web.Api.Controllers;
+
+public class FileController(IFileService fileService, IMapper mapper) : BaseGlobalController
+{
+    [HttpPost]
+    public async Task<IApiResult<FileDetailResponse>> Upload([FromForm] FileCreateRequest createRequest, CancellationToken cancellationToken = default)
+    {
+        var file = await fileService.Create(createRequest.Slug, createRequest.File, cancellationToken: cancellationToken);
+        return Ok(mapper.Map<File, FileDetailResponse>(file!));
+    }
+
+    [HttpGet]
+    public async Task<IApiResult<IEnumerable<FileDetailResponse>>> GetFiles()
+    {
+        var files = await fileService.GetAll();
+        return Ok(files.Select(file => mapper.Map<File, FileDetailResponse>(file!)));
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetFileById([FromRoute] Guid id)
+    {
+        var file = await fileService.GetById(id);
+        if (file == null)
+        {
+            return new NotFoundResult();
+        }
+        var fileStream = System.IO.File.OpenRead(file.LocalPath);
+        return new FileStreamResult(fileStream, file.MimeType)
+        {
+            FileDownloadName = file.Name
+        };
+    }
+    [HttpGet("{slug}")]
+    public async Task<IActionResult> GetFileBySlug([FromRoute] string slug)
+    {
+        var file = await fileService.GetBySlug(slug);
+        if (file == null)
+        {
+            return new NotFoundResult();
+        }
+        var fileStream = System.IO.File.OpenRead(file.LocalPath);
+        return new FileStreamResult(fileStream, file.MimeType)
+        {
+            FileDownloadName = file.Name
+        };
+    }
+    [HttpDelete("{id:guid}")]
+    public async Task<IApiResult<bool>> DeleteById([FromRoute] Guid id)
+    {
+        var file = await fileService.DeleteById(id);
+        return Ok(file != null);
+    }
+    [HttpDelete("{slug}")]
+    public async Task<IApiResult<bool>> DeleteBySlug([FromRoute] string slug)
+    {
+        var file = await fileService.DeleteBySlug(slug);
+        return Ok(file != null);
+    }
+
+}
