c520556475e484a19089f0c9953e4e614c6274cb
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-03-31 01:25:46 +0330

    chore: add duplicate file slug error code

--- src/FluentCMS.Shared/Exceptions/ExceptionCodes.cs
+++ src/FluentCMS.Shared/Exceptions/ExceptionCodes.cs
@@ -139,4 +139,10 @@ public class ExceptionCodes
     public const string PageHasChildren = "Page.PageHasChildren";
 
     #endregion
+
+    #region File
+
+    public const string FileDuplicateSlug = "File.DuplicateSlug";
+
+    #endregion
 }
