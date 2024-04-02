7b2d1e07e344b11e9c55debb170f22284f7b70e4
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-02 21:08:09 +0330

    refactor: remove App reference from WhenIGetAllRoles.cs

--- src/Tests/FluentCMS.E2eTests/StepDefinitions/When/WhenIGetAllRoles.cs
+++ src/Tests/FluentCMS.E2eTests/StepDefinitions/When/WhenIGetAllRoles.cs
@@ -5,9 +5,8 @@ public partial class StepDefinitions
     [When("I Get All Roles")]
     public async Task WhenIGetAllRolesAsync()
     {
-        var app = context.Get<AppDetailResponseIApiResult>();
         var rolesClient = context.Get<RoleClient>();
-        var response = await rolesClient.GetAllAsync(app.Data.Slug!);
+        var response = await rolesClient.GetAllAsync();
         context.Set(response);
     }
 }
