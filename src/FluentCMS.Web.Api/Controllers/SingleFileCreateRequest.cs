706828959394ae2688917fdf12cf0db205eb8c18
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-03 22:25:21 +0330

    feat: add `SingleFileCreateRequest`

---
+++ src/FluentCMS.Web.Api/Controllers/SingleFileCreateRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace FluentCMS.Web.Api.Controllers;
+
+public class SingleFileCreateRequest
+{
+    [Required]
+    public IFormFile File { get; set; } = default!;
+}
