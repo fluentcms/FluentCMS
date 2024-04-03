2dddf43742cd1ccc33d0d152601075e0380b6ce0
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-03 22:24:56 +0330

    refactor: rename `FileCreateRequest` to `MultipleFileCreateRequest`

---
+++ src/FluentCMS.Web.Api/Models/File/MultipleFileCreateRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace FluentCMS.Web.Api.Models.File;
+
+public class MultipleFileCreateRequest
+{
+    [Required]
+    public IFormFile[] Files { get; set; } = [];
+}
