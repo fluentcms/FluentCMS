6c8d4549296daaee04a37907b49deb9bafc2be8b
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-10 15:19:09 +0330

    fix: set authentication scheme to JWT explicitly

--- src/FluentCMS.Web.Api/Controllers/RoleController.cs
+++ src/FluentCMS.Web.Api/Controllers/RoleController.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 
 namespace FluentCMS.Web.Api.Controllers;
 
-[Authorize]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
 public class RoleController(IMapper mapper, IRoleService roleService) : BaseGlobalController
 {
     [HttpGet]
