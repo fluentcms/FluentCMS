2c03daead0450a7804298c170971c6f4f87bc86f
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-10 15:19:10 +0330

    refactor: remove unused service from LoginViewPlugin.razor.cs

--- src/FluentCMS.Web.UI/Plugins/Auth/LoginViewPlugin.razor.cs
+++ src/FluentCMS.Web.UI/Plugins/Auth/LoginViewPlugin.razor.cs
@@ -6,8 +6,6 @@ namespace FluentCMS.Web.UI.Plugins.Auth;
 
 public partial class LoginViewPlugin
 {
-    [Inject] private AuthStateProvider AuthStateProvider { get; set; } = default!;
-
     [Inject] private NavigationManager NavigationManager { get; set; } = default!;
 
     [Inject] private ILocalStorageService LocalStorageService { get; set; } = default!;
@@ -15,10 +13,10 @@ public partial class LoginViewPlugin
 
     private async Task OnSubmit()
     {
-        var result = await AuthStateProvider.LoginAsync(Model);
-        if (result.Errors!.Count == 0)
-        {
-            NavigationManager.NavigateTo("/");
-        }
+        //var result = await AuthStateProvider.LoginAsync(Model);
+        //if (result.Errors!.Count == 0)
+        //{
+        //    NavigationManager.NavigateTo("/");
+        //}
     }
 }
