c872419cef784253a6502d9f5cd47e035b6ec7a5
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-10 20:40:01 +0330

    feat: Implement PasswordValidatorAttribute

---
+++ src/FluentCMS.Web.Api/Validation/PasswordValidatorAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FluentCMS.Web.Api.Models;
+
+public class PasswordValidatorAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is not string) return new ValidationResult("Only string Value is accepted");
+        var password = (string)value;
+        var passwordValidator = validationContext.GetRequiredService<IPasswordValidator<User>>();
+        var userManager = validationContext.GetRequiredService<UserManager<User>>();
+        var result = passwordValidator.ValidateAsync(userManager, null, password).Result;
+        if (!result.Succeeded)
+        {
+            return new ValidationResult("InvalidPassword", result.Errors.Select(x => x.Description));
+        }
+        return ValidationResult.Success;
+    }
+}
