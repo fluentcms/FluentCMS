7f2c3e96bd008e5b58130cc02d270b8d7f0c5695
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-10 15:19:09 +0330

    fix: set authentication scheme to JWT explicitly

--- src/FluentCMS.Web.Api/Controllers/PluginDefinitionController.cs
+++ src/FluentCMS.Web.Api/Controllers/PluginDefinitionController.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 
 namespace FluentCMS.Web.Api.Controllers;
 
 public class PluginDefinitionController(IMapper mapper, IPluginDefinitionService pluginDefinitionService) : BaseGlobalController
 {
     [HttpPost]
-    [Authorize]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public async Task<IApiResult<PluginDefinitionDetailResponse>> Create([FromBody] PluginDefinitionCreateRequest request, CancellationToken cancellationToken = default)
     {
         var entity = mapper.Map<PluginDefinition>(request);
