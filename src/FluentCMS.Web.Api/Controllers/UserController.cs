e22f7f2ad754ac0f151e97aa84f79725b6d7098b
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-10 15:19:09 +0330

    fix: set authentication scheme to JWT explicitly

--- src/FluentCMS.Web.Api/Controllers/UserController.cs
+++ src/FluentCMS.Web.Api/Controllers/UserController.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 
 namespace FluentCMS.Web.Api.Controllers;
 
-[Authorize]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
 public class UserController(IUserService userService, IMapper mapper) : BaseGlobalController
 {
 
