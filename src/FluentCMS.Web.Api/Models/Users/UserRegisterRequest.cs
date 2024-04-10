8a2b88a1326e8e9b126bc7f31866eaec6c14f861
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-10 20:40:01 +0330

    fix: add missing validations UserRegisterRequest

--- src/FluentCMS.Web.Api/Models/Users/UserRegisterRequest.cs
+++ src/FluentCMS.Web.Api/Models/Users/UserRegisterRequest.cs
@@ -1,15 +1,37 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FluentCMS.Web.Api.Models;
 
 public class UserRegisterRequest
 {
     [Required]
+    [EmailAddress]
     public required string Email { get; set; }
 
     [Required]
+    [RegularExpression("^[a-zA-Z0-9][a-zA-Z0-9_]{4,19}$")]
     public required string Username { get; set; }
 
     [Required]
+    [PasswordValidator]
     public required string Password { get; set; }
+
+    public class PasswordValidatorAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is not string) return new ValidationResult("Only string Value is accepted");
+            var password = (string)value;
+            var passwordValidator = validationContext.GetRequiredService<IPasswordValidator<User>>();
+            var userManager = validationContext.GetRequiredService<UserManager<User>>();
+            var result = passwordValidator.ValidateAsync(userManager, null, password).Result;
+            if (!result.Succeeded)
+            {
+                return new ValidationResult("InvalidPassword", result.Errors.Select(x => x.Description))
+            }
+            return ValidationResult.Success;
+        }
+    }
 }
