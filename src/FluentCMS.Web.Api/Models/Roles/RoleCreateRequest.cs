d3a7247da8043dcf8319dc916eb9aa2b181a0ebf
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-10 20:40:00 +0330

    fix: add missing validations RoleCreateRequest

--- src/FluentCMS.Web.Api/Models/Roles/RoleCreateRequest.cs
+++ src/FluentCMS.Web.Api/Models/Roles/RoleCreateRequest.cs
@@ -1,7 +1,11 @@
-﻿namespace FluentCMS.Web.Api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FluentCMS.Web.Api.Models;
 
 public class RoleCreateRequest
 {
+    [Length(3, 20)]
+    [Required]
     public string Name { get; set; } = default!;
     public string? Description { get; set; }
 }
