80904c18fe736b8d5c5967489b06d4bcba72be7a
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-16 13:43:37 +0330

    feat: add ContentTypeFieldSetRequest

---
+++ src/FluentCMS.Web.Api/Models/ContentTypeFieldSetRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace FluentCMS.Web.Api.Models;
+
+public class ContentTypeFieldSetRequest
+{
+    public string Slug { get; set; } = default!;
+    public string Title { get; set; } = default!;
+    public string? Description { get; set; }
+    public string Label { get; set; } = default!;
+    public string? Placeholder { get; set; }
+    public string? Hint { get; set; }
+    public string? DefaultValue { get; set; }
+    public bool IsRequired { get; set; }
+    public bool IsPrivate { get; set; }
+    public string FieldType { get; set; }
+    public Dictionary<string, object> Metadata { get; set; }
+}
