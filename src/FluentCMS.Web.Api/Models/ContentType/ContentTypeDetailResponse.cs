0682aca48c2425036dd11214816c19892a917c2d
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-16 13:43:37 +0330

    refactor: change Fields from ContentTypeField to ContentTypeFieldResponse

--- src/FluentCMS.Web.Api/Models/ContentType/ContentTypeDetailResponse.cs
+++ src/FluentCMS.Web.Api/Models/ContentType/ContentTypeDetailResponse.cs
@@ -5,5 +5,5 @@ public class ContentTypeDetailResponse : BaseAppAssociatedResponse
     public string Slug { get; set; } = default!;
     public string Title { get; set; } = default!;
     public string? Description { get; set; }
-    public List<ContentTypeField> Fields { get; set; } = [];
+    public List<ContentTypeFieldResponse> Fields { get; set; } = [];
 }
