c4517ea0b9e62be87aacec98ecc68db7ec428873
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-16 21:56:06 +0330

    refactor: add `FirstName`, `LastName`, and `PhoneNumber` fields to `UserDetailResponse`

--- src/FluentCMS.Web.Api/Models/Users/UserDetailResponse.cs
+++ src/FluentCMS.Web.Api/Models/Users/UserDetailResponse.cs
@@ -4,6 +4,8 @@ public class UserDetailResponse : BaseAuditableResponse
 {
     public string Username { get; set; } = default!;
     public string Email { get; set; } = default!;
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
     public string? PhoneNumber { get; set; }
     public DateTime? LastLoginAt { get; set; }
     public int LoginCount { get; set; }
