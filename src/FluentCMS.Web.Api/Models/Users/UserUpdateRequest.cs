2499b87cc770391fb7ec8bb7b9300999b104ae3d
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-16 21:56:06 +0330

    refactor: add `FirstName`, `LastName`, and `PhoneNumber` fields to `UserUpdateRequest`

--- src/FluentCMS.Web.Api/Models/Users/UserUpdateRequest.cs
+++ src/FluentCMS.Web.Api/Models/Users/UserUpdateRequest.cs
@@ -8,4 +8,8 @@ public class UserUpdateRequest
     public required Guid Id { get; set; }
     [Required]
     public required string Email { get; set; }
+
+    public string PhoneNumber { get; set; } = default!;
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
 }
