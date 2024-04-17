0645e112a95d0d60dae711b57f023fc4a469ca72
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-17 20:24:11 +0330

    cleanup: seperate files

--- src/FluentCMS.Web.ApiClients/ClientServiceExtensions.cs
+++ src/FluentCMS.Web.ApiClients/ClientServiceExtensions.cs
@@ -3,9 +3,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using System.Net.Http.Headers;
 using System.Reflection;
-using System.Text.Json;
-using System.Web;
-using BitzArt.Blazor.Cookies;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -19,25 +16,6 @@ public static class ClientServiceExtensions
             client.BaseAddress = new Uri(configuration["urls"]);
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            //set auth header
-
-            // http context
-            //if (sp.GetService<IHttpContextAccessor>() is { HttpContext: { } httpContext } && httpContext.Request.Cookies.ContainsKey("UserLoginResponse"))
-            //{
-            //    var loginResponse = JsonSerializer.Deserialize<UserLoginResponse>(httpContext.Request.Cookies["UserLoginResponse"] ?? throw new Exception("Cookie 'UserLoginResponse' is null!")) ?? throw new Exception("Unable to deserialize UserLoginResponse");
-            //    var token = loginResponse.Token;
-            //    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
-            //}
-
-            //service
-            //if (sp.GetService<ICookieService>() is { } cookieService &&
-            //    cookieService.GetAsync(nameof(UserLoginResponse)).GetAwaiter().GetResult() is { } cookie &&
-            //    JsonSerializer.Deserialize<UserLoginResponse>(HttpUtility.UrlDecode(cookie.Value)) is { } response)
-            //{
-            //    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", response.Token);
-            //}
-
 
         }).AddHttpMessageHandler<HttpClientHeaderHandler>();
 
@@ -65,18 +43,5 @@ public static class ClientServiceExtensions
             });
         }
         return services;
-    }
-}
-
-public class HttpClientHeaderHandler(ICookieService cookieService):DelegatingHandler
-{
-    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-    {
-        if (cookieService.GetAsync(nameof(UserLoginResponse)).GetAwaiter().GetResult() is { } cookie &&
-            JsonSerializer.Deserialize<UserLoginResponse>(HttpUtility.UrlDecode(cookie.Value)) is { } response)
-        {
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", response.Token);
-        }
-        return base.SendAsync(request, cancellationToken);
     }
 }
---
+++ src/FluentCMS.Web.ApiClients/HttpClientHeaderHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Web;
+using BitzArt.Blazor.Cookies;
+using FluentCMS.Web.ApiClients;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public class HttpClientHeaderHandler(ICookieService cookieService):DelegatingHandler
+{
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        if (cookieService.GetAsync(nameof(UserLoginResponse)).GetAwaiter().GetResult() is { } cookie &&
+            JsonSerializer.Deserialize<UserLoginResponse>(HttpUtility.UrlDecode(cookie.Value)) is { } response)
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", response.Token);
+        }
+        return base.SendAsync(request, cancellationToken);
+    }
+}
