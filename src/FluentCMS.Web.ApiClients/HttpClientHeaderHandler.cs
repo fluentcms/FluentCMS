9cae388c980de5c084b57f1bf465a0b27c84c518
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-18 12:54:35 +0330

    test: compare results from `cookieService` and `httpContext`

--- src/FluentCMS.Web.ApiClients/HttpClientHeaderHandler.cs
+++ src/FluentCMS.Web.ApiClients/HttpClientHeaderHandler.cs
@@ -12,14 +12,28 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public class HttpClientHeaderHandler(IServiceProvider serviceProvider) : DelegatingHandler
 {
-    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
+        //use: Service
+
+        var cookieService = serviceProvider.GetRequiredService<ICookieService>();
+        var cookie = await cookieService.GetAsync(nameof(UserLoginResponse));
+
+        //use: httpContext
         var contextAccessor = serviceProvider.GetRequiredService<IHttpContextAccessor>();
         // if httpContext is available, and we have a valid auth cookie
         if (contextAccessor.HttpContext is var httpContext && httpContext != null && httpContext.Request.TryGetJsonCookie<UserLoginResponse>(nameof(UserLoginResponse), out var userLoginResponse))
         {
+            string Hash(string value)
+            {
+                return Convert.ToBase64String(MD5.HashData(Encoding.UTF8.GetBytes(value)));
+            }
+
+
+            Debug.WriteLine($"tokenReturnedByCookieService={Hash(cookie.Value??"")} and tokenReturnedByHttpContext={Hash(userLoginResponse.Token)}", "HttpClientHeaderHandler");
+
             request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", userLoginResponse.Token);
         }
-        return base.SendAsync(request, cancellationToken);
+        return await base.SendAsync(request, cancellationToken);
     }
 }
