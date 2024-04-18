0b743e47a84a5cfe4ac154421c25d9248f8f5afd
abdolian <m.abdolian@gmail.com>
2024-04-18 19:32:20 +0330

    feat: Add media content type field.

---
+++ src/FluentCMS.Web.UI/Plugins/ContentTypeManagement/ContentTypeFields/Media/Media.cs
@@ -0,0 +1,18 @@
+﻿namespace FluentCMS.Web.UI.Plugins.ContentTypeManagement.ContentTypeFields;
+
+public class MediaInfo : Base
+{
+    public static string Key = "media";
+
+    public static string Title = "Media";
+
+    public static string Description = "Files like images, videos, etc";
+
+    public static IconName Icon = IconName.Eye;
+
+    public static int Order = 8;
+
+    public static Type BasicSettings = typeof(MediaBasicSettings);
+
+    public static Type AdvancedSettings = typeof(MediaAdvancedSettings);
+}
