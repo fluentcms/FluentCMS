52248f487880747bfe013d69f45effffa6b4e69c
Amir Pournasserian <amir.pournasserian@live.com>
2024-04-20 13:46:04 -0400

    #1038 add BasePlugin, MyForm

---
+++ src/FluentCMS.Web.UI/Plugins/BasePlugin.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FluentCMS.Web.UI.Plugins;
+
+public abstract class BasePlugin : ComponentBase
+{
+    [Inject]
+    protected NavigationManager NavigationManager { get; set; } = default!;
+
+    [CascadingParameter]
+    protected HttpContext HttpContext { get; set; } = default!;
+
+    [CascadingParameter]
+    public PluginDetailResponse? Plugin { get; set; } = default!;
+
+    [CascadingParameter]
+    public PageFullDetailResponse? Page { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (HttpContext is null)
+            throw new ArgumentNullException(nameof(HttpContext));
+
+        if (HttpMethods.IsPost(HttpContext.Request.Method))
+        {
+            await OnPostAsync();
+        }
+        else
+        {
+            await OnFirstAsync();
+        }
+    }
+
+
+    protected virtual async Task OnPostAsync()
+    {
+        await Task.CompletedTask;
+    }
+
+    protected virtual async Task OnFirstAsync()
+    {
+        await Task.CompletedTask;
+    }
+}
--- src/FluentCMS.Web.UI/Plugins/Setup/SetupViewPlugin.razor.cs
+++ src/FluentCMS.Web.UI/Plugins/Setup/SetupViewPlugin.razor.cs
@@ -1,80 +1,41 @@
 ﻿using FluentCMS.Web.UI.Services;
-using Microsoft.AspNetCore.Http;
 
 namespace FluentCMS.Web.UI.Plugins.Setup;
 
 public partial class SetupViewPlugin
 {
-
     [SupplyParameterFromForm(FormName = "SetupForm")]
-    public SetupRequest Model { get; set; } = new()
-    {
-        AppTemplateName = "Blank",
-        SiteTemplateName = "Blank"
-    };
+    public SetupRequest Model { get; set; } = new();
 
     [Inject]
     private SetupManager SetupManager { get; set; } = default!;
-
-    [Inject]
-    private NavigationManager NavigationManager { get; set; } = default!;
-
-    [CascadingParameter]
-    private HttpContext HttpContext { get; set; } = default!;
-
-    private string? Message { get; set; }
-
-    private string? ErrorMessage { get; set; }
 
     private bool Initialized { get; set; } = false;
 
     protected override async Task OnInitializedAsync()
     {
-
+        await base.OnInitializedAsync();
         Initialized = await SetupManager.IsInitialized();
-
-        try
-        {
-            if (HttpContext is null)
-                throw new ArgumentNullException(nameof(HttpContext));
-
-            if (!HttpMethods.IsPost(HttpContext.Request.Method))
-            {
-                Model.Username = "admin";
-                Model.Email = "admin@example.com";
-                Model.Password = "Passw0rd!";
-                Model.AdminDomain = new Uri(NavigationManager.BaseUri).Authority;
-            }
-            else
-            {
-                // validate posted data
-            }
-
-        }
-        catch (Exception ex)
-        {
-            ErrorMessage = ex.Message;
-        }
     }
 
-    protected override void OnParametersSet()
+    protected override async Task OnFirstAsync()
     {
+        await base.OnFirstAsync();
 
+        Model = new SetupRequest
+        {
+            Username = "admin",
+            Email = "admin@example.com",
+            Password = "Passw0rd!",
+            AdminDomain = new Uri(NavigationManager.BaseUri).Authority,
+            AppTemplateName = "Blank",
+            SiteTemplateName = "Blank"
+        };
     }
 
-    private async Task HandleSubmit()
+    private async Task OnSubmit()
     {
-        try
-        {
-            if (await SetupManager.Start(Model))
-            {
-                Initialized = await SetupManager.IsInitialized();
-                Message = "Setup completed!";
-            }
-        }
-        catch (Exception ex)
-        {
-            ErrorMessage = ex.ToString();
-        }
+        if (await SetupManager.Start(Model))
+            Initialized = await SetupManager.IsInitialized();
     }
 }
