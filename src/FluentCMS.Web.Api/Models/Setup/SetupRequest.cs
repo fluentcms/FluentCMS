0a8eafaa391655b20de3541b710099c41bcdaddf
Amir Pournasserian <amir.pournasserian@live.com>
2024-04-21 12:31:58 -0400

    #1038 a few cleanup for UserCreatePlugin

--- src/FluentCMS.Web.Api/Models/Setup/SetupRequest.cs
+++ src/FluentCMS.Web.Api/Models/Setup/SetupRequest.cs
@@ -12,10 +12,13 @@ public class SetupRequest
 
     [Required]
     public string Password { get; set; } = default!;
+
     [Required]
     public string AppTemplateName { get; set; } = default!;
+
     [Required]
     public string SiteTemplateName { get; set; } = default!;
+
     [Required]
     [DomainName]
     public string AdminDomain { get; set; } = default!;
--- src/FluentCMS.Web.UI/Plugins/UserManagement/UserCreatePlugin.razor.cs
+++ src/FluentCMS.Web.UI/Plugins/UserManagement/UserCreatePlugin.razor.cs
@@ -1,33 +1,28 @@
 using FluentCMS.Web.UI.Services;
 
 namespace FluentCMS.Web.UI.Plugins.UserManagement;
+
 public partial class UserCreatePlugin
 {
-    const string _formName = "UserCreateForm";
-
-    [Inject]
-    NavigationManager NavigationManager { set; get; } = default!;
+    [SupplyParameterFromForm(FormName = _formName)]
+    UserCreateRequest Model { get; set; } = new();
 
     [Inject]
     IHttpClientFactory HttpClientFactory { set; get; } = default!;
-    UserClient UserClient { get; set; } = default!;
 
     string? Message { get; set; }
     public string BackUrl => new Uri(CurrentUrl).LocalPath;
 
-    [SupplyParameterFromForm(FormName = _formName)]
-    UserCreateRequest Model { get; set; } = new();
     protected override void OnInitialized()
     {
         base.OnInitialized();
-        UserClient = HttpClientFactory.GetClient<UserClient>();
     }
 
     async Task OnSubmit()
     {
         try
         {
-            var newUser = await UserClient.CreateAsync(Model);
+            var newUser = await HttpClientFactory.GetClient<UserClient>().CreateAsync(Model);
             Message = "Done!";
         }
         catch (Exception ex)
