f245452c630431178397515f59a78e2fa0667b5b
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-22 16:32:43 +0330

    feat: add support of Iterable objects

--- src/FluentCMS.Shared/MetadataExtensions.cs
+++ src/FluentCMS.Shared/MetadataExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace FluentCMS.Shared;
+﻿using System.Reflection;
+
+namespace FluentCMS.Shared;
 
 public static class MetadataExtensions
 {
@@ -10,13 +12,13 @@ public static class MetadataExtensions
             throw new Exception("Cant use this type");
         }
 
-        var metaDataDictionary = (IDictionary<string, object?>)metaDataField.GetValue(obj);
+        var metaDataDictionary = metaDataField.GetValue(obj) as IDictionary<string, object?>;
         if (metaDataDictionary == null)
         {
             metaDataField.SetValue(obj, new Dictionary<string, object?>());
-            metaDataDictionary = (IDictionary<string, object?>)metaDataField.GetValue(obj);
+            metaDataDictionary = metaDataField.GetValue(obj) as IDictionary<string, object?>;
         }
-        if (!metaDataDictionary.ContainsKey(fieldName))
+        if (!metaDataDictionary!.ContainsKey(fieldName))
         {
             metaDataDictionary[fieldName] = default(T);
         }
@@ -33,10 +35,19 @@ public static class MetadataExtensions
         var metaDataDictionary = metaDataField.GetValue(obj);
         if (metaDataDictionary == null)
         {
-            metaDataField.SetValue(obj,new Dictionary<string,object?>());
+            metaDataField.SetValue(obj, new Dictionary<string, object?>());
             metaDataDictionary = metaDataField.GetValue(obj);
         }
         ((IDictionary<string, object?>)metaDataDictionary!)[fieldName] = value;
     }
-
+    public static IEnumerable<T>? GetValueArray<T>(this object obj, string fieldName, string metadataField = "Metadata")
+    {
+        var metaDataField = obj.GetType().GetProperty(metadataField) ?? throw new Exception("Type Does not have a meta data field");
+        var metaDataDictionary = (metaDataField.GetValue(obj) as IDictionary<string, object?>) ?? new Dictionary<string, object?>();
+        var v = metaDataDictionary[fieldName] as IEnumerable<object?> ?? Array.Empty<object?>();
+        foreach (var item in v)
+        {
+            yield return ((T?)item) ?? throw new Exception("Couldnt cast item to T");
+        }
+    }
 }
