fe20b29f2156b641dcd4c2c746719d6c3b7e74c0
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-23 20:06:11 +0330

    use injection

--- src/FluentCMS.Web.UI/Plugins/UserManagement/UserListPlugin.razor.cs
+++ src/FluentCMS.Web.UI/Plugins/UserManagement/UserListPlugin.razor.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Components.Authorization;
+
 namespace FluentCMS.Web.UI.Plugins.UserManagement;
 
 public partial class UserListPlugin
@@ -7,8 +9,19 @@ public partial class UserListPlugin
     [Inject]
     public UserClient UserClient { get; set; } = default!;
 
+    [Inject]
+    public AuthenticationStateProvider AuthenticationStateProvider { get; set; } = default!;
+    public AuthenticationState? UserDetail { get; private set; }
+
     protected override async Task OnLoadAsync()
     {
+        UserDetail = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+
+        AuthenticationStateProvider.AuthenticationStateChanged += async (state) =>
+        {
+            UserDetail = await state;
+        };
+
         var usersResponse = await UserClient.GetAllAsync();
         Users = usersResponse?.Data?.ToList() ?? [];
     }
