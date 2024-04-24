ee8f2021d1584a1179678a465fbe750d6618d268
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-25 00:32:21 +0330

    refactor: User Management Cleanup
    cleaup UI implementation of UserManagment using InteractiveServer render mode!
    
    implement server side errors

--- src/FluentCMS.Web.UI/Plugins/UserManagement/UserCreatePlugin.razor.cs
+++ src/FluentCMS.Web.UI/Plugins/UserManagement/UserCreatePlugin.razor.cs
@@ -1,24 +1,16 @@
+using System.Linq;
+
 namespace FluentCMS.Web.UI.Plugins.UserManagement;
 
 public partial class UserCreatePlugin
 {
-    [SupplyParameterFromForm(FormName = FORM_NAME)]
-    private UserCreateRequest Model { get; set; } = new();
-
-    public const string FORM_NAME = "UserCreateForm";
+    private UserCreateRequest Model { get; set; } = new() { Enabled = true };
 
     private string? Error { get; set; }
 
     private async Task OnSubmit()
     {
-        try
-        {
-            var apiResult = await GetApiClient<UserClient>().CreateAsync(Model);
-            NavigateBack();
-        }
-        catch (Exception ex)
-        {
-            Error = ex.Message;
-        }
+        var apiResult = await GetApiClient<UserClient>().CreateAsync(Model);
+        NavigateBack();
     }
 }
