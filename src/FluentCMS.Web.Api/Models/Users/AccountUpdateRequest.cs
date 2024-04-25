d535c0deb0d31cef0c9ea4680ef6b410e291ca69
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-25 16:01:12 +0330

    refactor: implement profile update endpoint
    implement a seperate endpoint for changing profile by user itself

---
+++ src/FluentCMS.Web.Api/Models/Users/AccountUpdateRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FluentCMS.Web.Api.Models.Users;
+
+public class AccountUpdateRequest
+{
+    public required string Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
