674f67172991827b00db229a6cda192490974240
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-26 16:27:21 +0330

    refactor: simplify authorize attribute

---
+++ src/FluentCMS.Web.Api/Attributes/JwtAuthorize.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCMS.Web.Api.Attributes;
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+public class JwtAuthorize : AuthorizeAttribute
+{
+    public JwtAuthorize()
+    {
+        AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme;
+    }
+}
