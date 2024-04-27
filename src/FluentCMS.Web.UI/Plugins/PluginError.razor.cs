36c68dcb231a1f2e74071c1dcabf0100b61e60ee
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-27 20:21:27 +0330

    feat: implement `PluginError` component

---
+++ src/FluentCMS.Web.UI/Plugins/PluginError.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace FluentCMS.Web.UI.Plugins;
+
+public partial class PluginError
+{
+    [Parameter]
+    public string? Message { get; set; }
+
+    [Parameter]
+    public ErrorBoundary? ErrorBoundaryRef { get; set; }
+
+    private void Retry()
+    {
+        ErrorBoundaryRef?.Recover();
+    }
+}
