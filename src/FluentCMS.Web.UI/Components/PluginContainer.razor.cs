42806c3733ccc6c02e3d1ec50d855bef6b1616c1
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-27 21:25:42 +0330

    refactor: cleanup plugin container error handling

---
+++ src/FluentCMS.Web.UI/Components/PluginContainer.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace FluentCMS.Web.UI.Components;
+
+public partial class PluginContainer
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    public ErrorBoundary ErrorBoundaryRef { get; set; }
+}
