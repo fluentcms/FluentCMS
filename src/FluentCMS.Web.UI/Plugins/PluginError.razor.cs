8e0ccf54b7f3fb04abdabfb70a6f9abe514997a5
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-27 21:02:10 +0330

    refactor: removing this as a blank blazor project does not have this issue

--- src/FluentCMS.Web.UI/Plugins/PluginError.razor.cs
+++ src/FluentCMS.Web.UI/Plugins/PluginError.razor.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Components.Routing;
-using Microsoft.AspNetCore.Components.Web;
+﻿using Microsoft.AspNetCore.Components.Web;
 
 namespace FluentCMS.Web.UI.Plugins;
 
@@ -11,34 +10,8 @@ public partial class PluginError
     [Parameter]
     public ErrorBoundary? ErrorBoundaryRef { get; set; }
 
-    [Inject]
-    public NavigationManager? NavigationManager { get; set; }
-
-    protected override void OnAfterRender(bool firstRender)
-    {
-        base.OnAfterRender(firstRender);
-        if (firstRender && NavigationManager != null)
-        {
-            NavigationManager.LocationChanged += NavigationManagerOnLocationChanged;
-        }
-    }
-
-    private void NavigationManagerOnLocationChanged(object? sender, LocationChangedEventArgs e)
-    {
-        // recover component to be able to navigate
-        Retry();
-    }
-
     private void Retry()
     {
         ErrorBoundaryRef?.Recover();
-    }
-
-    public void Dispose()
-    {
-        if (NavigationManager != null)
-        {
-            NavigationManager.LocationChanged -= NavigationManagerOnLocationChanged;
-        }
     }
 }
