1a5a49fd482681805eeb9e158c84f3a84b981ca9
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-27 17:44:25 +0330

    refactor: remove unused variable `apiResult`

--- src/FluentCMS.Web.UI/Plugins/UserManagement/UserCreatePlugin.razor.cs
+++ src/FluentCMS.Web.UI/Plugins/UserManagement/UserCreatePlugin.razor.cs
@@ -1,5 +1,3 @@
-using System.Linq;
-
 namespace FluentCMS.Web.UI.Plugins.UserManagement;
 
 public partial class UserCreatePlugin
@@ -8,7 +6,7 @@ public partial class UserCreatePlugin
 
     private async Task OnSubmit()
     {
-        var apiResult = await GetApiClient<UserClient>().CreateAsync(Model);
+        await GetApiClient<UserClient>().CreateAsync(Model);
         NavigateBack();
     }
 }
