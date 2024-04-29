f6a5d943a206bc178497ccb21f0fff5984a84df0
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-29 16:01:35 +0330

    fix: `ChangePasswordPlugin` functionality

--- src/FluentCMS.Web.UI/Plugins/Auth/ChangePasswordPlugin.razor.cs
+++ src/FluentCMS.Web.UI/Plugins/Auth/ChangePasswordPlugin.razor.cs
@@ -1,9 +1,18 @@
-﻿namespace FluentCMS.Web.UI.Plugins.Auth;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace FluentCMS.Web.UI.Plugins.Auth;
+
 public partial class ChangePasswordPlugin
 {
-    private UserChangePasswordRequest Model { get; set; } = new();
-    public async Task OnSumbit()
+    private UserChangePasswordRequest Model { get; } = new();
+
+    [CascadingParameter]
+    public Task<AuthenticationState> AuthenticationState { get; set; } = default!;
+
+    private async Task OnSubmit()
     {
+        Model.UserId = Guid.Parse((await AuthenticationState).User.FindFirstValue(ClaimTypes.Sid)!);
         await GetApiClient<AccountClient>().ChangePasswordAsync(Model);
         NavigateBack();
     }
