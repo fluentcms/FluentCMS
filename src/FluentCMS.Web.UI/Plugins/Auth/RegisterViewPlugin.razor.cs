372de876d8892b7bccfaa823fd85837500757e22
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-29 16:10:03 +0330

    refactor: `RegisterViewPlugin` functionality

---
+++ src/FluentCMS.Web.UI/Plugins/Auth/RegisterViewPlugin.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace FluentCMS.Web.UI.Plugins.Auth;
+
+public partial class RegisterViewPlugin
+{
+    private UserRegisterRequest Model { get; } = new();
+
+    private async Task OnSubmit()
+    {
+        await GetApiClient<AccountClient>().RegisterAsync(Model);
+
+        NavigationManager.NavigateTo("/auth/login", true);
+    }
+}
