c93c4fa5b25ffc9d19a0327ffba3f4cd9d0dab3d
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-04-30 19:37:49 +0330

    refactor: `ForgotPasswordViewPlugin` & `ResetPasswordViewPlugin` functionality review

---
+++ src/FluentCMS.Web.UI/Plugins/Auth/ResetPasswordViewPlugin.razor.cs
@@ -0,0 +1,26 @@
+﻿namespace FluentCMS.Web.UI.Plugins.Auth;
+
+public partial class ResetPasswordViewPlugin
+{
+    [SupplyParameterFromQuery(Name = nameof(Email))]
+    public string Email { get; set; }
+
+    private UserValidatePasswordResetTokenRequest Model { get; set; } = new();
+
+    private async Task OnSubmit()
+    {
+        await GetApiClient<AccountClient>().ValidatePasswordResetTokenAsync(Model);
+
+        NavigationManager.NavigateTo("/auth/login", true);
+    }
+
+    protected override void OnInitialized()
+    {
+        if (string.IsNullOrEmpty(Email))
+        {
+            throw new ArgumentNullException(nameof(Email));
+        }
+
+        Model.Email = Email;
+    }
+}
