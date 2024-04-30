15cf4fd1d1997b30401e61937e66815554326f4d
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-05-01 01:04:21 +0330

    refactor: use `OnLoadAsync` instead of `OnInitialize` in `ResetPasswordViewPlugin`

--- src/FluentCMS.Web.UI/Plugins/Auth/ResetPasswordViewPlugin.razor.cs
+++ src/FluentCMS.Web.UI/Plugins/Auth/ResetPasswordViewPlugin.razor.cs
@@ -3,7 +3,7 @@
 public partial class ResetPasswordViewPlugin
 {
     [SupplyParameterFromQuery(Name = nameof(Email))]
-    public string Email { get; set; }
+    public string Email { get; set; } = default!;
 
     private UserValidatePasswordResetTokenRequest Model { get; set; } = new();
 
@@ -14,13 +14,13 @@ public partial class ResetPasswordViewPlugin
         NavigationManager.NavigateTo("/auth/login", true);
     }
 
-    protected override void OnInitialized()
+    protected override async Task OnLoadAsync()
     {
+        await base.OnLoadAsync();
         if (string.IsNullOrEmpty(Email))
         {
             throw new ArgumentNullException(nameof(Email));
         }
-
         Model.Email = Email;
     }
 }
