acedcc5282b48bf5b7e729c793b2d243624ff182
abdolian <m.abdolian@gmail.com>
2024-05-01 11:41:32 +0330

    feat: Add plugin base components.

---
+++ src/FluentCMS.Web.UI/Components/Form/PluginSection.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace FluentCMS.Web.UI;
+
+public partial class PluginSection
+{
+    [Parameter]
+    public string? Title { get; set; }
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+}
