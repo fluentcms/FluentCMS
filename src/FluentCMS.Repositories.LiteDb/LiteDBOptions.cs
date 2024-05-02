3e29258a1b988544db3e5080c9bd48f571af9da0
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-05-02 19:31:04 +0330

    feat: implement `LiteDBOptions`

---
+++ src/FluentCMS.Repositories.LiteDb/LiteDBOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace FluentCMS.Repositories.LiteDb;
+
+public class LiteDBOptions<TContext> where TContext : ILiteDBContext
+{
+    public LiteDBOptions(string connectionString)
+    {
+        // Guard clause to ensure the connection string is not null or empty
+        if (string.IsNullOrWhiteSpace(connectionString))
+            throw new ArgumentException("Connection string cannot be null or empty.", nameof(connectionString));
+
+        ConnectionString = connectionString;
+    }
+
+    public string ConnectionString { get; }
+}
