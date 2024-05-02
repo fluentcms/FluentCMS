3789e466c45a70befb83d29a611b6be681ef6e7c
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-05-02 19:48:58 +0330

    feat: implement `SiteRepository` for LiteDb

---
+++ src/FluentCMS.Repositories.LiteDb/SiteRepository.cs
@@ -0,0 +1,21 @@
+﻿using FluentCMS.Entities;
+using FluentCMS.Repositories.Abstractions;
+using LiteDB;
+
+namespace FluentCMS.Repositories.LiteDb;
+
+public class SiteRepository : AuditableEntityRepository<Site>, ISiteRepository
+{
+    public SiteRepository(ILiteDBContext liteDbContext, IAuthContext authContext) : base(liteDbContext, authContext)
+    {
+    }
+    public async Task<Site?> GetByUrl(string url, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var filter = Query.Any().EQ(nameof(Site.Urls),url);
+        var findResult = await Collection.FindAsync(filter);
+
+        return findResult.SingleOrDefault();
+    }
+}
