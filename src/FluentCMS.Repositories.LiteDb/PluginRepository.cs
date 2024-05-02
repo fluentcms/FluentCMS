82f5a370406029c61df50ba6d1e53902d079d9d3
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-05-02 19:45:06 +0330

    feat: implement `PluginRepository` for LiteDb

---
+++ src/FluentCMS.Repositories.LiteDb/PluginRepository.cs
@@ -0,0 +1,21 @@
+﻿using FluentCMS.Entities;
+using FluentCMS.Repositories.Abstractions;
+using LiteDB;
+
+namespace FluentCMS.Repositories.LiteDb;
+
+public class PluginRepository : SiteAssociatedRepository<Plugin>, IPluginRepository
+{
+    public PluginRepository(ILiteDBContext liteDbContext, IAuthContext authContext) : base(liteDbContext, authContext)
+    {
+    }
+
+    public async Task<IEnumerable<Plugin>> GetByPageId(Guid pageId, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var filter = Query.EQ(nameof(Plugin.PageId), pageId);
+        var result = await Collection.FindAsync(filter);
+        return result.ToList();
+    }
+}
