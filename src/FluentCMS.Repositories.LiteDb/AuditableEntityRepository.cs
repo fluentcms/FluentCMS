e7205d83930c35a3e92f134f2e9be757edc9c25b
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-05-02 19:15:51 +0330

    feat: implement `AuditableEntityRepository` for LiteDb

---
+++ src/FluentCMS.Repositories.LiteDb/AuditableEntityRepository.cs
@@ -0,0 +1,50 @@
+﻿using FluentCMS.Entities;
+using FluentCMS.Repositories.Abstractions;
+
+namespace FluentCMS.Repositories.LiteDb;
+
+public class AuditableEntityRepository<TEntity> : EntityRepository<TEntity>, IAuditableEntityRepository<TEntity>
+    where TEntity : IAuditableEntity
+{
+    protected readonly IAuthContext AuthContext;
+
+    public AuditableEntityRepository(ILiteDBContext liteDbContext, IAuthContext authContext) : base(liteDbContext)
+    {
+        AuthContext = authContext;
+    }
+
+    public override async Task<TEntity?> Create(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        SetAuditableFieldsForCreate(entity);
+        return await base.Create(entity, cancellationToken);
+    }
+
+    public override async Task<IEnumerable<TEntity>> CreateMany(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        foreach (var entity in entities)
+            SetAuditableFieldsForCreate(entity);
+
+        return await base.CreateMany(entities, cancellationToken);
+    }
+
+    public override async Task<TEntity?> Update(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        SetAuditableFieldsForUpdate(entity);
+        return await base.Update(entity, cancellationToken);
+    }
+
+    private void SetAuditableFieldsForCreate(TEntity entity)
+    {
+        entity.CreatedAt = DateTime.UtcNow;
+        entity.CreatedBy = AuthContext.Username;
+    }
+
+    private void SetAuditableFieldsForUpdate(TEntity entity)
+    {
+        entity.ModifiedAt = DateTime.UtcNow;
+        entity.ModifiedBy = AuthContext.Username;
+    }
+}
