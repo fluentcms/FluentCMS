2fa9fdef9a95865fec9c353fe499fad2142310b4
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-05-02 19:44:23 +0330

    feat: implement `SiteAssociatedRepository` for LiteDb

---
+++ src/FluentCMS.Repositories.LiteDb/SiteAssociatedRepository.cs
@@ -0,0 +1,31 @@
+﻿using FluentCMS.Entities;
+using FluentCMS.Repositories.Abstractions;
+using LiteDB;
+
+namespace FluentCMS.Repositories.LiteDb;
+
+public abstract class SiteAssociatedRepository<TEntity> : AuditableEntityRepository<TEntity>, ISiteAssociatedRepository<TEntity>
+    where TEntity : ISiteAssociatedEntity
+{
+    public SiteAssociatedRepository(ILiteDBContext liteDbContext, IAuthContext authContext) : base(liteDbContext, authContext)
+    {
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAllForSite(Guid siteId, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        var filter = Query.EQ(nameof(ISiteAssociatedEntity.SiteId), siteId);
+        var findResult = await Collection.FindAsync(filter);
+        return findResult.ToList();
+    }
+
+    public async Task<TEntity?> GetByIdForSite(Guid id, Guid siteId, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        var idFilter = Query.EQ(nameof(ISiteAssociatedEntity.Id), id);
+        var siteIdFilter = Query.EQ(nameof(ISiteAssociatedEntity.SiteId), siteId);
+        var combinedFilter = Query.And(idFilter, siteIdFilter);
+        var findResult = await Collection.FindAsync(combinedFilter);
+        return findResult.SingleOrDefault();
+    }
+}
