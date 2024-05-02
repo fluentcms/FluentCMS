35bd8bcd9ce51a1ab8a46356d8e8f975a1689121
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-05-03 00:10:37 +0330

    feat: implement `ApiTokenRepository` for LiteDb

---
+++ src/FluentCMS.Repositories.LiteDb/ApiTokenRepository.cs
@@ -0,0 +1,13 @@
+﻿using FluentCMS.Entities;
+using FluentCMS.Repositories.Abstractions;
+
+namespace FluentCMS.Repositories.LiteDb;
+
+public class ApiTokenRepository(
+    ILiteDBContext liteDbContext,
+    IAuthContext authContext) :
+    AuditableEntityRepository<ApiToken>(liteDbContext, authContext),
+    IApiTokenRepository
+{
+
+}
