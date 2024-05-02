e673a7c1e6160bcfb6d82a4ed9672fdac0a622da
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-05-02 19:21:33 +0330

    feat: implement `ContentTypeRepository` for LiteDb

---
+++ src/FluentCMS.Repositories.LiteDb/ContentTypeRepository.cs
@@ -0,0 +1,23 @@
+﻿using FluentCMS.Entities;
+using FluentCMS.Repositories.Abstractions;
+using LiteDB;
+
+namespace FluentCMS.Repositories.LiteDb;
+
+public class ContentTypeRepository (
+    ILiteDBContext liteDbContext,
+    IAuthContext authContext) :
+    AuditableEntityRepository<ContentType>(liteDbContext, authContext),
+    IContentTypeRepository
+{
+    public async Task<ContentType?> GetBySlug(string contentTypeSlug, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var filter = Query.EQ(nameof(ContentType.Slug), contentTypeSlug);
+
+        var findResult = await Collection.FindAsync(filter);
+
+        return findResult.SingleOrDefault();
+    }
+}
