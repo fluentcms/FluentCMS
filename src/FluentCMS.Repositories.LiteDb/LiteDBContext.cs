2ff1f08cf45236664e7def4079281d5d401dafdb
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-05-02 19:30:22 +0330

    feat: implement `LiteDBContext`

---
+++ src/FluentCMS.Repositories.LiteDb/LiteDBContext.cs
@@ -0,0 +1,22 @@
+﻿using LiteDB.Async;
+
+namespace FluentCMS.Repositories.LiteDb;
+
+public interface ILiteDBContext
+{
+    ILiteDatabaseAsync Database { get; }
+}
+
+public class LiteDBContext : ILiteDBContext
+{
+    public LiteDBContext(LiteDBOptions<LiteDBContext> options)
+    {
+        // Validate input arguments to ensure they are not null.
+        ArgumentNullException.ThrowIfNull(options, nameof(options));
+        ArgumentNullException.ThrowIfNull(options.ConnectionString, nameof(options.ConnectionString));
+
+        Database = new LiteDatabaseAsync(options.ConnectionString);
+    }
+
+    public ILiteDatabaseAsync Database { get; }
+}
