d8d10ae3035599d1b5716411b2a53c0b785868cb
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-05-04 16:37:14 +0330

    feat: add `AccountUpdateRequest` Dto

---
+++ src/FluentCMS.Web.Api/Models/Users/AccountUpdateRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace FluentCMS.Web.Api.Controllers;
+
+public class AccountUpdateRequest
+{
+    public required string Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
