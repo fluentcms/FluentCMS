06d8602b10b18bf46630b5a17f4b45dc96711d45
Amir Pournasserian <amir.pournasserian@live.com>
2024-05-04 17:16:17 -0400

    #1256 revert visible functionality in base

---
+++ src/FluentCMS.Web.UI.Components/Components/Badge/Badge.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FluentCMS.Web.UI.Components;
+public partial class Badge
+{
+    [Parameter]
+    [CSSProperty]
+    public Color Color { get; set; } = Color.Default;
+}
