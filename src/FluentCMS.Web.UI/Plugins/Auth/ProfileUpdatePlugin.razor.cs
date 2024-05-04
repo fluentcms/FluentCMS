ecba9d9d5ac5118216d56b89aac0b8630a40567b
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-05-04 21:46:24 +0330

    feat: implement `ProfileUpdatePlugin`

---
+++ src/FluentCMS.Web.UI/Plugins/Auth/ProfileUpdatePlugin.razor.cs
@@ -0,0 +1,31 @@
+﻿namespace FluentCMS.Web.UI.Plugins.Auth;
+
+public partial class ProfileUpdatePlugin
+{
+    public AccountUpdateRequest Model { get; set; } = new()
+    {
+        Email = "",
+        FirstName = "",
+        LastName = "",
+        PhoneNumber = ""
+    };
+
+    protected override async Task OnLoadAsync()
+    {
+        await base.OnLoadAsync();
+        var user = (await GetApiClient<AccountClient>().GetCurrentAsync()).Data;
+        Model = new AccountUpdateRequest()
+        {
+            Email = user.Email ?? string.Empty,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            PhoneNumber = user.PhoneNumber,
+        };
+    }
+
+    private async Task OnSubmit()
+    {
+        await GetApiClient<AccountClient>().UpdateCurrentAsync(Model);
+        NavigateBack();
+    }
+}
