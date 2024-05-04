1b5409cb050d20a2ce42635e483a6d26f7094902
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-05-04 21:45:46 +0330

    feat: implement `ProfileChangePasswordPlugin`

---
+++ src/FluentCMS.Web.UI/Plugins/Auth/ProfileChangePasswordPlugin.razor.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace FluentCMS.Web.UI.Plugins.Auth;
+
+public partial class ProfileChangePasswordPlugin
+{
+    [CascadingParameter]
+    public Task<AuthenticationState> UserState { get; set; } = default!;
+
+    private UserChangePasswordRequest Model { get; set; } = new()
+    {
+        UserId = Guid.Empty,
+        NewPassword = "",
+        OldPassword = ""
+    };
+
+    protected override async Task OnLoadAsync()
+    {
+        await base.OnLoadAsync();
+        Model.UserId = Guid.Parse((await UserState).User.FindFirstValue(ClaimTypes.Sid)!);
+    }
+
+    private async Task OnSubmit()
+    {
+        await GetApiClient<AccountClient>().ChangePasswordAsync(Model);
+        NavigateBack();
+    }
+}
