2798b63c477172c8377ed61a0d6efc86f594acc8
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-05-04 18:07:49 +0330

    fix: add data annotation validations

--- src/FluentCMS.Web.Api/Models/Users/AccountUpdateRequest.cs
+++ src/FluentCMS.Web.Api/Models/Users/AccountUpdateRequest.cs
@@ -2,8 +2,13 @@
 
 public class AccountUpdateRequest
 {
+    [Required]
+    [EmailAddress]
     public required string Email { get; set; }
+
+    [Phone]
     public string? PhoneNumber { get; set; }
+
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
 }
