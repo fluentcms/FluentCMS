3c98017e53ef3c529a7fd88730363e9fc018bb6b
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-05-04 23:28:31 +0330

    feat: implement `GenerateToken` for `ApiToken`s

--- src/FluentCMS.Services/ApiTokenService.cs
+++ src/FluentCMS.Services/ApiTokenService.cs
@@ -1,4 +1,11 @@
-﻿namespace FluentCMS.Services;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace FluentCMS.Services;
 
 public interface IApiTokenService : IAutoRegisterService
 {
@@ -9,7 +16,7 @@ public interface IApiTokenService : IAutoRegisterService
     Task<ApiToken> Update(Guid tokenId, string name, string? description, bool enabled, List<Policy> policies, CancellationToken cancellationToken = default);
 }
 
-public class ApiTokenService(IApiTokenRepository apiTokenRepository) : IApiTokenService
+public class ApiTokenService(IApiTokenRepository apiTokenRepository, IOptions<JwtOptions> options) : IApiTokenService
 {
     public async Task<IEnumerable<ApiToken>> GetAll(CancellationToken cancellationToken = default)
     {
@@ -23,8 +30,37 @@ public class ApiTokenService(IApiTokenRepository apiTokenRepository) : IApiToken
 
     public async Task<ApiToken> Create(ApiToken apiToken, CancellationToken cancellationToken = default)
     {
-        return await apiTokenRepository.Create(apiToken, cancellationToken) ??
-            throw new AppException(ExceptionCodes.ApiTokenUnableToCreate);
+        apiToken =  await apiTokenRepository.Create(apiToken, cancellationToken) ??
+               throw new AppException(ExceptionCodes.ApiTokenUnableToCreate);
+        apiToken.Token = GenerateToken(apiToken);
+        await apiTokenRepository.Update(apiToken);
+        return apiToken;
+    }
+
+    private string GenerateToken(ApiToken apiToken)
+    {
+        var key = SHA512.Create().ComputeHash(Encoding.UTF8.GetBytes(options.Value.Secret));
+        var claims = GetApiKeyClaims(apiToken);
+
+        var tokenDescriptor = new SecurityTokenDescriptor()
+        {
+            Issuer = options.Value.Issuer,
+            Audience = "M2M",
+            Subject = new ClaimsIdentity(claims),
+            Expires = apiToken.ExpiredAt ?? DateTime.MaxValue,
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature),
+            NotBefore = DateTime.UtcNow,
+            IssuedAt = DateTime.UtcNow
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateJwtSecurityToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+
+    private IEnumerable<Claim>? GetApiKeyClaims(ApiToken apiToken)
+    {
+        yield return new Claim(ClaimTypes.NameIdentifier, apiToken.Id.ToString("D"));
     }
 
     public async Task<ApiToken> Update(Guid tokenId, string name, string? description, bool enabled, List<Policy> policies, CancellationToken cancellationToken = default)
