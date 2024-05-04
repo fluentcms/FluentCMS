2423429cc45207bfbe3906074bd3bf73c9ac9c42
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-05-04 16:40:16 +0330

    feat: implement `UpdateCurrent` action in `AccountController`

--- src/FluentCMS.Web.Api/Controllers/AccountController.cs
+++ src/FluentCMS.Web.Api/Controllers/AccountController.cs
@@ -60,4 +60,14 @@ public class AccountController(IMapper mapper, IUserService userService, IAuthCo
         var user = await userService.GetById(authContext.UserId, cancellationToken);
         return Ok(mapper.Map<UserDetailResponse>(user));
     }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<IApiResult<UserDetailResponse>> UpdateCurrent(AccountUpdateRequest userUpdateRequest)
+    {
+        var user = await userService.GetById(authContext.UserId);
+        mapper.Map(userUpdateRequest, user);
+        await userService.Update(user);
+        return Ok(mapper.Map<UserDetailResponse>(user));
+    }
 }
