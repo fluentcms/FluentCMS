52a3929df0c52697a0e7f46363ed000e789b6cbf
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-05-05 18:29:19 +0430

    feat: add more components

---
+++ src/FluentCMS.Web.UI.Components/Components/Tooltip/Tooltip.razor.cs
@@ -0,0 +1,26 @@
+namespace FluentCMS.Web.UI.Components
+
+public partial class Tooltip
+{
+    public ElementReference element;
+
+    private IJSObjectReference module = default!;
+
+    [Parameter]
+    public TooltipPlacement? Placement { get; set; }
+
+    public void Dispose()
+    {
+        module.InvokeVoidAsync("dispose", DotNetObjectReference.Create(this), element);
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (!firstRender) return;
+
+        module = await JS.InvokeAsync<IJSObjectReference>("import", "/_content/FluentCMS.Web.UI.Components/Components/Tooltip/Tooltip.razor.js");
+
+        // TODO: handle run time changing properties
+        await module.InvokeVoidAsync("initialize", DotNetObjectReference.Create(this), element, new { Placement = Placement?.ToString().FromPascalCaseToKebabCase() });
+    }
+}
