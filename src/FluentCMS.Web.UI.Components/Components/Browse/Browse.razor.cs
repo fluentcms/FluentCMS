c45a9015cce4b184075ff1c807f0f6195ffda862
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-05-05 16:37:31 +0430

    feat: Add Browse.razor.cs file

---
+++ src/FluentCMS.Web.UI.Components/Components/Browse/Browse.razor.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Components;
+
+namespace FluentCMS.Web.UI.Components;
+
+public partial class Browse : BaseComponent
+{    
+    [Inject]
+    private IJSRuntime JS { get; set; }
+    
+    [Parameter]
+    public string? Accept { get; set; }
+
+    [Parameter]
+    public bool Multiple { get; set; }
+
+    [Parameter]
+    public EventCallback<InputFileChangeEventArgs> OnChange { get; set; }
+
+    public ElementReference Element;
+
+    public IJSObjectReference Module = default!;
+
+    async Task HandleChange(InputFileChangeEventArgs evt)
+    {
+        OnChange.InvokeAsync(evt);
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (!firstRender) return;
+
+        Module = await JS.InvokeAsync<IJSObjectReference>("import", "/_content/FluentCMS.Web.UI.Components/Components/Browse/Browse.razor.js");
+
+        await Module.InvokeVoidAsync("initialize", DotNetObjectReference.Create(this), Element);
+    }
+}
