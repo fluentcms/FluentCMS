93e4fafb8977e0ae01df37fc7bdd392c5f2bfa41
abdolian <m.abdolian@gmail.com>
2024-05-19 20:03:24 +0330

    chore: update inputs form plugins

--- src/FluentCMS.Web.UI/Components/Form/PluginFormCheckboxGroup.razor.cs
+++ src/FluentCMS.Web.UI/Components/Form/PluginFormCheckboxGroup.razor.cs
@@ -16,4 +16,34 @@ public partial class PluginFormCheckboxGroup<TItem, TValue>
 
     [Parameter]
     public bool Vertical { get; set; }
+
+    private bool IsChecked(TItem item)
+    {
+        return Value?.Contains(GetValue(item)) ?? false;
+    }
+
+    private TValue? GetValue(TItem item)
+    {
+        if (string.IsNullOrEmpty(ValueField))
+        {
+            return default!;
+        }
+        return (TValue?)item!.GetType().GetProperty(ValueField)?.GetValue(item);
+    }
+
+    private void OnValueChange(TItem item)
+    {
+        var value = Value?.ToList() ?? new ();
+
+        if (IsChecked(item))
+        {
+            value.Remove(GetValue(item));
+        }
+        else
+        {
+            value.Add(GetValue(item));
+        }
+
+        ValueChanged.InvokeAsync(value);
+    }
 }
