ba2748ba66659bba7ee72cd133b3f5822a4dac73
abdolian <m.abdolian@gmail.com>
2024-05-19 13:59:58 +0330

    chore: Update plugin form actions

--- src/FluentCMS.Web.UI/Components/Form/PluginFormActions.razor.cs
+++ src/FluentCMS.Web.UI/Components/Form/PluginFormActions.razor.cs
@@ -9,10 +9,10 @@ public partial class PluginFormActions
     public RenderFragment? ChildContent { get; set; }
 
     [Parameter]
-    public bool? Submit { get; set; }
+    public bool Submit { get; set; }
 
     [Parameter]
-    public bool? Cancel { get; set; }
+    public bool Cancel { get; set; }
 
     protected virtual string GetBackUrl()
     {
