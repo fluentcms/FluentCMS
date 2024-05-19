ccb7474657494eb24c18988f9abf9a6d7ef8c4d7
abdolian <m.abdolian@gmail.com>
2024-05-19 09:48:40 +0330

    chore: Update checkbox group

--- src/FluentCMS.Web.UI/Components/Form/PluginFormCheckboxGroup.razor.cs
+++ src/FluentCMS.Web.UI/Components/Form/PluginFormCheckboxGroup.razor.cs
@@ -1,7 +1,19 @@
 ﻿namespace FluentCMS.Web.UI.Plugins.Components;
 
-public partial class PluginFormCheckboxGroup
+public partial class PluginFormCheckboxGroup<TItem, TValue>
 {
     [Parameter]
     public int Cols { get; set; } = 12;
+
+    [Parameter]
+    public ICollection<TItem> Data { get; set; } = [];
+
+    [Parameter]
+    public Func<TItem, string>? TextField { get; set; } = default!;
+
+    [Parameter]
+    public string? ValueField { get; set; }
+
+    [Parameter]
+    public bool Vertical { get; set; }
 }
