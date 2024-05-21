0a450cf7213b7ec23ebcf02525ea799e592f6da0
Masood Abdolian <m.abdolian@gmail.com>
2024-05-21 21:03:10 +0330

    refactor: Refactor plugin form inputs, setup plugin, and user management. (#1375)

--- src/FluentCMS.Web.UI/Components/Form/PluginFormField.razor.cs
+++ src/FluentCMS.Web.UI/Components/Form/PluginFormField.razor.cs
@@ -1,7 +1,13 @@
 ﻿namespace FluentCMS.Web.UI.Plugins.Components;
 
 public partial class PluginFormField
-{
+{ 
+    [Parameter]
+    public RenderFragment ChildContent { get; set; } = default!;
+
     [Parameter]
     public int Cols { get; set; } = 12;
+
+    [Parameter]
+    public bool Visible { get; set; } = true;
 }
--- src/FluentCMS.Web.UI/Plugins/UserManagement/UserCreatePlugin.razor.cs
+++ src/FluentCMS.Web.UI/Plugins/UserManagement/UserCreatePlugin.razor.cs
@@ -2,10 +2,18 @@ namespace FluentCMS.Web.UI.Plugins.UserManagement;
 
 public partial class UserCreatePlugin
 {
+    public const string FORM_NAME = "UserCreateForm";
+
     [SupplyParameterFromForm(FormName = FORM_NAME)]
     private UserCreateRequest Model { get; set; } = new();
 
-    public const string FORM_NAME = "UserCreateForm";
+    private List<RoleDetailResponse> Roles { get; set; } = [];
+
+    protected override async Task OnLoadAsync()
+    {
+        var rolesResponse = await GetApiClient<RoleClient>().GetAllAsync();
+        Roles = rolesResponse?.Data?.ToList() ?? [];
+    }
 
     private async Task OnSubmit()
     {
