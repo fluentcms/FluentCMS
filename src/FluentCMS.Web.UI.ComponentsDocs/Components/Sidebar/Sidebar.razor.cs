f123ad9f98cf8196d00111ce3b7b19a66c8eeb55
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-05-22 16:20:17 +0430

    clean: remove sidebar.scss from design system project

--- src/FluentCMS.Web.UI.ComponentsDocs/Components/Sidebar/Sidebar.razor.cs
+++ src/FluentCMS.Web.UI.ComponentsDocs/Components/Sidebar/Sidebar.razor.cs
@@ -17,4 +17,8 @@ public partial class Sidebar
 
     [Parameter]
     public string? Title { get; set; }
+
+    public string Class => $"h-screen transition-transform border-r border-gray-200 dark:border-gray-700 min-h-full flex flex-col overflow-y-auto -translate-x-full lg:!translate-x-0 {(Secondary ? "" : "fixed top-0 left-0 z-40 w-64 pt-14 lg:pt-0 bg-white dark:bg-gray-800")}";
+
+    public string MainClass => $"py-5 px-3 grow {(Secondary ? "w-52 pt-0" : "bg-white dark:bg-gray-800")}";
 }
