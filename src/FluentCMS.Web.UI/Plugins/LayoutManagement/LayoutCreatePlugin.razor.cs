3d28ca73b214011d11895c8538ea0e667953f150
Amir Pournasserian <amir.pournasserian@live.com>
2024-05-23 20:47:18 -0400

    #1426 extract cs files in layout plugin razor comopnents

---
+++ src/FluentCMS.Web.UI/Plugins/LayoutManagement/LayoutCreatePlugin.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace FluentCMS.Web.UI.Plugins;
+
+public partial class LayoutCreatePlugin
+{
+    public const string FORM_NAME = "LayoutCreateForm";
+
+    [SupplyParameterFromForm(FormName = FORM_NAME)]
+    private LayoutCreateRequest Model { get; set; } = new();
+
+    private async Task OnSubmit()
+    {
+        await GetApiClient<LayoutClient>().CreateAsync(Model);
+        NavigateBack();
+    }
+}
---
+++ src/FluentCMS.Web.UI/Plugins/LayoutManagement/LayoutUpdatePlugin.razor.cs
@@ -0,0 +1,27 @@
+﻿namespace FluentCMS.Web.UI.Plugins;
+public partial class LayoutUpdatePlugin
+{
+    public const string FORM_NAME = "LayoutUpdateForm";
+
+    [SupplyParameterFromForm(FormName = FORM_NAME)]
+    private LayoutUpdateRequest Model { get; set; } = new();
+
+    [SupplyParameterFromQuery(Name = "id")]
+    private Guid Id { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var layoutResponse = await GetApiClient<LayoutClient>().GetAsync(Id);
+        var layout = layoutResponse.Data;
+        Model.IsDefault = layout.IsDefault;
+        Model.Name = layout.Name;
+        Model.Head = layout.Head;
+        Model.Body = layout.Body;
+    }
+
+    private async Task OnSubmit()
+    {
+        await GetApiClient<LayoutClient>().UpdateAsync(Model);
+        NavigateBack();
+    }
+}
