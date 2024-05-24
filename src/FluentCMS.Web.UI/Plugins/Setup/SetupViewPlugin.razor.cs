4fd84a6bff6b5a8653a53e6c827f330c3a2e5f0d
Amir Pournasserian <amir.pournasserian@live.com>
2024-05-24 15:08:36 -0400

    1430 apply changes to layout setup user management plugins (#1432)
    * #1430 resolve nullable Model issue in PluginForm
    
    According to:
    https://github.com/dotnet/aspnetcore/issues/55868
    
    * #1430 resolve nullable model issue in Setup
    
    * #1430 add hidden domain field to setup plugin
    
    * #1430 resolve nullable Model issue in Layout plugin
    
    According to:
    https://github.com/dotnet/aspnetcore/issues/55868
    
    * #1430 resolve nullable Model issue in user management
    
    According to:
    https://github.com/dotnet/aspnetcore/issues/55868

--- src/FluentCMS.Web.UI/Plugins/Setup/SetupViewPlugin.razor.cs
+++ src/FluentCMS.Web.UI/Plugins/Setup/SetupViewPlugin.razor.cs
@@ -5,12 +5,7 @@ public partial class SetupViewPlugin
     public const string FORM_NAME = "SetupForm";
 
     [SupplyParameterFromForm(FormName = FORM_NAME)]
-    public SetupRequest Model { get; set; } = new()
-    {
-        Username = "admin",
-        Email = "admin@example.com",
-        Password = "Passw0rd!"
-    };
+    public SetupRequest? Model { get; set; }
 
     [Inject]
     private SetupManager SetupManager { get; set; } = default!;
@@ -21,20 +16,25 @@ public partial class SetupViewPlugin
     [CascadingParameter]
     protected HttpContext HttpContext { get; set; } = default!;
 
-    private bool Initialized { get; set; } = false;
-
-    protected override async Task OnLoadAsync()
+    protected override async Task OnParametersSetAsync()
     {
-        Initialized = await SetupManager.IsInitialized();
-        Model.AdminDomain = new Uri(NavigationManager.BaseUri).Authority;
+        if (await SetupManager.IsInitialized())
+            throw new InvalidOperationException("Setup is already complete.");
+
+        Model ??= new SetupRequest
+        {
+            Username = "admin",
+            Email = "admin@example.com",
+            Password = "Passw0rd!",
+            AdminDomain = new Uri(NavigationManager.BaseUri).Authority
+        };
     }
 
     private async Task OnSubmit()
     {
-        if (await SetupManager.Start(Model))
+        if (await SetupManager.Start(Model!))
         {
-            Initialized = await SetupManager.IsInitialized();
-            await AuthService.Login(HttpContext, Model.Username, Model.Password, true);
+            await AuthService.Login(HttpContext, Model!.Username, Model.Password, true);
             NavigateTo("/");
         }
     }
