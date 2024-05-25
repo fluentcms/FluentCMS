52d4b2866194d3321a75340a29903466d504d0bf
abdolian <m.abdolian@gmail.com>
2024-05-25 19:21:14 +0330

    feat: Add text property to the checkbox

--- src/FluentCMS.Web.UI/Components/Form/PluginFormCheckbox.razor.cs
+++ src/FluentCMS.Web.UI/Components/Form/PluginFormCheckbox.razor.cs
@@ -7,6 +7,9 @@ public partial class PluginFormCheckbox
     [Parameter]
     public int Cols { get; set; } = 12;
 
+    [Parameter]
+    public string? Text { get; set; }
+
     protected override bool TryParseValueFromString(string? value, out bool result, [NotNullWhen(false)] out string? validationErrorMessage)
     {
         throw new NotSupportedException();
