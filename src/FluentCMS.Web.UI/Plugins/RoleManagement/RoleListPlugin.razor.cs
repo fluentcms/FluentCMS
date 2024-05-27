fcd9fcc532933fe41374fa28490aa15b8ffc2e7b
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-05-27 17:34:08 +0430

    #1479 add delete role feature

--- src/FluentCMS.Web.UI/Plugins/RoleManagement/RoleListPlugin.razor.cs
+++ src/FluentCMS.Web.UI/Plugins/RoleManagement/RoleListPlugin.razor.cs
@@ -9,5 +9,25 @@ public partial class RoleListPlugin
         var rolesResponse = await GetApiClient<RoleClient>().GetAllAsync();
         Roles = rolesResponse?.Data?.ToList() ?? [];
     }
+
+    class DeleteRequest {
+        public Guid Id { get; set; } 
+    }
+
+    public string GetDeleteCode(Guid id) 
+    {
+        return $"deleteIdInput.value='{id.ToString()}'";
+    } 
+
+    public const string FORM_NAME = "DeleteRoleForm";
+
+    [SupplyParameterFromForm(FormName = FORM_NAME)]
+    DeleteRequest DeleteModel { get; set; } = new();
+
+    public async Task OnDelete() 
+    {
+        var res = await GetApiClient<RoleClient>().DeleteAsync(DeleteModel.Id);
+        NavigateBack();
+    }
 }
 
