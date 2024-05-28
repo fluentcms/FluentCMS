ebb01a635011802fa1da7222661e9e92ae051142
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-05-28 10:08:51 +0430

    #1479 Implement Delete Role feature with Javascript Confirmation

--- src/FluentCMS.Web.UI/Plugins/RoleManagement/RoleListPlugin.razor.cs
+++ src/FluentCMS.Web.UI/Plugins/RoleManagement/RoleListPlugin.razor.cs
@@ -10,5 +10,20 @@ public partial class RoleListPlugin
         var rolesResponse = await GetApiClient<RoleClient>().GetAllAsync();
         Roles = rolesResponse?.Data?.ToList() ?? [];
     }
+
+    class DeleteRequest {
+        public Guid Id { get; set; } 
+    }
+
+    private const string FORM_NAME = "DeleteRoleForm";
+
+    [SupplyParameterFromForm(FormName = FORM_NAME)]
+    private DeleteRequest DeleteModel { get; set; } = new();
+
+    private async Task OnDelete() 
+    {
+        var res = await GetApiClient<RoleClient>().DeleteAsync(DeleteModel.Id);
+        NavigateBack();
+    }
 }
 
