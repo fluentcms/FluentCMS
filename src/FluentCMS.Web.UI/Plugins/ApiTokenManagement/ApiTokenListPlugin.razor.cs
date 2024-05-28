3bc45925266fbc6d7b3ee1e427b94bfd7c9cdfe7
Hadi Ahmadi <TheHadiAhmadi@gmail.com>
2024-05-28 21:07:48 +0430

    #1496 Cleanup ApiToken plugins (#1497)

---
+++ src/FluentCMS.Web.UI/Plugins/ApiTokenManagement/ApiTokenListPlugin.razor.cs
@@ -0,0 +1,14 @@
+namespace FluentCMS.Web.UI.Plugins.ApiTokenManagement;
+
+public partial class ApiTokenListPlugin
+{
+    private List<ApiTokenDetailResponse> ApiTokens { get; set; } = [];
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        var apiTokensResponse = await GetApiClient<ApiTokenClient>().GetAllAsync();
+        ApiTokens = apiTokensResponse?.Data?.ToList() ?? [];
+    }
+}
+
