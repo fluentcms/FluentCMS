23aa780ac211a41266f1455e8989f968d205d7ff
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-05-28 16:39:24 +0430

    #1479 Implement Delete role feature using interactiveServer based confirmation

--- src/FluentCMS.Web.UI/Plugins/RoleManagement/RoleListPlugin.razor.cs
+++ src/FluentCMS.Web.UI/Plugins/RoleManagement/RoleListPlugin.razor.cs
@@ -2,13 +2,30 @@ namespace FluentCMS.Web.UI.Plugins.RoleManagement;
 
 public partial class RoleListPlugin
 {
+    [Inject]
+    public ConfirmService? ConfirmService { get; set; }
+
     private List<RoleDetailResponse> Roles { get; set; } = [];
+
+    public async Task Load() {
+        var rolesResponse = await GetApiClient<RoleClient>().GetAllAsync();
+        Roles = rolesResponse?.Data?.ToList() ?? [];
+    }
 
     protected override async Task OnInitializedAsync()
     {
         await base.OnInitializedAsync();
-        var rolesResponse = await GetApiClient<RoleClient>().GetAllAsync();
-        Roles = rolesResponse?.Data?.ToList() ?? [];
+        await Load();
+    }
+
+    public async Task OnDelete(RoleDetailResponse role)
+    {
+        var result = await ConfirmService!.Show("Are you sure to remove this Role?");
+
+        if(result) {
+            await GetApiClient<RoleClient>().DeleteAsync(role.Id);
+            await Load();
+        }
     }
 }
 
