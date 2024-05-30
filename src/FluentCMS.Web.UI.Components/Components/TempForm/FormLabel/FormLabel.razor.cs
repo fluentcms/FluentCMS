3e3271f5b90b6e351ffc39ef8f298cafa8211727
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-05-30 14:51:36 +0430

    #1512 Add FormLabel component and refactor PluginForm components

---
+++ src/FluentCMS.Web.UI.Components/Components/TempForm/FormLabel/FormLabel.razor.cs
@@ -0,0 +1,10 @@
+namespace FluentCMS.Web.UI.Components;
+
+public partial class FormLabel
+{
+    [Parameter]
+    public string? Id { get; set; }
+
+    [Parameter]
+    public bool Required { get; set; }
+}
