ed97d1c3b2627ab46d9e3f3fafd5849b65a71430
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-06-02 14:28:19 +0430

    #1406 Handle Empty State for DataTable

--- src/FluentCMS.Web.UI.Components/Components/DataTable/DataTable.razor.cs
+++ src/FluentCMS.Web.UI.Components/Components/DataTable/DataTable.razor.cs
@@ -6,6 +6,12 @@ public partial class DataTable<TItem>
     public IReadOnlyList<TItem>? Items { get; set; }
 
     [Parameter]
+    public RenderFragment EmptyStateFragment { get; set; }
+    
+    [Parameter]
+    public RenderFragment ColumnsFragment { get; set; }
+
+    [Parameter]
     [CSSProperty]
     public bool Hoverable { get; set; } = true;
 }
