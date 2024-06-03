3774c21a0922a90f16377d765e2ad93f955e903c
Amir Pournasserian <amir.pournasserian@live.com>
2024-06-03 11:30:59 -0400

    #1592 add meta to content type field

--- src/Backend/FluentCMS.Entities/ContentTypeField.cs
+++ src/Backend/FluentCMS.Entities/ContentTypeField.cs
@@ -4,8 +4,4 @@ public class ContentTypeField
 {
     public string Name { get; set; } = default!;
     public string Type { get; set; } = default!;
-    public string Label { get; set; } = default!;
-    public string? Description { get; set; }
-    public string? Placeholder { get; set; }
-    public bool Required { get; set; }
 }
