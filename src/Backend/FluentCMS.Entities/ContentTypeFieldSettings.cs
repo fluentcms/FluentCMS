2d8bba861409982ed50adf5c3c1f688063ca1c34
Hadi Ahmadi <TheHadiAhmadi@gmail.com>
2024-06-05 14:49:45 +0430

    #1605 Add UI of `String` field type

--- src/Backend/FluentCMS.Entities/ContentTypeFieldSettings.cs
+++ src/Backend/FluentCMS.Entities/ContentTypeFieldSettings.cs
@@ -5,5 +5,7 @@ public class ContentTypeFieldSettings
     public string Label { get; set; } = default!;
     public string? Description { get; set; }
     public bool Required { get; set; }
+    public int Cols { get; set; }
+    public int Order { get; set; }
     public Dictionary<string, object>? Meta { get; set; }
 }
--- src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.ContentTypeManagement/Components/ContentTypeFieldCreate.razor.cs
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.ContentTypeManagement/Components/ContentTypeFieldCreate.razor.cs
@@ -17,6 +17,47 @@ public partial class ContentTypeFieldCreate
 
     private FieldTypes FieldTypes { get; set; } = [];
 
+    #region Text 
+
+    private string MetaViewType { get; set; }
+    private string MetaPattern { get; set; }
+    private bool MetaUnique { get; set; }
+    private bool MetaMinimumEnabled { get; set; }
+    private int MetaMinimum { get; set; }
+    private bool MetaMaximumEnabled { get; set; }
+    private int MetaMaximum { get; set; }
+    private string MetaDefaultValue { get; set; }
+ 
+    class SelectItem {
+        public string Key { get; set; }
+        public string Text { get; set; }
+    };
+
+    private List<SelectItem> TextViewTypes = new List<SelectItem> {
+        new()
+        {
+            Key="input",
+            Text="Input"
+        },
+        new()
+        {
+            Key="textarea",
+            Text="Textarea"
+        },
+        new()
+        {
+            Key="markdown",
+            Text="Markdown"
+        },
+        new()
+        {
+            Key="rich-text",
+            Text="Rich Text"
+        },
+    };
+
+    #endregion
+
     protected override async Task OnInitializedAsync()
     {
         await base.OnInitializedAsync();
@@ -34,6 +75,15 @@ public partial class ContentTypeFieldCreate
     private async Task OnTypeSelect(FieldType type)
     {
         Model!.Type = type.Key;
+
+        // Model!.Settings.Meta.DefaultValue = MetaDefaultValue;
+        // Model!.Settings.Meta.Unique = MetaUnique;
+        // Model!.Settings.Meta.MinimumEnabled = MetaMinimumEnabled;
+        // Model!.Settings.Meta.Minimum = MetaMinimum;
+        // Model!.Settings.Meta.MaximumEnabled = MetaMaximumEnabled;
+        // Model!.Settings.Meta.Pattern = MetaPattern;
+        // Model!.Settings.Meta.ViewType = MetaViewType;
+        
         FieldType = type;
         await Task.CompletedTask;
     }
