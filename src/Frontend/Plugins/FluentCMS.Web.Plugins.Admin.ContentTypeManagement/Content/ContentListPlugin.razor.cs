e95cf25b68013c812b0905f588c7688b6178ad4e
Amir Pournasserian <amir.pournasserian@live.com>
2024-06-07 17:55:32 -0400

    #1650 implement content deletion

--- src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.ContentTypeManagement/Content/ContentListPlugin.razor.cs
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.ContentTypeManagement/Content/ContentListPlugin.razor.cs
@@ -8,9 +8,43 @@ public partial class ContentListPlugin
         {
             var contentTypeResponse = await GetApiClient<ContentTypeClient>().GetBySlugAsync(ContentTypeSlug);
             ContentType = contentTypeResponse?.Data;
+            await Load();
+        }
+    }
 
+    private async Task Load()
+    {
+        if (!string.IsNullOrEmpty(ContentTypeSlug) && ContentType != null)
+        {
             var contentsResponse = await GetApiClient<ContentClient>().GetAllAsync(ContentTypeSlug);
             Contents = contentsResponse?.Data?.ToList() ?? [];
         }
     }
+
+    #region Delete Content
+
+    private Guid? SelectedContentId { get; set; }
+
+    private async Task OnDelete()
+    {
+        if (SelectedContentId == null)
+            return;
+
+        await GetApiClient<ContentClient>().DeleteAsync(ContentTypeSlug!, SelectedContentId.Value);
+        await Load();
+        SelectedContentId = default;
+    }
+
+    private async Task OnConfirm(Guid contentId)
+    {
+        SelectedContentId = contentId;
+        await Task.CompletedTask;
+    }
+    private async Task OnConfirmClose()
+    {
+        SelectedContentId = default;
+        await Task.CompletedTask;
+    }
+
+    #endregion
 }
