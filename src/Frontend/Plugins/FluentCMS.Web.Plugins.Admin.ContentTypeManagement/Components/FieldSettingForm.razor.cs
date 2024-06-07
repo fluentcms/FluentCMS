894a2c7f1fbfc7da4299058284143d9cc2934a46
Amir Pournasserian <amir.pournasserian@live.com>
2024-06-07 14:04:45 -0400

    #1646 refactor form for field settings (#1647)

---
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.ContentTypeManagement/Components/FieldSettingForm.razor.cs
@@ -0,0 +1,27 @@
+﻿namespace FluentCMS.Web.Plugins.Admin.ContentTypeManagement;
+
+public partial class FieldSettingForm<TModel>
+{
+    [Parameter, EditorRequired]
+    public TModel Model { get; set; } = default!;
+
+    [CascadingParameter]
+    public ContentTypeField? ContentTypeField { get; set; }
+
+    [CascadingParameter]
+    protected FieldManagementState CurrentState { get; set; }
+
+    [CascadingParameter]
+    protected FieldType? FieldType { get; set; }
+
+    [Parameter, EditorRequired]
+    public EventCallback OnCancel { get; set; }
+
+    [Parameter, EditorRequired]
+    public EventCallback<ContentTypeField> OnSubmit { get; set; }
+
+    protected async Task OnFormSubmit()
+    {
+        await OnSubmit.InvokeAsync(Model?.ToContentTypeField());
+    }
+}
