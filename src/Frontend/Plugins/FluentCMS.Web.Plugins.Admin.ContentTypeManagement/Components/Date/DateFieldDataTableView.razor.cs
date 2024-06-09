d73e0f7c2c6d0a10efa65ce13b0ce698703d7a7c
Amir Pournasserian <amir.pournasserian@live.com>
2024-06-09 12:47:59 -0400

    #1662 resolve json desterilize issue in client api for date string

--- src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.ContentTypeManagement/Components/Date/DateFieldDataTableView.razor.cs
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.ContentTypeManagement/Components/Date/DateFieldDataTableView.razor.cs
@@ -2,24 +2,19 @@
 
 public partial class DateFieldDataTableView
 {
+    [Parameter]
+    public DateTime? Value { get; set; }
+
     [CascadingParameter]
     private ContentTypeField? ContentTypeField { get; set; }
 
     private DateFieldModel? FieldModel { get; set; }
-
-    private DateTime? _value { get; set; }
 
     protected override Task OnInitializedAsync()
     {
         if (ContentTypeField is not null)
             FieldModel = ContentTypeField.ToFieldModel<DateFieldModel>();
 
-        if (Value != null)
-            _value = DateTime.Parse(Value);
-
         return base.OnInitializedAsync();
     }
-
-    [Parameter]
-    public string? Value { get; set; }
 }
