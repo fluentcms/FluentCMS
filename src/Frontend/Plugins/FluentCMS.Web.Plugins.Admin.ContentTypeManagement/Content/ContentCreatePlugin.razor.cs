0ff13047ac97bf0f34833452113e5225139509b4
Amir Pournasserian <amir.pournasserian@live.com>
2024-06-09 10:07:03 -0400

    #1661 initial dictionary binding to content create form

--- src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.ContentTypeManagement/Content/ContentCreatePlugin.razor.cs
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.ContentTypeManagement/Content/ContentCreatePlugin.razor.cs
@@ -1,5 +1,3 @@
-using System.Reflection;
-
 namespace FluentCMS.Web.Plugins.Admin.ContentTypeManagement.Content;
 
 public partial class ContentCreatePlugin
@@ -7,20 +5,61 @@ public partial class ContentCreatePlugin
     public const string FORM_NAME = "ContentCreateForm";
 
     [SupplyParameterFromForm(FormName = FORM_NAME)]
-    private ContentCreateRequest Model { get; set; } = new();
+    private ContentCreateRequest? Model { get; set; }
+
+    private Dictionary<string, string?> StringValueDict { get; set; } = [];
+    private Dictionary<string, decimal?> DecimalValueDict { get; set; } = [];
+    private Dictionary<string, bool> BooleanValueDict { get; set; } = [];
+    private Dictionary<string, DateTime?> DateValueDict { get; set; } = [];
 
     protected override async Task OnInitializedAsync()
     {
-        Model ??= new ContentCreateRequest();
         if (ContentType is null && !string.IsNullOrEmpty(ContentTypeSlug))
         {
             var contentTypeResponse = await GetApiClient<ContentTypeClient>().GetBySlugAsync(ContentTypeSlug!);
             ContentType = contentTypeResponse.Data;
+
+            if (ContentType is null)
+                return;
+
+            Model = new ContentCreateRequest() { Value = new Dictionary<string, object?>() };
+
+            foreach (var field in ContentType.Fields ?? [])
+            {
+                switch (field.Type)
+                {
+                    case FieldTypes.STRING:
+                        StringValueDict[field.Name!] = null;
+                        break;
+
+                    case FieldTypes.NUMBER:
+                        DecimalValueDict[field.Name!] = null;
+                        break;
+
+                    case FieldTypes.BOOLEAN:
+                        BooleanValueDict[field.Name!] = false;
+                        break;
+
+                    case FieldTypes.DATE:
+                        DateValueDict[field.Name!] = null;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
         }
     }
 
     private async Task OnSubmit()
     {
+        Model ??= new ContentCreateRequest();
+        Model.Value = new Dictionary<string, object?>();
+        // add string values
+        foreach (var (key, value) in StringValueDict)
+        {
+            Model.Value[key] = value;
+        }
         await GetApiClient<ContentClient>().CreateAsync(ContentTypeSlug!, Model);
         NavigateBack();
     }
@@ -33,26 +72,26 @@ public partial class ContentCreatePlugin
         return [.. ContentType.Fields.OrderBy(x => x.GetDecimal(nameof(IFieldModel.FormViewOrder)))];
     }
 
-    private Type GetFormFieldViewType(ContentTypeField contentTypeField)
-    {
-        var typeName = contentTypeField.GetString(nameof(IFieldModel.FormViewComponent));
+    //private Type GetFormFieldViewType(ContentTypeField contentTypeField)
+    //{
+    //    var typeName = contentTypeField.GetString(nameof(IFieldModel.FormViewComponent));
 
-        // find view type by name in this assembly
-        var viewType = Assembly.GetExecutingAssembly().GetTypes().FirstOrDefault(x => x.Name == typeName);
+    //    // find view type by name in this assembly
+    //    var viewType = Assembly.GetExecutingAssembly().GetTypes().FirstOrDefault(x => x.Name == typeName);
 
-        return viewType ?? typeof(StringFieldFormText);
-    }
+    //    return viewType ?? typeof(StringFieldFormText);
+    //}
 
-    private static IDictionary<string, object> GetParameters(ContentTypeField field)
-    {
-        return new Dictionary<string, object>();
-        if (field == null)
-            return new Dictionary<string, object>();
+    //private static IDictionary<string, object> GetParameters(ContentTypeField field)
+    //{
+    //    return new Dictionary<string, object>();
+    //    if (field == null)
+    //        return new Dictionary<string, object>();
 
-        return new Dictionary<string, object>
-        {
-            { "ContentTypeField", field }
-        };
-    }
+    //    return new Dictionary<string, object>
+    //    {
+    //        { "ContentTypeField", field }
+    //    };
+    //}
 
 }
