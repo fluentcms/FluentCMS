316e6d08b54a82d05cda8f10483c22a8967ccdf4
Amir Pournasserian <amir.pournasserian@live.com>
2024-06-11 18:25:20 -0400

    #1661 initial dictionary binding to content create form (#1671)
    * #1661 initial dictionary binding to content create form
    
    * #1661 major refactor for rendering content type and content
    
    * #1661 run code analyzer
    
    * #1661 resolve create/update field issue
    
    * #1661 add field components to fieldtypes
    
    * #1661 remove unused files
    
    * #1661 add field value model
    
    * #1683 fix content type field CRUD after refactor (#1685)
    
    * #1661 refactor content list and create
    
    * #1661 refactor models
    
    * #1661 Feat update content form and add update content page (#1687)
    
    * #1661 Update design of create content page and add update content page
    
    * #1687 cleanup
    
    * #1687 cleanup
    
    * #1687 run code analyzer
    
    * #1687 update content update plugin
    
    ---------
    
    Co-authored-by: Amir Pournasserian <amir.pournasserian@live.com>
    
    ---------
    
    Co-authored-by: Hadi Ahmadi <TheHadiAhmadi@gmail.com>

--- src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.ContentTypeManagement/Components/Number/NumberFieldModel.cs
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.ContentTypeManagement/Components/Number/NumberFieldModel.cs
@@ -1,20 +1,12 @@
 ﻿namespace FluentCMS.Web.Plugins.Admin.ContentTypeManagement;
 
-public class NumberFieldModel : IFieldModel
+public class NumberFieldModel : FieldModel<decimal?>
 {
-    public string Name { get; set; } = string.Empty;
-    public string Type { get; set; } = FieldTypes.NUMBER;
+    public override string Type { get; set; } = FieldTypes.NUMBER;
     public string Label { get; set; } = string.Empty;
     public bool Required { get; set; }
     public string? Description { get; set; }
     public string? Placeholder { get; set; }
     public decimal? MinimumValue { get; set; }
     public decimal? MaximumValue { get; set; }
-    public decimal DataTableColumnOrder { get; set; }
-    public bool DataTableVisible { get; set; }
-    public string DataTableViewComponent { get; set; } = nameof(NumberFieldDataTableView);
-    public decimal FormViewOrder { get; set; }
-    public decimal FormColWidth { get; set; } = 12;
-    public string FormViewComponent { get; set; } = nameof(NumberFieldFormInput);
-    public decimal DefaultValue { get; set; }
 }
---
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.ContentTypeManagement/Models/FieldTypes.cs
@@ -0,0 +1,122 @@
+﻿namespace FluentCMS.Web.Plugins.Admin.ContentTypeManagement;
+
+public class FieldTypes
+{
+    public const string STRING = "string";
+    public const string NUMBER = "decimal";
+    public const string BOOLEAN = "bool";
+    public const string ENUMERATION = "enumeration";
+    public const string DATE_TIME = "datetime";
+    public const string MEDIA = "media";
+
+    public static Type GetSettingType(string fieldTypeKey) =>
+        All[fieldTypeKey].SettingViewType;
+
+    public static Dictionary<string, FieldType> All = new()
+    {
+        {
+            STRING,
+            new FieldType
+            {
+                Icon = IconName.Text,
+                Title = "String",
+                Description = "Small or long text like title or description",
+                Key = STRING,
+                Order = 1,
+                SettingViewType = typeof(StringFieldSettings),
+                FormComponents =
+                [
+                    new FieldType.Component("Input", typeof(StringFieldFormText)),
+                    new FieldType.Component("TextArea", typeof(StringFieldFormTextArea)),
+                    new FieldType.Component("Rich Text Editor", typeof(StringFieldFormRichText)),
+                    new FieldType.Component("Markdown Editor", typeof(StringFieldFormMarkdownText))
+                ],
+                DataTableComponents =
+                [
+                    new FieldType.Component("Raw Text", typeof(StringFieldDataTableView))
+                ]
+            }
+        },
+        {
+            NUMBER,
+            new FieldType
+            {
+                Title = "Number",
+                Icon = IconName.Number,
+                Description = "Numbers (integer, float, decimal)",
+                Key = NUMBER,
+                Order = 2,
+                SettingViewType = typeof(NumberFieldSettings),
+                FormComponents =
+                [
+                    new FieldType.Component("Number Input", typeof(NumberFieldFormInput)),
+                    new FieldType.Component("Range Input", typeof(NumberFieldFormRange)),
+                ],
+                DataTableComponents =
+                [
+                    new FieldType.Component("Raw Text", typeof(NumberFieldDataTableView))
+                ]
+            }
+        },
+        {
+            BOOLEAN,
+            new FieldType
+            {
+                Title = "Boolean",
+                Icon = IconName.Boolean,
+                Description = "Yes or no, 1 or 0, true or false",
+                Key = BOOLEAN,
+                Order = 3,
+                SettingViewType = typeof(BooleanFieldSettings),
+                FormComponents =
+                [
+                    new FieldType.Component("Switch", typeof(BooleanFieldFormSwitch)),
+                    new FieldType.Component("Checkbox", typeof(BooleanFieldFormCheckbox)),
+                ],
+                DataTableComponents =
+                [
+                    new FieldType.Component("True/False", typeof(BooleanFieldDataTableTrueFalse)),
+                    new FieldType.Component("Yes/No", typeof(BooleanFieldDataTableYesNo)),
+                    new FieldType.Component("Switch", typeof(BooleanFieldDataTableSwitch)),
+                    new FieldType.Component("Indicator", typeof(BooleanFieldDataTableIndicator)),
+                ]
+            }
+        },
+        //{ENUMERATION, new FieldType
+        //{
+        //    Title = "Enumeration",
+        //    Icon = IconName.List,
+        //    Description = "List of values, then pick one",
+        //    Key = ENUMERATION,
+        //    Order = 4
+        //}},
+        {
+            DATE_TIME,
+            new FieldType
+            {
+                Title = "Date Time",
+                Icon = IconName.Clock,
+                Description = "A date picker with hours, minutes and seconds",
+                Key = DATE_TIME,
+                Order = 5,
+                SettingViewType = typeof(DateFieldSettings),
+                FormComponents =
+                [
+                    new FieldType.Component("Input", typeof(DateFieldFormInput))
+                ],
+                DataTableComponents =
+                [
+                    new FieldType.Component("Text", typeof(DateFieldDataTableView)),
+                ]
+            }
+        }
+        //{MEDIA, new FieldType
+        //{
+        //    Title = "Media",
+        //    Icon = IconName.ClipboardList,
+        //    Description = "Files like images, videos, etc",
+        //    Key = MEDIA,
+        //    Order = 6
+        //}}
+    };
+}
