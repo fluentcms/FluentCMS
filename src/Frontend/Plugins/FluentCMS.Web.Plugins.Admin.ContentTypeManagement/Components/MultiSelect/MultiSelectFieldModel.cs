587749e9e20f3af4c86254a3290c64980350276f
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-06-12 08:56:06 +0430

    #1665 Cleanup Multi select field type

--- src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.ContentTypeManagement/Components/MultiSelect/MultiSelectFieldModel.cs
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.ContentTypeManagement/Components/MultiSelect/MultiSelectFieldModel.cs
@@ -1,19 +1,10 @@
 ﻿namespace FluentCMS.Web.Plugins.Admin.ContentTypeManagement;
 
-public class MultiSelectFieldModel : IFieldModel
+public class MultiSelectFieldModel : FieldModel<ICollection<string>>
 {
-    public string Name { get; set; } = string.Empty;
-    public string Type { get; set; } = FieldTypes.MULTI_SELECT;
-    public string Label { get; set; } = string.Empty;
+    public override string Type { get; set; } = FieldTypes.MULTI_SELECT;
     public bool Required { get; set; }
+    public string Label { get; set; } = string.Empty;
     public string? Description { get; set; }
-    public string? Placeholder { get; set; }
-    public decimal DataTableColumnOrder { get; set; }
-    public bool DataTableVisible { get; set; }
-    public string DataTableViewComponent { get; set; } = nameof(MultiSelectFieldDataTableBadges);
-    public decimal FormViewOrder { get; set; }
-    public decimal FormColWidth { get; set; }
-    public string FormViewComponent { get; set; } = nameof(MultiSelectFieldFormCheckboxes);
-    public ICollection<string> DefaultValue { get; set; } = [];
     public string Options { get; set; }
 }
--- src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.ContentTypeManagement/Components/MultiSelect/MultiSelectFieldSettings.razor.cs
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.ContentTypeManagement/Components/MultiSelect/MultiSelectFieldSettings.razor.cs
@@ -2,16 +2,6 @@ namespace FluentCMS.Web.Plugins.Admin.ContentTypeManagement;
 
 public partial class MultiSelectFieldSettings
 {
-    private List<ComponentTypeOption> FormViewTypes
-    {
-        get => [new(nameof(MultiSelectFieldFormCheckboxes), "Checkboxes")];
-    }
-
-    private List<ComponentTypeOption> TableViewTypes
-    {
-        get => [new(nameof(MultiSelectFieldDataTableBadges), "Badges")];
-    }
-
     private List<string> Options {
         get => Model.Options.Split("\n").Select(x => x.Trim()).ToList();
     }
