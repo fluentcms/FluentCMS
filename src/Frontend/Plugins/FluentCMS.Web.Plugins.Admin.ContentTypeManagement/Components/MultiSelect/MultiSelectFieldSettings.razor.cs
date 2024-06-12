d24e009466be4f84e8aad4b3cdbaf1a126d6c8df
Hadi Ahmadi <TheHadiAhmadi@gmail.com>
2024-06-12 18:43:18 +0430

    #1680 Add RadioGroup component (#1690)
    * #1680 Add RadioGroup component
    
    * Update FormRadioGroup.razor.cs
    
    * #1680 run code analyser
    
    ---------
    
    Co-authored-by: Amir Pournasserian <amir.pournasserian@live.com>

--- src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.ContentTypeManagement/Components/MultiSelect/MultiSelectFieldSettings.razor.cs
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.ContentTypeManagement/Components/MultiSelect/MultiSelectFieldSettings.razor.cs
@@ -2,7 +2,8 @@ namespace FluentCMS.Web.Plugins.Admin.ContentTypeManagement;
 
 public partial class MultiSelectFieldSettings
 {
-    private List<string> Options {
+    private List<string> Options
+    {
         get => Model.Options.Split("\n").Select(x => x.Trim()).ToList();
     }
 }
