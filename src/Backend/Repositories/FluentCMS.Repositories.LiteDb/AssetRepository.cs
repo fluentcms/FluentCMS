13ee075773e2705be5d0727aad6bcbddf2e49d07
Amir Pournasserian <amir.pournasserian@live.com>
2024-06-12 18:02:38 -0400

    Update AssetRepository.cs

--- src/Backend/Repositories/FluentCMS.Repositories.LiteDb/AssetRepository.cs
+++ src/Backend/Repositories/FluentCMS.Repositories.LiteDb/AssetRepository.cs
@@ -1,8 +1,17 @@
-﻿namespace FluentCMS.Repositories.LiteDb;
+﻿
+namespace FluentCMS.Repositories.LiteDb;
 
 public class AssetRepository : AuditableEntityRepository<Asset>, IAssetRepository
 {
     public AssetRepository(ILiteDBContext liteDbContext, IAuthContext authContext) : base(liteDbContext, authContext)
     {
+    }
+
+    public async Task<IEnumerable<Asset>> GetByParentId(Guid? parentId, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var findResult = await Collection.Query().Where(x => x.Id == parentId).ToListAsync();
+        return findResult.ToList();
     }
 }
