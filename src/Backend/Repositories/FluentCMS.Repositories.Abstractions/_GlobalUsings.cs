32b1caf7482cd0e7f7ad7888f99ae0dba94e3b34
Amir Pournasserian <amir.pournasserian@live.com>
2024-06-16 10:28:21 -0400

    Update _GlobalUsings.cs

--- src/Backend/Repositories/FluentCMS.Repositories.Abstractions/_GlobalUsings.cs
+++ src/Backend/Repositories/FluentCMS.Repositories.Abstractions/_GlobalUsings.cs
@@ -1,5 +1,6 @@
 ﻿global using FluentCMS.Entities;
 global using System;
+global using System.Linq;
 global using System.Collections.Generic;
 global using System.Threading;
 global using System.Threading.Tasks;
