c9d9054e209ff5397b6feac1ec017f415f499192
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-06-18 00:05:39 +0430

    #1718 Disable Multiple file upload

--- src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.FileManagement/Files/FileUploadModal.razor.cs
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin.FileManagement/Files/FileUploadModal.razor.cs
@@ -11,32 +11,28 @@ public partial class FileUploadModal
     [Parameter]
     public EventCallback OnCancel { get; set; }
 
-    private List<FileParameter> Files { get; set; }
+    private string Model { get; set; } = string.Empty;
 
+    private List<FileParameter> Files { get; set; }
 
     private async Task OnFilesChanged(InputFileChangeEventArgs e)
     {
-        Console.WriteLine($"OnFilesChanged {Config.MaxCount}");
         Files = [];
         foreach (var file in e.GetMultipleFiles(Config.MaxCount))
         {
-            Console.WriteLine("file");
-
             var Data = file.OpenReadStream(Config.MaxSize);
             Files.Add(new FileParameter(Data, file.Name, file.ContentType));
         }
-        Console.WriteLine("end loop");
-
-
-        // await GetApiClient<FileClient>().UploadAsync(FolderId, Files);
-        // NavigateTo(GetUrl("Files List", new { folderId = FolderId }));
-
-        Console.WriteLine(Files.Select(x => x.FileName).ToList().Count);
         await OnSubmit.InvokeAsync(Files);
     }
 
     private async Task HandleCancel()
     {
         await OnCancel.InvokeAsync();
+    }
+
+    private async Task HandleSubmit()
+    {
+        await OnSubmit.InvokeAsync(Files);
     }
 }
