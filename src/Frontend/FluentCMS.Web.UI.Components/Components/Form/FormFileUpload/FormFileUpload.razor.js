93310033f15e8fa49cd8f5bc60053f7480e61f32
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-06-19 12:02:18 +0430

    #1730 Add FormFileUpload and remove Browse component

---
+++ src/Frontend/FluentCMS.Web.UI.Components/Components/Form/FormFileUpload/FormFileUpload.razor.js
@@ -0,0 +1,47 @@
+﻿const fileUploads = new Map();
+
+function onDragHover(event) {
+    event.preventDefault();
+}
+
+function onDragLeave(event) {
+    event.preventDefault();
+}
+
+function onDrop(event) {
+    event.preventDefault();
+
+    const input = event.currentTarget.querySelector('input');
+
+    input.files = event.dataTransfer.files;
+
+    const evt = new Event('change', { bubbles: true });
+
+    input.dispatchEvent(evt);
+}
+
+export function initialize(dotnet, element) {
+    dispose(dotnet, element);
+
+    const fileUpload = element;
+ 
+    fileUpload.addEventListener("dragenter", onDragHover);
+    fileUpload.addEventListener("dragover", onDragHover);
+    fileUpload.addEventListener("dragleave", onDragLeave);
+    fileUpload.addEventListener("drop", onDrop);
+
+    fileUploads.set(element, fileUpload);
+}
+
+export function dispose(dotnet, element) {
+    const fileUpload = fileUploads.get(element);
+
+    if(!fileUpload) return;
+
+    fileUpload.removeEventListener("dragenter", onDragHover);
+    fileUpload.removeEventListener("dragover", onDragHover);
+    fileUpload.removeEventListener("dragleave", onDragLeave);
+    fileUpload.removeEventListener("drop", onDrop);
+
+    fileUploads.delete(element);
+}
