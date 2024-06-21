b8c19d5061e75a001ced514a49022ce6adfa5061
Amir Pournasserian <amir.pournasserian@live.com>
2024-06-21 09:37:51 -0400

    Enhance UI with new components and attributes
    - Added `PluginContainerActions.razor` to FluentCMS.Web.UI.csproj for runtime modification.
    - Enhanced `PluginContainer.razor` with `data-id` for plugin identification and added `<PluginContainerActions />` for interactive plugin management.
    - Updated `PluginsSection.razor` with `data-name` for improved section manageability.
    - Introduced `PageEditorSidebar` component for displaying plugin definitions and added `PageEditorToggler.razor` for UI mode toggling.
    - Created `PluginContainerActions.razor` component for direct plugin interaction through UI.
    - Minor placeholder changes in CSS and JavaScript files to support the above enhancements.

---
+++ src/Frontend/FluentCMS.Web.UI/PageEditorSidebar.razor.cs
@@ -0,0 +1,25 @@
+﻿using FluentCMS.Web.ApiClients;
+using Microsoft.AspNetCore.Components;
+
+namespace FluentCMS.Web.UI;
+public partial class PageEditorSidebar
+{
+    [Inject]
+    protected UserLoginResponse? UserLogin { get; set; }
+
+    [Inject]
+    protected IHttpClientFactory HttpClientFactory { get; set; } = default!;
+
+    private ICollection<PluginDefinitionDetailResponse>? PluginDefinitions { get; set; } = default!;
+
+    protected TClient GetApiClient<TClient>() where TClient : class, IApiClient
+    {
+        return HttpClientFactory.CreateApiClient<TClient>(UserLogin);
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var response = await GetApiClient<PluginDefinitionClient>().GetAllAsync();
+        PluginDefinitions= response.Data;
+    }
+}
