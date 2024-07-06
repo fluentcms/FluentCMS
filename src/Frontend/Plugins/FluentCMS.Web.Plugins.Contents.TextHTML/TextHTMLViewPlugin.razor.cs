75c7fb95014e5a5d6ef4a7a6dde0f96ca23f3b25
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-07-06 19:13:29 +0430

    #1781 remove Plugin components

--- src/Frontend/Plugins/FluentCMS.Web.Plugins.Contents.TextHTML/TextHTMLViewPlugin.razor.cs
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Contents.TextHTML/TextHTMLViewPlugin.razor.cs
@@ -4,6 +4,35 @@ namespace FluentCMS.Web.Plugins.Contents.TextHTML;
 
 public partial class TextHTMLViewPlugin
 {
+    [Inject]
+    protected NavigationManager NavigationManager { get; set; } = default!;
+
+    [Parameter]
+    public string? SectionName { get; set; }
+
+    [Parameter]
+    public PluginViewState? Plugin { get; set; } = default!;
+
+    [Inject]
+    protected ApiClientFactory ApiClient { get; set; } = default!;
+
+    [Inject]
+    protected IHttpContextAccessor? HttpContextAccessor { get; set; }
+
+    protected virtual void NavigateBack()
+    {
+        var url = new Uri(NavigationManager.Uri).LocalPath;
+        NavigateTo(url);
+    }
+
+    protected virtual void NavigateTo(string path)
+    {
+        if (HttpContextAccessor?.HttpContext != null && !HttpContextAccessor.HttpContext.Response.HasStarted)
+            HttpContextAccessor.HttpContext.Response.Redirect(path);
+        else
+            NavigationManager.NavigateTo(path);
+    }
+
     private TextHTMLContent? Content { get; set; }
 
     protected override async Task OnInitializedAsync()
