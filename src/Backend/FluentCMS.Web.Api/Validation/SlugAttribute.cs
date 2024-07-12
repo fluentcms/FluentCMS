47e2823e92051acc698780022c3cd4c675d5675d
Vahid <Vahid.jafari@outlook.com>
2024-07-12 16:37:43 +0330

    replace new regex with static generatedRegex attribute to care about mem performance

--- src/Backend/FluentCMS.Web.Api/Validation/SlugAttribute.cs
+++ src/Backend/FluentCMS.Web.Api/Validation/SlugAttribute.cs
@@ -2,22 +2,21 @@ using System.Text.RegularExpressions;
 
 namespace FluentCMS.Web.Api.Validation;
 
-public class SlugAttribute : ValidationAttribute
+public partial class SlugAttribute : ValidationAttribute
 {
-    public static readonly Regex SlugRegex = new(@"^[a-z0-9]+(?:-[a-z0-9]+)*$", RegexOptions.Compiled);
+    [GeneratedRegex(@"^[a-z0-9]+(?:-[a-z0-9]+)*$", RegexOptions.Compiled)]
+    private static partial Regex MyRegex();
+
+    private static readonly Regex _slugRegex = MyRegex();
 
     protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
     {
-        if (value is null || value is not string slug)
-        {
+        if (value is not string slug)
             return ValidationResult.Success!;
-        }
 
-        if (!SlugRegex.IsMatch(slug))
-        {
-            return new ValidationResult("Invalid slug format. Slug can only contain lowercase letters, numbers, and hyphens, and must start and end with a letter or number.");
-        }
-
-        return ValidationResult.Success!;
+        return !_slugRegex.IsMatch(slug) ?
+            new ValidationResult("Invalid slug format. Slug can only contain lowercase letters," +
+                                 " numbers, and hyphens, and must start and end with a letter or number.") :
+            ValidationResult.Success!;
     }
 }
