83e3d5ea0f2938a1c3913a846225914822e959a6
Amir Pournasserian <amir.pournasserian@live.com>
2024-07-15 12:07:35 -0400

    Reverting a few changes

--- src/Frontend/FluentCMS.Web.ApiClients/ApiClientServiceExtensions.cs
+++ src/Frontend/FluentCMS.Web.ApiClients/ApiClientServiceExtensions.cs
@@ -8,7 +8,7 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ApiClientServiceExtensions
 {
-    private const string _httpClientApiName = "FluentCMS.Web.Api";
+    public const string HTTP_CLIENT_API_NAME = "FluentCMS.Web.Api";
 
     public static IServiceCollection AddApiClients(this IServiceCollection services, IConfiguration configuration)
     {
@@ -21,10 +21,10 @@ public static class ApiClientServiceExtensions
 
         services.AddScoped<ApiClientFactory>();
 
-        services.AddHttpClient(_httpClientApiName, (sp, client) =>
+        services.AddHttpClient(HTTP_CLIENT_API_NAME, (sp, client) =>
         {
-            var apiServer = configuration?["ApiServer"] ??
-                            throw new InvalidOperationException("ApiServer is not configured in appsettings.json");
+            string apiServer = configuration?["ApiServer"] ??
+                throw new ArgumentNullException("ApiServer is not configured in appsettings.json");
 
             client.BaseAddress = new Uri(apiServer);
             client.DefaultRequestHeaders.Accept.Clear();
@@ -39,14 +39,14 @@ public static class ApiClientServiceExtensions
         foreach (var apiClientType in apiClientTypes ?? [])
         {
             // find the implementation of the interface
-            var implementationType = Array.Find(Assembly.GetAssembly(apiClientType)?.GetTypes() ?? Type.EmptyTypes,
-                t => apiClientType.IsAssignableFrom(t) && !t.IsInterface);
+            var implementationType = Assembly.GetAssembly(apiClientType)?.GetTypes()
+                .FirstOrDefault(t => apiClientType.IsAssignableFrom(t) && !t.IsInterface);
 
             // register the interface with the implementation
-            if (implementationType is not null)
+            if (implementationType != null)
                 services.AddScoped(apiClientType, sp =>
                 {
-                    var httpClient = sp.GetRequiredService<IHttpClientFactory>().CreateClient(_httpClientApiName);
+                    var httpClient = sp.GetRequiredService<IHttpClientFactory>().CreateClient(HTTP_CLIENT_API_NAME);
 
                     var userLogin = sp.GetRequiredService<UserLoginResponse>();
 
