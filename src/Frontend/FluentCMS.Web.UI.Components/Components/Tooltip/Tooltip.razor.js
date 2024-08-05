7cbc06ed76962e00dc884e1c652a16c8b0001e24
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-08-05 17:40:38 +0430

    #1838 initial code of bootstrap theme

--- src/Frontend/FluentCMS.Web.UI.Components/Components/Tooltip/Tooltip.razor.js
+++ src/Frontend/FluentCMS.Web.UI.Components/Components/Tooltip/Tooltip.razor.js
@@ -1,3 +1,6 @@
+import '/_content/FluentCMS.Web.UI.Components/js/floating-ui.core.js'
+import '/_content/FluentCMS.Web.UI.Components/js/floating-ui.dom.js'
+
 const tooltips = new Map();
 
 export function update(dotnet, element, config) {
@@ -5,6 +8,7 @@ export function update(dotnet, element, config) {
 }
 
 export function initialize(dotnet, element, config) {
+    console.log('initialize', element, window.FloatingUIDOM)
     dispose(dotnet, element);
 
     const target = element;
@@ -15,10 +19,46 @@ export function initialize(dotnet, element, config) {
 
     const options = {
         placement: config.placement,
-        triggerType: 'hover',
+        middleware: [
+            FloatingUIDOM.offset(6),
+            FloatingUIDOM.flip(), 
+            FloatingUIDOM.shift({padding: 5}), 
+        ]
     };
 
-    const tooltip = new window.Flowbite.default.Tooltip(target, trigger, options);
+    const cleanup = FloatingUIDOM.autoUpdate(trigger, target, () => {
+        FloatingUIDOM.computePosition(trigger, target, options).then(({x, y}) => {
+            Object.assign(element.style, {
+                left: `${x}px`,
+                top: `${y}px`,
+            });
+        });
+    })
+
+    function showTooltip() {
+        Object.assign(element.style, {
+            display: 'block'
+        });
+    } 
+    
+    function hideTooltip() {
+        Object.assign(element.style, {
+            display: 'none'
+        });
+    }
+
+    hideTooltip()
+    trigger.addEventListener('mouseenter', showTooltip)
+    trigger.addEventListener('mouseleave', hideTooltip)
+    
+    const tooltip = {
+        destroy() {
+            trigger.removeEventListener('mouseenter', showTooltip)
+            trigger.removeEventListener('mouseleave', hideTooltip)
+            cleanup()
+        }
+    }
+    
 
     tooltips.set(element, tooltip);
 }
