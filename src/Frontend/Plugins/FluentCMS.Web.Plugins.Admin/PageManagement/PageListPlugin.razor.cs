86d12e9d286d4d4ef97ea9b003835ff6644f0c74
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-08-10 18:19:16 +0430

    #1467 Implement Page management

---
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin/PageManagement/PageListPlugin.razor.cs
@@ -0,0 +1,77 @@
+namespace FluentCMS.Web.Plugins.Admin.PageManagement;
+
+public partial class PageListPlugin
+{
+    [CascadingParameter]
+    private ViewState ViewState { get; set; } = default!;
+
+    private List<PageDetailResponse> Pages { get; set; } = [];
+    private List<LayoutDetailResponse> Layouts { get; set; } = [];
+
+    [SupplyParameterFromQuery(Name = "id")]
+    private Guid? Id { get; set; } = default!;
+
+    public async Task Load()
+    {
+        if(Id != null)
+        {
+            NavigateTo(GetUrl("Update Page", new { Id = Id }));
+        }
+        var pagesResponse = await ApiClient.Page.GetAllAsync(ViewState.Site.Urls[0]);
+        Pages = pagesResponse?.Data?.Where(x => !x.Locked).ToList() ?? [];
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var layoutsResponse = await ApiClient.Layout.GetAllAsync();
+        Layouts = layoutsResponse?.Data?.ToList() ?? [];
+        await Load();
+    }
+
+    private string GetPageUrl(PageDetailResponse page)
+    {
+        var result = new List<string>();
+        var currentPage = page;
+        while(currentPage != null)
+        {
+            result.Add(currentPage.Path);
+            if (currentPage.ParentId.HasValue)
+            {
+                currentPage = Pages.Where(x => x.Id == currentPage.ParentId.Value).FirstOrDefault();
+            }
+            else
+            {
+                currentPage = default!;
+            }
+        }        
+        result.Reverse();
+        
+        return string.Join("", result);
+    }
+
+    #region Delete Page
+
+    private PageDetailResponse? SelectedPage { get; set; }
+    public async Task OnDelete()
+    {
+        if (SelectedPage == null)
+            return;
+
+        await ApiClient.Page.DeleteAsync(SelectedPage.Id);
+        await Load();
+        SelectedPage = default;
+    }
+
+    public async Task OnConfirm(PageDetailResponse page)
+    {
+        SelectedPage = page;
+        await Task.CompletedTask;
+    }
+    public async Task OnConfirmClose()
+    {
+        SelectedPage = default;
+        await Task.CompletedTask;
+    }
+    #endregion
+
+}
---
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin/PageManagement/PageUpdatePlugin.razor.cs
@@ -0,0 +1,98 @@
+namespace FluentCMS.Web.Plugins.Admin.PageManagement;
+
+public partial class PageUpdatePlugin
+{
+    [CascadingParameter]
+    private ViewState ViewState { get; set; } = default!;
+
+    public const string FORM_NAME = "PageUpdateForm";
+
+    [SupplyParameterFromQuery(Name = "id")]
+    private Guid Id { get; set; }
+
+    [SupplyParameterFromForm(FormName = FORM_NAME)]
+    private PageUpdateRequest? Model { get; set; }
+
+    private List<LayoutDetailResponse>? Layouts { get; set; }
+    private List<PageDetailResponse>? Pages { get; set; }
+
+    private List<SelectOption>? LayoutOptions { get; set; }
+    private List<SelectOption>? PageOptions { get; set; }
+
+    private PageDetailResponse? Page { get; set; }
+
+    async Task GetAvailableParentPages()
+    {
+        var pagesResponse = await ApiClient.Page.GetAllAsync(ViewState.Site.Urls[0]);
+        var pages = pagesResponse?.Data?.Where(x => !x.Locked);
+
+        pages = pages.Where(x => x.ParentId != Id);
+        pages = pages.Where(x => x.Id != Id);
+
+        Pages = pages.ToList();
+
+        PageOptions = new List<SelectOption> {
+            new SelectOption {
+                Title = "(none)",
+                Value = Guid.Empty
+            }
+        };
+
+        foreach (var page in Pages) {
+            PageOptions.Add(
+                new SelectOption {
+                Title = page.Title,
+                Value = page.Id
+                }
+            );
+        }
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (Layouts is null)
+        {
+            var layoutsResponse = await ApiClient.Layout.GetAllAsync();
+            Layouts = layoutsResponse?.Data?.ToList() ?? [];
+
+            LayoutOptions = new List<SelectOption> {
+                new SelectOption {
+                    Title = "(default)",
+                    Value = Guid.Empty
+                }
+            };
+
+            foreach (var layout in Layouts) {
+                LayoutOptions.Add(
+                    new SelectOption {
+                        Title = layout.Name,
+                        Value = layout.Id
+                    }
+                );
+            }
+        }
+
+        if (Pages is null)
+        {
+            await GetAvailableParentPages();
+        }
+
+        if (Model is null)
+        {
+            var pageResponse = await ApiClient.Page.GetByIdAsync(Id);
+            Page = pageResponse.Data;
+            Model = Mapper.Map<PageUpdateRequest>(Page);
+        }
+    }
+
+    private async Task OnSubmit()
+    {
+        if (Model.ParentId == Guid.Empty)
+        {
+            Model.ParentId = default!;
+        }
+
+        await ApiClient.Page.UpdateAsync(Model);
+        NavigateBack();
+    }
+}
