630b83187324baee10154041c47d3ca61f3cd46a
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-08-14 18:12:20 +0430

    "#1867 Initial version of Contact Us"

---
+++ src/Frontend/FluentCMS.Web.ApiClients/IPluginSettings.cs
@@ -0,0 +1,6 @@
+namespace FluentCMS.Web.ApiClients;
+
+public interface IPluginSettings
+{
+
+}
---
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Contents.ContactUs/ContactUsSettings.cs
@@ -0,0 +1,5 @@
+public class ContactUsSettings : IPluginSettings
+{
+    public string EmailAddress { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
