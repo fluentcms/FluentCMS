b4de41b5ae3a284e7fea0943358536456de44494
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-08-15 16:08:32 +0430

    #1867 add Google Map plugin

---
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Contents.GoogleMap/GoogleMapSettings.cs
@@ -0,0 +1,6 @@
+public class GoogleMapSettings : IPluginSettings
+{
+    public string ApiKey { get; set; } = string.Empty;
+    public string Latitude { get; set; } = string.Empty;
+    public string Longitude { get; set; } = string.Empty;
+}
