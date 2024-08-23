b43a2693430dbe1a392046222712ef140a2b7d00
Hadi Ahmadi <TheHadiAhmadi@gmail.com>
2024-08-23 19:42:31 +0430

    #1891 Update site builder based on figma design (#1893)
    * #1891 Update site builder based on figma design
    
    * apply some changes and fix bugs
    
    * #1891 allow drag and drop between sections
    
    * Refactor class locations and update property initializations
    
    - Moved `PageUpdatePluginOrdersRequest` and `PagePluginDetail` classes within the file.
    - Removed default value assignment for `Id` property in `PagePluginDetail`.
    - Updated `PageUpdatePluginOrdersRequest` to initialize `Plugins` property with new syntax (`= []`).
    
    ---------
    
    Co-authored-by: Amir Pournasserian <amir.pournasserian@live.com>

---
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Contents.TextHTML/RichText/RichTextEditPlugin.razor.cs
@@ -0,0 +1,101 @@
+namespace FluentCMS.Web.Plugins.Contents.TextHTML;
+
+public partial class RichTextEditPlugin
+{
+    public const string CONTENT_TYPE_NAME = nameof(RichTextContent);
+
+    [Inject]
+    protected NavigationManager NavigationManager { get; set; } = default!;
+
+    [Parameter]
+    public string? SectionName { get; set; }
+
+    [Parameter]
+    public PluginViewState? Plugin { get; set; } = default!;
+
+    [Inject]
+    protected ApiClientFactory ApiClient { get; set; } = default!;
+
+    [Inject]
+    protected IHttpContextAccessor? HttpContextAccessor { get; set; }
+
+    [SupplyParameterFromQuery(Name = "redirectTo")]
+    private string RedirectTo { get; set; } = string.Empty;
+
+    protected virtual void NavigateBack()
+    {
+        if (!string.IsNullOrEmpty(RedirectTo)) {
+            NavigateTo(Uri.UnescapeDataString(RedirectTo));
+        }
+        else
+        {
+            var url = new Uri(NavigationManager.Uri).LocalPath;
+            NavigateTo(url);
+        }
+    }
+
+    protected virtual void NavigateTo(string path)
+    {
+        if (HttpContextAccessor?.HttpContext != null && !HttpContextAccessor.HttpContext.Response.HasStarted)
+            HttpContextAccessor.HttpContext.Response.Redirect(path);
+        else
+            NavigationManager.NavigateTo(path, true);
+    }
+
+    protected virtual string GetBackUrl()
+    {
+        if(!string.IsNullOrEmpty(RedirectTo))
+        {
+            return Uri.UnescapeDataString(RedirectTo);
+        }
+        else
+        {
+            return new Uri(NavigationManager.Uri).LocalPath;
+        }
+    }
+
+    [SupplyParameterFromForm(FormName = CONTENT_TYPE_NAME)]
+    private RichTextContent? Model { get; set; }
+
+    private bool IsEditMode { get; set; } = false;
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (Model is null)
+        {
+            var response = await ApiClient.PluginContent.GetAllAsync(CONTENT_TYPE_NAME, Plugin!.Id);
+
+            var content = response.Data.ToContentList<RichTextContent>();
+
+            if(content.Count > 0)
+            {
+                Model = new RichTextContent
+                {
+                    Id = content[0].Id,
+                    Content = content[0].Content,
+                };
+                IsEditMode = true;
+            }
+            else
+            {
+                Model = new RichTextContent
+                {
+                    Id = Guid.Empty
+                };
+            }
+        }
+    }
+
+    private async Task OnSubmit()
+    {
+        if (Model is null || Plugin is null)
+            return;
+
+        if (IsEditMode)
+            await ApiClient.PluginContent.UpdateAsync(CONTENT_TYPE_NAME, Plugin.Id, Model.Id, Model.ToDictionary());
+        else
+            await ApiClient.PluginContent.CreateAsync(CONTENT_TYPE_NAME, Plugin.Id, Model.ToDictionary());
+
+        NavigateBack();
+    }
+}
