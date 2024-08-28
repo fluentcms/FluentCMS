643bcbb4b8c943e7fa832065e1904193ed51e4bc
BabakDavoodi <davoodi.babak@gmail.com>
2024-08-28 19:51:47 +0330

    roletype is set to old value in update role duplication check bug is fixed

--- src/Backend/FluentCMS.Services/RoleService.cs
+++ src/Backend/FluentCMS.Services/RoleService.cs
@@ -24,7 +24,7 @@ public class RoleService(IRoleRepository roleRepository, IMessagePublisher messa
 
     public async Task<Role> Create(Role role, CancellationToken cancellationToken)
     {
-        var sameRole = await roleRepository.GetByNameAndSiteId(role.SiteId, role.Name, cancellationToken);
+        var sameRole = await roleRepository.GetByNameAndSiteId(role.SiteId, role.Name.Trim(), cancellationToken);
 
         if (sameRole != null)
             throw new AppException(ExceptionCodes.RoleNameIsDuplicated);
@@ -39,13 +39,15 @@ public class RoleService(IRoleRepository roleRepository, IMessagePublisher messa
 
     public async Task<Role> Update(Role role, CancellationToken cancellationToken)
     {
-        _ = await roleRepository.GetById(role.Id, cancellationToken) ??
-            throw new AppException(ExceptionCodes.RoleNotFound);
+        var existRole = await roleRepository.GetById(role.Id, cancellationToken) ??
+             throw new AppException(ExceptionCodes.RoleNotFound);
 
-        var sameRole = await roleRepository.GetByNameAndSiteId(role.SiteId, role.Name, cancellationToken);
+        var sameRole = await roleRepository.GetByNameAndSiteId(role.SiteId, role.Name.Trim(), cancellationToken);
 
-        if (sameRole != null)
+        if (sameRole != null && sameRole.Id != role.Id)
             throw new AppException(ExceptionCodes.RoleNameIsDuplicated);
+
+        role.Type = existRole.Type;
 
         var updatedRole = await roleRepository.Update(role, cancellationToken) ??
             throw new AppException(ExceptionCodes.RoleUnableToUpdate);
