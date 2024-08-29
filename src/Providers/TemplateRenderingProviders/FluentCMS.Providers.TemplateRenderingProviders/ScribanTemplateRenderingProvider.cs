61fefa1cfb5d937195ac3222e5d76b4b262b0394
Amir Pournasserian <amir.pournasserian@live.com>
2024-08-29 16:08:07 -0400

    Add TemplateRenderingProviders and update project references
    - Updated `FluentCMS.sln` to replace project type GUIDs.
    - Added new `TemplateRenderingProviders` projects.
    - Updated `GlobalSection` in `FluentCMS.sln` for new projects.
    - Added project reference to `FluentCMS.Providers.TemplateRenderingProviders` in `FluentCMS.csproj`.
    - Registered `ScribanTemplateRenderingProvider` in `Program.cs`.
    - Updated `appsettings.json` with new API key for `ApiSettings`.
    - Refactored `LayoutProcessor.cs` to use `ITemplateRenderingProvider`.
    - Updated `FluentCMS.Web.UI.csproj` to remove Scriban and add project reference.
    - Updated `ContentListViewPlugin.razor.cs` to use `ITemplateRenderingProvider`.
    - Updated `FluentCMS.Web.Plugins.Contents.ContentViewer.csproj` to add project reference and remove Scriban.
    - Added new project files for `TemplateRenderingProviders` and `TemplateRenderingProviders.Abstractions`.
    - Added `ITemplateRenderingProvider` interface.
    - Added `ScribanTemplateRenderingProvider` class.
    - Added `ServiceExtensions` for registering `ScribanTemplateRenderingProvider`.

---
+++ src/Providers/TemplateRenderingProviders/FluentCMS.Providers.TemplateRenderingProviders/ScribanTemplateRenderingProvider.cs
@@ -0,0 +1,24 @@
+﻿using FluentCMS.Providers.TemplateRenderingProviders.Abstractions;
+using Scriban.Runtime;
+using Scriban;
+
+namespace FluentCMS.Providers.TemplateRenderingProviders;
+
+public class ScribanTemplateRenderingProvider : ITemplateRenderingProvider
+{
+    public string Render(string? content, Dictionary<string, object> keyValues)
+    {
+        if(string.IsNullOrEmpty(content))
+            return string.Empty;
+
+        var scriptObject = new ScriptObject();
+        foreach (var keyValue in keyValues)
+            scriptObject.Add(keyValue.Key, keyValue.Value);
+
+        var context = new TemplateContext();
+        context.PushGlobal(scriptObject);
+
+        var template = Template.Parse(content);
+        return template.Render(context);
+    }
+}
