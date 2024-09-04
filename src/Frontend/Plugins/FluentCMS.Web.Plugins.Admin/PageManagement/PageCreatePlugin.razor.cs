a436ca7abe28d645fc466655f90362adc1f7e1a0
Hadi Ahmadi <TheHadiAhmadi@gmail.com>
2024-09-04 14:56:11 +0430

    #1998 add "Add page" button to toolbar. and update layout styles (#2001)

--- src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin/PageManagement/PageCreatePlugin.razor.cs
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin/PageManagement/PageCreatePlugin.razor.cs
@@ -7,6 +7,9 @@ public partial class PageCreatePlugin
     [SupplyParameterFromForm(FormName = FORM_NAME)]
     private PageCreateRequest? Model { get; set; }
 
+    [SupplyParameterFromQuery(Name = "openNewPage")]
+    private bool OpenNewPage { get; set; } = false;
+
     private string Urls { get; set; } = string.Empty;
     private List<LayoutDetailResponse>? Layouts { get; set; }
     private List<PageDetailResponse>? Pages { get; set; }
@@ -69,10 +72,36 @@ public partial class PageCreatePlugin
         Model ??= new();
     }
 
+    private static string GetFullPath(Dictionary<Guid, PageDetailResponse> allPages, PageDetailResponse page)
+    {
+        var result = new List<string>();
+        var currentPage = page;
+        while (currentPage != null)
+        {
+            result.Add(currentPage.Path);
+            currentPage = currentPage.ParentId.HasValue ? allPages[currentPage.ParentId.Value] : default!;
+        }
+        result.Reverse();
+
+        return string.Join("", result);
+    }
+
     private async Task OnSubmit()
     {
         Model.SiteId = ViewState.Site.Id;
-        await ApiClient.Page.CreateAsync(Model);
-        NavigateBack();
+        var response = await ApiClient.Page.CreateAsync(Model);
+
+        if (OpenNewPage)
+        {
+            var pagesResponse = await ApiClient.Page.GetAllAsync(ViewState.Site.Urls[0]);
+            var pagesDictionary = pagesResponse!.Data!.ToDictionary(x => x.Id, x => x);
+
+            var path = GetFullPath(pagesDictionary, response.Data!);
+            NavigateTo(path);
+        }
+        else
+        {
+            NavigateBack();
+        }
     }
 }
