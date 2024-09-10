ce4f604488569ff52f271280bc3df0a4e9ba0b28
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-09-11 03:00:03 +0430

    #1902 cleanup

--- src/Frontend/FluentCMS.Web.UI/wwwroot/helpers.js
+++
@@ -1,9 +0,0 @@
-export function debounce(cb, timeout = 300) {
-    let timer;
-    return (...args) => {
-        if(timer) clearTimeout(timer)
-        timer = setTimeout(() => {
-            cb(...args)
-        }, timeout)
-    }
-}
