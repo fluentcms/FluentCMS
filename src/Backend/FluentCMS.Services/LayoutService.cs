e58c6fc2398305d6848c56122d3b09d05e2f996c
Amir Pournasserian <amir.pournasserian@live.com>
2024-09-17 10:42:46 -0400

    Refactor update methods and enhance request classes
    Refactored the `Update` method in `LayoutService` to directly update the `layout` object and changed the exception from `LayoutNotFound` to `LayoutUnableToUpdate`. Updated `LayoutCreateRequest` to include `Body` and `Head` properties. Refactored `LayoutUpdateRequest` to define its own properties: `Id`, `Name`, `Body`, and `Head`.
    
    Modified `AuditableEntityRepository<TEntity>` to fetch and update the existing entity, and adjusted `SetAuditableFieldsForUpdate` to accept both new and old entities. Updated `SiteAssociatedRepository<TEntity>` to override the `Update` method, preserving `SiteId`, and added a method to get all entities for a specific site. Ensured consistency in `SiteAssociatedRepository<TEntity>` for different database contexts (`IMongoDBContext` and `ILiteDBContext`).

--- src/Backend/FluentCMS.Services/LayoutService.cs
+++ src/Backend/FluentCMS.Services/LayoutService.cs
@@ -38,15 +38,8 @@ public class LayoutService(ILayoutRepository layoutRepository, IMessagePublisher
 
     public async Task<Layout> Update(Layout layout, CancellationToken cancellationToken)
     {
-        var original = await layoutRepository.GetById(layout.Id, cancellationToken) ??
-            throw new AppException(ExceptionCodes.LayoutNotFound);
-
-        original.Body = layout.Body;
-        original.Head = layout.Head;
-        original.Name = layout.Name;
-
-        var updated = await layoutRepository.Update(original, cancellationToken) ??
-            throw new AppException(ExceptionCodes.LayoutNotFound);
+        var updated = await layoutRepository.Update(layout, cancellationToken) ??
+            throw new AppException(ExceptionCodes.LayoutUnableToUpdate);
 
         await messagePublisher.Publish(new Message<Layout>(ActionNames.LayoutUpdated, updated), cancellationToken);
 
