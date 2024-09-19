3848674ad9f1e7d5fbcc9e3497fe9c30c685bb3c
Amir Pournasserian <amir.pournasserian@live.com>
2024-09-19 09:35:01 -0400

    Reformat code for improved readability
    Reformatted the code to ensure consistent indentation, specifically adjusting the `[Parameter]` and `[Inject]` properties, the `IsDesignMode` field, and the `OnInitializedAsync` method. These changes enhance the readability and maintainability of the code without altering its logic or functionality.

--- src/Frontend/FluentCMS.Web.UI/Components/PluginContainerActions.razor.cs
+++ src/Frontend/FluentCMS.Web.UI/Components/PluginContainerActions.razor.cs
@@ -2,17 +2,17 @@ namespace FluentCMS.Web.UI;
 
 public partial class PluginContainerActions
 {
-        [Parameter]
-        public PluginViewState Plugin { get; set; } = default!;
+    [Parameter]
+    public PluginViewState Plugin { get; set; } = default!;
 
-        [Inject]
-        public ViewState ViewState { get; set; } = default!;
+    [Inject]
+    public ViewState ViewState { get; set; } = default!;
 
-        private bool IsDesignMode { get; set; } = false;
+    private bool IsDesignMode { get; set; } = false;
 
-        protected override async Task OnInitializedAsync()
-        {
-                IsDesignMode = ViewState.Type == ViewStateType.PagePreview;
-                await Task.CompletedTask;
-        }
+    protected override async Task OnInitializedAsync()
+    {
+        IsDesignMode = ViewState.Type == ViewStateType.PagePreview;
+        await Task.CompletedTask;
+    }
 }
