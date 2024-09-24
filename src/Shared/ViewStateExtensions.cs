737edf7a5054667eae9cc8f8571e769dcdef9ffd
Amir Pournasserian <amir.pournasserian@live.com>
2024-09-24 12:15:59 -0400

    Refactor and simplify components, improve role checks
    Significant refactoring and simplification of `PageEditorSidebar`, `PageToolbar`, and `PluginContainer` components. Removed unnecessary structures and logic, focusing on essential functionality. Enhanced `PluginContainerActions` with conditional rendering based on user roles. Removed `SiteBuilderForms` and updated `SiteBuilderPreview` for dynamic content rendering. Streamlined `Default` component and introduced `ViewStateExtensions` for cleaner role checks.

---
+++ src/Shared/ViewStateExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace FluentCMS;
+
+public static class ViewStateExtensions
+{
+    public static bool HasPageAdminAccess(this ViewState viewState)
+    {
+        return viewState.User.Roles.Any(role => role.Type == RoleTypesViewState.Authenticated);
+    }
+
+    public static bool HasPageContributorAccess(this ViewState viewState)
+    {
+        return viewState.User.Roles.Any(role => role.Type == RoleTypesViewState.Authenticated);
+    }
+}
