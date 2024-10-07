a52b026015986d2fa477fb13756cfee4f56449ed
Amir Pournasserian <amir.pournasserian@live.com>
2024-10-07 19:17:08 -0400

    Remove `PageDetail` class and its properties
    The `PageDetail` class, which inherited from the `Page` class, has been removed. This includes the `FullPath` property of type `string` and the `Children` property, which was a list of `PageDetail` objects.

--- src/Backend/FluentCMS.Services/Models/PageDetail.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FluentCMS.Services.Models;
-
-public class PageDetail : Page
-{
-    public string FullPath { get; set; } = default!;
-    public List<PageDetail> Children { get; set; } = [];
-}
