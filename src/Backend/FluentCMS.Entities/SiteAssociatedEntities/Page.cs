64bc648b955e27b16e954711be592f12b2f21aba
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-10-13 23:17:48 +0430

    #1940 comment roleIds in page model (to fix exception temporarily)

--- src/Backend/FluentCMS.Entities/SiteAssociatedEntities/Page.cs
+++ src/Backend/FluentCMS.Entities/SiteAssociatedEntities/Page.cs
@@ -11,7 +11,7 @@ public class Page : SiteAssociatedEntity
     public Guid? DetailLayoutId { get; set; }
     public bool Locked { get; set; } = false;
     public Dictionary<string, string> Settings { get; set; } = [];
-    public IEnumerable<Guid> ViewRoleIds { get; set; } = [];
-    public IEnumerable<Guid> ContributorRoleIds { get; set; } = [];
-    public IEnumerable<Guid> AdminRoleIds { get; set; } = [];
+    // public IEnumerable<Guid> ViewRoleIds { get; set; } = [];
+    // public IEnumerable<Guid> ContributorRoleIds { get; set; } = [];
+    // public IEnumerable<Guid> AdminRoleIds { get; set; } = [];
 }
