4aaaaa8543783c48cf972d6ee01a3b892d6e7619
Amir Pournasserian <amir.pournasserian@live.com>
2024-10-18 08:06:56 -0400

    Refactor DefinitionListPlugin and remove filtering
    Refactored DefinitionListPlugin by removing the `Load` method and integrating its logic into `OnInitializedAsync`. This change simplifies the code and includes all plugin definitions without filtering out locked items. Also, removed an unnecessary line break in `DefinitionListPlugin.razor`.

--- src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin/PluginDefinitionManagement/DefinitionListPlugin.razor.cs
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Admin/PluginDefinitionManagement/DefinitionListPlugin.razor.cs
@@ -4,14 +4,9 @@ public partial class DefinitionListPlugin
 {
     private List<PluginDefinitionDetailResponse> PluginDefinitions { get; set; } = [];
 
-    public async Task Load()
-    { 
-        var pluginDefinitionsResponse = await ApiClient.PluginDefinition.GetAllAsync();
-        PluginDefinitions = pluginDefinitionsResponse?.Data?.Where(x => !x.Locked).ToList() ?? [];
-    }
-
     protected override async Task OnInitializedAsync()
     {
-        await Load();
+        var pluginDefinitionsResponse = await ApiClient.PluginDefinition.GetAllAsync();
+        PluginDefinitions = pluginDefinitionsResponse?.Data?.ToList() ?? [];
     }
 }
