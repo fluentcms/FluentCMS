4485718499a9491d9d9101743e0ddda647169e39
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-10-25 18:38:59 +0430

    #2182 rename Iframe to IFrame and add switch for edit form

--- src/Frontend/Plugins/FluentCMS.Web.Plugins.Contents.IFrame/IFrameEditPlugin.razor.cs
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Contents.IFrame/IFrameEditPlugin.razor.cs
@@ -1,8 +1,8 @@
-namespace FluentCMS.Web.Plugins.Contents.Iframe;
+namespace FluentCMS.Web.Plugins.Contents.IFrame;
 
-public partial class IframeEditPlugin
+public partial class IFrameEditPlugin
 {
-    private IframeSettingsModel? Model { get; set; }
+    private IFrameSettingsModel? Model { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
@@ -11,12 +11,14 @@ public partial class IframeEditPlugin
             Plugin.Settings.TryGetValue("Src", out var src);
             Plugin.Settings.TryGetValue("Height", out var height);
             Plugin.Settings.TryGetValue("EmbedCode", out var embedCode);
+            Plugin.Settings.TryGetValue("IsUsingSrc", out var isUsingSrc);
 
             Model = new()
             {
                 Src = src ?? string.Empty,
                 Height = height ?? string.Empty,
                 EmbedCode = embedCode ?? string.Empty,
+                IsUsingSrc = isUsingSrc == "true" ? true : false,
             };
         }
     }
@@ -33,6 +35,7 @@ public partial class IframeEditPlugin
                 { "Src", Model.Src },
                 { "Height", Model.Height },
                 { "EmbedCode", Model.EmbedCode },
+                { "IsUsingSrc", Model.IsUsingSrc.ToString() },
             }
         };
         await ApiClient.Settings.UpdateAsync(request);
@@ -40,10 +43,11 @@ public partial class IframeEditPlugin
         await OnSubmit.InvokeAsync();
     }
 
-    class IframeSettingsModel
+    class IFrameSettingsModel
     {
         public string Src { get; set; } = string.Empty;
         public string Height { get; set; } = string.Empty;
         public string EmbedCode { get; set; } = string.Empty;
+        public bool IsUsingSrc { get; set; } = true;
     };
 }
--- src/Frontend/Plugins/FluentCMS.Web.Plugins.Contents.IFrame/IFrameViewPlugin.razor.cs
+++ src/Frontend/Plugins/FluentCMS.Web.Plugins.Contents.IFrame/IFrameViewPlugin.razor.cs
@@ -1,10 +1,11 @@
-namespace FluentCMS.Web.Plugins.Contents.Iframe;
+namespace FluentCMS.Web.Plugins.Contents.IFrame;
 
-public partial class IframeViewPlugin
+public partial class IFrameViewPlugin
 {
     private string Source { get; set; }
     private string Height { get; set; }
     private string EmbedCode { get; set; }
+    private bool IsUsingSrc { get; set; } = false;
 
     protected override async Task OnInitializedAsync()
     {
@@ -13,10 +14,12 @@ public partial class IframeViewPlugin
             Plugin.Settings.TryGetValue("Src", out var src);
             Plugin.Settings.TryGetValue("Height", out var height);
             Plugin.Settings.TryGetValue("EmbedCode", out var embedCode);
+            Plugin.Settings.TryGetValue("IsUsingSrc", out var isUsingSrc);
 
             Source = src;
             Height = height;
             EmbedCode = embedCode;
+            IsUsingSrc = isUsingSrc == "true";
 
         }
     }
