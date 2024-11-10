10abb4b59ea3756012ccc9029af9fa77ad7d96cd
Hadi Ahmadi <TheHadiAhmadi@gmail.com>
2024-11-10 20:49:29 +0430

    #2237 Add files for blank template (#2242)

---
+++ src/FluentCMS/Templates/Blank/Files/js/app.head.js
@@ -0,0 +1,5 @@
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+} else {
+    document.documentElement.classList.remove('dark')
+}
