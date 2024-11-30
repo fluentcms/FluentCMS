bcd098192f50b8fa9a80cc58c594e071ca0baaba
Amir Pournasserian <amir.pournasserian@live.com>
2024-11-29 19:27:48 -0500

    Fix collection name typo in HttpLogRepository.cs
    Modified the GetCollectionName method in HttpLogRepository.cs to update collection names based on status codes. Changed "HttpLog400", "HttpLog300", "HttpLog500", and "HttpLog200" to "httplog400", "httplog300", "httplog500", and "httplog200" respectively. Corrected a typo where "httphog400" should be "httplog400".

--- src/Backend/Repositories/FluentCMS.Repositories.LiteDb/HttpLogRepository.cs
+++ src/Backend/Repositories/FluentCMS.Repositories.LiteDb/HttpLogRepository.cs
@@ -37,9 +37,9 @@ public class HttpLogRepository : IHttpLogRepository
 
     private static string GetCollectionName(int statusCode) => statusCode switch
     {
-        < 500 and >= 400 => "HttpLog400",
-        < 400 and >= 300 => "HttpLog300",
-        >= 500 => "HttpLog500",
-        _ => "HttpLog200"
+        < 500 and >= 400 => "httphog400",
+        < 400 and >= 300 => "httplog300",
+        >= 500 => "httplog500",
+        _ => "httplog200"
     };
 }
--- src/Backend/Repositories/FluentCMS.Repositories.MongoDB/HttpLogRepository.cs
+++ src/Backend/Repositories/FluentCMS.Repositories.MongoDB/HttpLogRepository.cs
@@ -16,9 +16,9 @@ public class HttpLogRepository(IMongoDBContext mongoDbContext) : IHttpLogReposit
     }
     private static string GetCollectionName(int statusCode) => statusCode switch
     {
-        < 500 and >= 400 => "HttpLog400",
-        < 400 and >= 300 => "HttpLog300",
-        >= 500 => "HttpLog500",
-        _ => "HttpLog200"
+        < 500 and >= 400 => "httphog400",
+        < 400 and >= 300 => "httplog300",
+        >= 500 => "httplog500",
+        _ => "httplog200"
     };
 }
