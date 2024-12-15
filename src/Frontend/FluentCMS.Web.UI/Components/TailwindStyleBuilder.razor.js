ae3e8a45b52c99e8ea1061ba1b0f2f1bda3b51bf
Hadi Ahmadi <TheHadiAhmadi@gmail.com>
2024-12-15 17:39:20 +0430

    #2340 Refactor tailwind blocks and current templates (#2341)

--- src/Frontend/FluentCMS.Web.UI/Components/TailwindStyleBuilder.razor.js
+++ src/Frontend/FluentCMS.Web.UI/Components/TailwindStyleBuilder.razor.js
@@ -6,23 +6,33 @@ tailwind.config = {
         extend: {
             colors: {
                 primary: {
-                    50: '#f4f8fd',
-                    100: '#e8f1fb',
-                    200: '#c6ddf4',
-                    300: '#a3c8ed',
-                    400: '#5e9fe0',
-                    500: '#1976d2',
-                    600: '#176abd',
-                    700: '#13599e',
-                    800: '#0f477e',
-                    900: '#0c3a67',
-                    on: '#f4f8fd',
-                    dark: {
-                        DEFAULT: '#a3c8ed',
-                        on: '#0f477e',
-                    }
+                    50: 'hsl(var(--f-primary-50, var(--f-primary), 100%, 95%))',
+                    100: 'hsl(var(--f-primary-100, var(--f-primary), 80%, 90%))',
+                    200: 'hsl(var(--f-primary-200, var(--f-primary), 70%, 80%))',
+                    300: 'hsl(var(--f-primary-300, var(--f-primary), 60%, 70%))',
+                    400: 'hsl(var(--f-primary-400, var(--f-primary), 60%, 60%))',
+                    500: 'hsl(var(--f-primary-500, var(--f-primary), 60%, 50%))',
+                    600: 'hsl(var(--f-primary-600, var(--f-primary), 80%, 40%))',
+                    700: 'hsl(var(--f-primary-700, var(--f-primary), 80%, 30%))',
+                    800: 'hsl(var(--f-primary-800, var(--f-primary), 80%, 20%))',
+                    900: 'hsl(var(--f-primary-900, var(--f-primary), 90%, 15%))',
                 },
-            },
+                surface: {
+                    DEFAULT: 'hsl(var(--f-surface))',
+                    muted: 'hsl(var(--f-surface-muted))',
+                    accent: 'hsl(var(--f-surface-accent))',
+                },
+                content: {
+                    DEFAULT: 'hsl(var(--f-content))',
+                    muted: 'hsl(var(--f-content-muted))',
+                    accent: 'hsl(var(--f-content-accent))'
+                },
+                border: {
+                    DEFAULT: 'hsl(var(--f-border))',
+                    muted: 'hsl(var(--f-border-muted))',
+                    accent: 'hsl(var(--f-border-accent))',
+                }
+            }
         },
     }
 }
