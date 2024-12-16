309faa1d12105d201eb147713558aa063840bb8a
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-12-16 17:40:00 +0430

    #2342 Remove console.log and remove using from global imports

--- src/Frontend/FluentCMS.Web.UI.TailwindStyleBuilder/wwwroot/TailwindStyleBuilder.js
+++ src/Frontend/FluentCMS.Web.UI.TailwindStyleBuilder/wwwroot/TailwindStyleBuilder.js
@@ -3,7 +3,6 @@ import "/_content/FluentCMS.Web.UI.TailwindStyleBuilder/tailwind.cdn.js"
 export async function initialize(dotnet, config)
 {
     tailwind.config = JSON.parse(config)
-    console.log(tailwind.config, config)
     await new Promise(resolve => setTimeout(resolve, 1000));
 
     const styleTags = document.querySelectorAll('style')
