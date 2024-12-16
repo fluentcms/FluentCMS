e50d429fbcbaecd4812fe368d2d5a1e334a7ba69
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-12-16 15:54:23 +0430

    #2342 add TailwindStyleBuilder project

---
+++ src/Frontend/FluentCMS.Web.UI.TailwindStyleBuilder/wwwroot/TailwindStyleBuilder.js
@@ -0,0 +1,19 @@
+import "/_content/FluentCMS.Web.UI.TailwindStyleBuilder/tailwind.cdn.js"
+
+export async function initialize(dotnet, config)
+{
+    tailwind.config = JSON.parse(config)
+    console.log(tailwind.config, config)
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    const styleTags = document.querySelectorAll('style')
+    let result = ''
+
+    styleTags.forEach(style => {
+        if(style.textContent.slice(0, 20).includes('tailwind')){
+            result = style.textContent
+        }
+    })
+    
+    return result
+}
