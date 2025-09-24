db6dd61eb14a9f117e5bf9880b1fe59146f314f0
Amir Pournasserian <amir.pournasserian@live.com>
2025-09-24 17:53:05 -0400

    Add PagedResult<T> class for pagination support
    Introduced the `PagedResult<T>` class in `PagedResult.cs` to serve as a pagination result wrapper. This class includes properties for items, total count, current page, page size, and computed properties for total pages, as well as indicators for next and previous pages. The namespace `FluentCMS.Repositories.Abstractions` has also been added.

---
+++ src/Inftastructure/Repositories/FluentCMS.Repositories.Abstractions/PagedResult.cs
@@ -0,0 +1,15 @@
+namespace FluentCMS.Repositories.Abstractions;
+
+/// <summary>
+/// Pagination result wrapper
+/// </summary>
+public class PagedResult<T>
+{
+    public IEnumerable<T> Items { get; set; } = [];
+    public long TotalCount { get; set; }
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+    public bool HasNextPage => Page < TotalPages;
+    public bool HasPreviousPage => Page > 1;
+}
