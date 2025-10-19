e4664d2c049cffe02b3ba0b8b0835377c86e4140
Amir Pournasserian <amir.pournasserian@live.com>
2025-10-19 12:56:28 -0400

    Add AutoMapper profile registration in plugin
    This commit introduces the registration of AutoMapper profiles in the `ConfigureServices` method of the `CmsCoreManagementPlugin` class. It includes a new configuration that adds the `MappingProfile`, facilitating object-to-object mappings within the application.

--- src/Api/Plugins/FluentCMS.Api.Plugins.CmsCoreManagement/CmsCoreManagementPlugin.cs
+++ src/Api/Plugins/FluentCMS.Api.Plugins.CmsCoreManagement/CmsCoreManagementPlugin.cs
@@ -5,6 +5,12 @@ public class CmsCoreManagementPlugin : IPluginStartup
 {
     public void ConfigureServices(IServiceCollection services, IConfiguration? configuration)
     {
+        // Register auto-mapper profiles
+        services.AddAutoMapper(cfg =>
+        {
+            cfg.AddProfile<MappingProfile>();
+        });
+
         // Register database context
         services.AddDatabaseContext<CmsCoreDbContext, ICmsCoreDatabaseMarker>();
         services.AddSchemaValidator<CmsCoreSchemaValidator, ICmsCoreDatabaseMarker>();
