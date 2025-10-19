da1f61c920a30078954c673ad1383012622c584e
Amir Pournasserian <amir.pournasserian@live.com>
2025-10-19 13:01:53 -0400

    Remove [Required] attributes from action method parameters
    This commit removes the `[Required]` attribute from the parameters of several asynchronous action methods in the `AccountsController` class, including `ConfirmEmail`, `ResendConfirmation`, `ForgotPassword`, `ResetPassword`, and `ChangePassword`. This change may impact model validation behavior during API requests, as the parameters will no longer be enforced as required.

--- src/Api/Plugins/FluentCMS.Api.Plugins.IdentityManagement/Controllers/AccountsController.cs
+++ src/Api/Plugins/FluentCMS.Api.Plugins.IdentityManagement/Controllers/AccountsController.cs
@@ -24,35 +24,35 @@ public class AccountsController(IAccountService accountService) : BaseController
     }
 
     [HttpPost]
-    public async Task<ActionResult<ApiResponse>> ConfirmEmail([Required] AccountConfirmEmailRequest confirmEmailRequest, CancellationToken cancellationToken = default)
+    public async Task<ActionResult<ApiResponse>> ConfirmEmail(AccountConfirmEmailRequest confirmEmailRequest, CancellationToken cancellationToken = default)
     {
         await accountService.ConfirmEmail(confirmEmailRequest.UserName, confirmEmailRequest.Email, confirmEmailRequest.Token, cancellationToken);
         return Success(MessageCodes.AccountEmailConfirmedSuccessfully);
     }
 
     [HttpPost]
-    public async Task<ActionResult<ApiResponse>> ResendConfirmation([Required] AccountResendConfirmationRequest resendConfirmationRequest, CancellationToken cancellationToken = default!)
+    public async Task<ActionResult<ApiResponse>> ResendConfirmation(AccountResendConfirmationRequest resendConfirmationRequest, CancellationToken cancellationToken = default!)
     {
         await accountService.ResendConfirmation(resendConfirmationRequest.Email, cancellationToken);
         return Success(MessageCodes.AccountConfirmationEmailSent);
     }
 
     [HttpPost]
-    public async Task<ActionResult<ApiResponse>> ForgotPassword([Required] AccountForgotPasswordRequest forgotPasswordRequest, CancellationToken cancellationToken = default!)
+    public async Task<ActionResult<ApiResponse>> ForgotPassword(AccountForgotPasswordRequest forgotPasswordRequest, CancellationToken cancellationToken = default!)
     {
         await accountService.ForgotPassword(forgotPasswordRequest.Email, cancellationToken);
         return Success(MessageCodes.AccountPasswordResetEmailSent);
     }
 
     [HttpPost]
-    public async Task<ActionResult<ApiResponse>> ResetPassword([Required] AccountResetPasswordRequest resetPasswordRequest, CancellationToken cancellationToken = default!)
+    public async Task<ActionResult<ApiResponse>> ResetPassword(AccountResetPasswordRequest resetPasswordRequest, CancellationToken cancellationToken = default!)
     {
         await accountService.ResetPassword(resetPasswordRequest.Email, resetPasswordRequest.Token, resetPasswordRequest.NewPassword, cancellationToken);
         return Success(MessageCodes.AccountPasswordResetSuccessfully);
     }
 
     [HttpPost]
-    public async Task<ActionResult<ApiResponse>> ChangePassword([Required] AccountChangePasswordRequest changePasswordRequest, CancellationToken cancellationToken = default!)
+    public async Task<ActionResult<ApiResponse>> ChangePassword(AccountChangePasswordRequest changePasswordRequest, CancellationToken cancellationToken = default!)
     {
         await accountService.ChangePassword(changePasswordRequest.UserName, changePasswordRequest.OldPassword, changePasswordRequest.NewPassword, cancellationToken);
         return Success(MessageCodes.AccountPasswordChangedSuccessfully);
