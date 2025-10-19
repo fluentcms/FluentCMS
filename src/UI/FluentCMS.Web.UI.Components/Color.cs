4c89648f5a4d1cb77ac72a0cb0febbf568419c3e
Amir Pournasserian <amir.pournasserian@live.com>
2025-10-19 17:20:09 -0400

    Add FluentCMS.Web.UI.Components project and components
    - Introduced new project "FluentCMS.Web.UI.Components" in the solution.
    - Added core classes: BaseComponent, BaseComponentHelpers, Color, CssPropertyAttribute, IBaseComponent, StringExtensions, Icon, and IconResource.
    - Created UI components: Avatar, Badge, Breadcrumb, etc., utilizing the new base class.
    - Updated project file to target .NET 9.0 and included necessary package references.
    - Enhanced UI with new styles in MainLayout.razor.css and NavMenu.razor.css.
    - Added new pages: Counter, Weather, and Error for functionality and error handling.
    - Configured application settings in appsettings.json and launchSettings.json.
    - Included favicon image for the application.

---
+++ src/UI/FluentCMS.Web.UI.Components/Color.cs
@@ -0,0 +1,16 @@
+namespace FluentCMS.Web.UI.Components;
+
+public enum Color
+{
+    Default,
+    Primary,
+    Secondary,
+    Success,
+    Danger,
+    Warning,
+    Info,
+    Light,
+    Dark,
+    White,
+    Transparent
+}
---
+++ src/UI/FluentCMS.Web.UI.Components/_GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using FluentCMS.Web.UI.Components;
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Forms;
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.JSInterop;
