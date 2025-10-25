e41ca36196c5f5a949c64ccbfeae3ebbe93355e1
Amir Pournasserian <amir.pournasserian@live.com>
2025-10-25 10:08:19 +0400

    Refactor CSS class generation in components
    Moved CSS class generation logic from BaseComponentHelpers.cs into BaseComponent. Updated Avatar, Badge, and Button components to use GetClasses() instead of CssClass. Removed unused Href parameter from Button and added OnClick event callback.

--- src/UI/FluentCMS.Web.UI.Components/Base/BaseComponent.razor.cs
+++ src/UI/FluentCMS.Web.UI.Components/Base/BaseComponent.razor.cs
@@ -2,6 +2,11 @@
 
 public abstract partial class BaseComponent : ComponentBase, IBaseComponent
 {
+    // css prefix for auto-generated classes
+    public const string CSS_PREFIX = "f";
+
+    public const string SEPARATOR = "-";
+
     protected abstract RenderFragment BuildContent { get; }
 
     [Parameter]
@@ -16,10 +21,6 @@ public abstract partial class BaseComponent : ComponentBase, IBaseComponent
     [Parameter]
     public string? CssName { get; set; }
 
-    protected string CssClass => this.GetClasses();
-
-    protected IReadOnlyList<string> CssClassList => this.ClassNames();
-
     public virtual string GetDefaultCssName()
     {
         var type = GetType();
@@ -27,5 +28,65 @@ public abstract partial class BaseComponent : ComponentBase, IBaseComponent
             return type.Name.Split("`").First().FromPascalCaseToKebabCase();
         else
             return type.Name.FromPascalCaseToKebabCase();
+    }
+
+    private static readonly ConcurrentDictionary<Type, CssPropertyMetadata[]> _cssPropertyCache = new();
+
+    private sealed record CssPropertyMetadata(PropertyInfo Property, Func<IBaseComponent, object?> ValueAccessor);
+
+    private static CssPropertyMetadata[] ResolveCssPropertyMetadata(Type componentType)
+    {
+        return [.. componentType
+            .GetProperties()
+            .Where(p => p.CustomAttributes.Any(x => x.AttributeType == typeof(CssPropertyAttribute)))
+            .Select(property =>
+            {
+                var componentParameter = Expression.Parameter(typeof(IBaseComponent), "component");
+                var castComponent = Expression.Convert(componentParameter, property.DeclaringType ?? componentType);
+                var propertyAccess = Expression.Property(castComponent, property);
+                var convertResult = Expression.Convert(propertyAccess, typeof(object));
+                var lambda = Expression.Lambda<Func<IBaseComponent, object?>>(convertResult, componentParameter).Compile();
+
+                return new CssPropertyMetadata(property, lambda);
+            })];
+    }
+
+    private List<string> ClassNames()
+    {
+        var classes = new List<string>();
+
+        // get properties with CSSProperty Attribute
+        var componentType = GetType();
+        var properties = _cssPropertyCache.GetOrAdd(componentType, ResolveCssPropertyMetadata);
+
+        var cssName = CssName?.FromPascalCaseToKebabCase() ?? GetDefaultCssName();
+
+        foreach (var property in properties)
+        {
+            if (property.ValueAccessor(this) is not { } value)
+                continue;
+
+            var propertyValue = value.ToString()?.FromPascalCaseToKebabCase() ?? string.Empty;
+            classes.Add(string.Join(SEPARATOR, [CSS_PREFIX, cssName, property.Property.Name.FromPascalCaseToKebabCase(), propertyValue]));
+        }
+
+        return classes;
+    }
+
+    public virtual string GetClasses()
+    {
+        var cssName = CssName?.FromPascalCaseToKebabCase() ?? GetDefaultCssName();
+
+        // component's class name from its name (f-button, f-badge, etc.)
+        var componentCss = string.Join(SEPARATOR, [CSS_PREFIX, cssName]);
+
+        // add css properties
+        List<string> classes = [componentCss, .. ClassNames()];
+
+        // if class is set by user, add the same class name
+        if (!string.IsNullOrEmpty(Class))
+            classes = [.. classes, Class];
+
+        return string.Join(" ", classes);
     }
 }
