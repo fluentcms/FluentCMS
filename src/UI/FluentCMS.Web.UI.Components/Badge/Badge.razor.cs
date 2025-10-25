bd2899d02d92b7ceece24c0b62d9b6676eb033eb
Amir Pournasserian <amir.pournasserian@live.com>
2025-10-25 09:29:57 +0400

    Refactor component base classes to .razor files
    Moved base component logic from .cs files to .razor files for Avatar, Badge, and Button components. Removed obsolete .razor.cs and BaseComponent.cs files, introduced BaseComponent.razor, BaseComponentWithContent.razor, and IBaseComponent.cs for improved Blazor component structure. Updated components to remove 'Visible' checks from markup and consolidate parameters into .razor files.

--- src/UI/FluentCMS.Web.UI.Components/Badge/Badge.razor.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FluentCMS.Web.UI.Components;
-
-public partial class Badge : BaseComponentWithContent
-{
-    [Parameter]
-    [CssProperty]
-    public Color Color { get; set; } = Color.Default;
-}
