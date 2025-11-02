6a78b7ef2bc9efe2229aaffca3be4c0b4802b0b1
TheHadiAhmadi <thehadiahmadi@gmail.com>
2025-11-02 21:01:44 +0430

    Move components to Components folder and add Icon component

--- src/UI/TestApp/Program.cs
+++ src/UI/TestApp/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddUIComponents();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
