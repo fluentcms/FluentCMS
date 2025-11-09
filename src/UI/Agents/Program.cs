f47ee02180e19158adde0aa59d80e380291c53c7
TheHadiAhmadi <thehadiahmadi@gmail.com>
2025-11-09 15:35:57 +0430

    first api call worked

--- src/UI/Agents/Program.cs
+++ src/UI/Agents/Program.cs
@@ -7,6 +7,7 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddUIComponents();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
